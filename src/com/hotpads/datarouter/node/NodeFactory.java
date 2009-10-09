b8ae7d7f4e20475fc0c9e31d7def795894b6deaf
Matt Corgan <mcorgan@hotpads.com>
2009-10-09 20:35:53 +0000

    map type

--- src/com/hotpads/datarouter/node/NodeFactory.java
+++ src/com/hotpads/datarouter/node/NodeFactory.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import com.hotpads.datarouter.client.imp.hibernate.node.HibernateNode;
 import com.hotpads.datarouter.node.op.IndexedSortedStorageNode;
+import com.hotpads.datarouter.node.op.MapStorageNode;
 import com.hotpads.datarouter.node.op.SortedStorageNode;
 import com.hotpads.datarouter.node.type.masterslave.MasterSlaveIndexedSortedStorageNode;
+import com.hotpads.datarouter.node.type.masterslave.MasterSlaveMapStorageNode;
 import com.hotpads.datarouter.node.type.masterslave.MasterSlaveSortedStorageNode;
 import com.hotpads.datarouter.routing.DataRouter;
 import com.hotpads.datarouter.storage.databean.Databean;
@@ -14,6 +16,27 @@ import com.hotpads.util.core.CollectionTool;
 import com.hotpads.util.core.ListTool;
 
 public class NodeFactory {
+
+	public static <D extends Databean> 
+	MasterSlaveMapStorageNode<D,MapStorageNode<D>> 
+	newMasterSlaveMap(
+			DataRouter router, Class<D> databeanClass, 
+			String masterClientName, Collection<String> slaveClientNames){
+		
+		MapStorageNode<D> master = null;
+		if(masterClientName != null){
+			master = new HibernateNode<D>(databeanClass, router, masterClientName);
+		}
+		
+		List<MapStorageNode<D>> slaves = ListTool.createLinkedList();
+		for(String slaveClientName : CollectionTool.nullSafe(slaveClientNames)){
+			slaves.add(new HibernateNode<D>(databeanClass, router, slaveClientName));
+		}
+		
+		return new MasterSlaveMapStorageNode<D,MapStorageNode<D>>(
+				databeanClass, router, master, slaves);
+		
+	}
 
 	public static <D extends Databean> 
 	MasterSlaveSortedStorageNode<D,SortedStorageNode<D>> 
