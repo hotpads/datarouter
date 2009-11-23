175882d6229a3e9dd70bcd4cae06a487566615bd
Matt Corgan <mcorgan@hotpads.com>
2009-11-23 18:18:35 +0000

    common Config objects

---
+++ src/com/hotpads/datarouter/config/Configs.java
@@ -0,0 +1,10 @@
+package com.hotpads.datarouter.config;
+
+public class Configs {
+
+	public static final Config SLAVE_OK = new Config().setSlaveOk(true);
+	
+	public static final Config INSERT_UPDATE = new Config().setPutMethod(PutMethod.INSERT_OR_UPDATE);
+	public static final Config UPDATE_INSERT = new Config().setPutMethod(PutMethod.UPDATE_OR_INSERT);
+	
+}
