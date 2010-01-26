f679f7a1e3b249f0631627add7f2c2bfc8a74999
mcorgan <mcorgan@hotpads.com>
2010-01-26 12:41:03 -0500

    new

---
+++ src/com/hotpads/datarouter/client/imp/memory/node/HashMapNode.java
@@ -0,0 +1,73 @@
+package com.hotpads.datarouter.client.imp.memory.node;
+
+import java.util.Collection;
+
+import com.hotpads.datarouter.config.Config;
+import com.hotpads.datarouter.node.Node;
+import com.hotpads.datarouter.node.type.physical.PhysicalMapStorageNode;
+import com.hotpads.datarouter.routing.DataRouter;
+import com.hotpads.datarouter.storage.databean.Databean;
+import com.hotpads.datarouter.storage.key.Key;
+import com.hotpads.util.core.CollectionTool;
+
+public class HashMapNode<D extends Databean> 
+extends HashMapReaderNode<D>
+implements PhysicalMapStorageNode<D>
+{
+	
+	public HashMapNode(Class<D> databeanClass, DataRouter router, String clientName, 
+			String physicalName, String qualifiedPhysicalName) {
+		super(databeanClass, router, clientName, physicalName, qualifiedPhysicalName);
+	}
+	
+	public HashMapNode(Class<D> databeanClass, DataRouter router, String clientName) {
+		super(databeanClass, router, clientName);
+	}
+	
+	@Override
+	public Node<D> getMaster() {
+		return this;
+	}
+	
+	
+	/************************************ MapStorageWriter methods ****************************/
+
+	@Override
+	public void delete(Key<D> key, Config config) {
+		if(key==null){ return; }
+		this.backingMap.remove(key);
+	}
+	
+	@Override
+	public void deleteMulti(Collection<? extends Key<D>> keys, Config config) {
+		for(Key<D> key : CollectionTool.nullSafe(keys)){
+			this.backingMap.remove(key);
+		}
+	}
+	
+	
+	@Override
+	public void deleteAll(Config config) {
+		this.backingMap.clear();
+	}
+
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public void put(final D databean, Config config) {
+		if(databean==null || databean.getKey()==null){ return; }
+		this.backingMap.put(databean.getKey(), databean);
+	}
+
+	
+	@Override
+	public void putMulti(Collection<D> databeans, Config config) {
+		for(D databean : CollectionTool.nullSafe(databeans)){
+			this.put(databean, config);
+		}
+	}
+	
+	
+
+	
+}
---
+++ src/com/hotpads/datarouter/client/imp/memory/node/HashMapReaderNode.java
@@ -0,0 +1,88 @@
+package com.hotpads.datarouter.client.imp.memory.node;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.hotpads.datarouter.client.imp.hibernate.HibernateClientImp;
+import com.hotpads.datarouter.config.Config;
+import com.hotpads.datarouter.node.Node;
+import com.hotpads.datarouter.node.base.physical.BasePhysicalNode;
+import com.hotpads.datarouter.node.type.physical.PhysicalMapStorageReaderNode;
+import com.hotpads.datarouter.routing.DataRouter;
+import com.hotpads.datarouter.storage.databean.Databean;
+import com.hotpads.datarouter.storage.key.Key;
+import com.hotpads.util.core.CollectionTool;
+import com.hotpads.util.core.ListTool;
+
+public class HashMapReaderNode<D extends Databean> 
+extends BasePhysicalNode<D>
+implements PhysicalMapStorageReaderNode<D>
+{
+	
+	protected Map<Key<D>,D> backingMap = new HashMap<Key<D>,D>();
+
+	public HashMapReaderNode(Class<D> databeanClass, DataRouter router, String clientName, 
+			String physicalName, String qualifiedPhysicalName) {
+		super(databeanClass, router, clientName, physicalName, qualifiedPhysicalName);
+	}
+	
+	public HashMapReaderNode(Class<D> databeanClass, DataRouter router, String clientName) {
+		super(databeanClass, router, clientName);
+	}
+
+	@Override
+	public HibernateClientImp getClient(){
+		return (HibernateClientImp)this.router.getClient(getClientName());
+	}
+	
+	@Override
+	public Node<D> getMaster() {
+		return null;
+	}
+	
+	@Override
+	public void clearThreadSpecificState(){
+		//do nothing, i think
+	}
+
+	
+	/************************************ MapStorageReader methods ****************************/
+	
+	@Override
+	public boolean exists(Key<D> key, Config config) {
+		return this.backingMap.containsKey(key);
+	}
+
+	
+	@Override
+	public D get(final Key<D> key, Config config) {
+		return this.backingMap.get(key);
+	}
+	
+	
+	@Override
+	public List<D> getAll(final Config config) {		
+		List<D> result = ListTool.createArrayList(this.backingMap.size());
+		for(D d : this.backingMap.values()){
+			result.add(d);
+		}
+		return result;
+	}
+
+	
+	@Override
+	public List<D> getMulti(final Collection<? extends Key<D>> keys, Config config) {		
+		List<D> result = ListTool.createLinkedList();
+		for(Key<D> key : CollectionTool.nullSafe(keys)){
+			D value = this.backingMap.get(key);
+			if(value != null){
+				result.add(value);
+			}
+		}
+		return result;
+	}
+
+	
+}
