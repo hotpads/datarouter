a392d4f39630838e2f31fe5bd8c0889037b7f53a
mcorgan <mcorgan@hotpads.com>
2010-06-09 16:44:25 -0400

    field serializers

--- src/com/hotpads/datarouter/storage/field/imp/ByteField.java
+++ src/com/hotpads/datarouter/storage/field/imp/ByteField.java
@@ -1,8 +1,5 @@
 package com.hotpads.datarouter.storage.field.imp;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.hotpads.datarouter.storage.field.PrimitiveField;
 import com.hotpads.util.core.ByteTool;
 
@@ -23,44 +20,8 @@ public class ByteField extends PrimitiveField<Byte>{
 	
 	
 	public byte[] getComparableByteArray(){
-		if(value >= 0){
-			return new byte[]{(byte) (value + Byte.MIN_VALUE) };
-		}else{
-			return new byte[]{(byte) (value - Byte.MIN_VALUE) };
-		}
+		return ByteTool.getComparableByteArray(value);
 	}
 	
-	
-	public static class Tests{
-		@Test public void testGetOrderedBytes(){
-			byte min = Byte.MIN_VALUE;
-			Assert.assertEquals(-128, min);
-			byte max = Byte.MAX_VALUE;
-			Assert.assertEquals(127, max);
-			Assert.assertTrue(min < max);
-			
-			byte[] minArray = new ByteField("",min).getComparableByteArray();
-			byte[] maxArray = new ByteField("",max).getComparableByteArray();
-			Assert.assertTrue(ByteTool.bitwiseCompare(maxArray, minArray) > 0);
-			
-			System.out.println(ByteTool.getBinaryStringBigEndian(min)+" "
-					+ByteTool.getBinaryStringBigEndian(max)+" "
-					+ByteTool.getBinaryStringBigEndian(minArray)+" "
-					+ByteTool.getBinaryStringBigEndian(maxArray));
-
-			byte negative = -3;
-			byte positive = 5;
-			Assert.assertTrue(negative < positive);
-
-			byte[] negativeArray = new ByteField("",negative).getComparableByteArray();
-			byte[] positiveArray = new ByteField("",positive).getComparableByteArray();
-			Assert.assertTrue(ByteTool.bitwiseCompare(positiveArray, negativeArray) > 0);
-			
-			System.out.println(ByteTool.getBinaryStringBigEndian(negative)+" "
-					+ByteTool.getBinaryStringBigEndian(positive)+" "
-					+ByteTool.getBinaryStringBigEndian(negativeArray)+" "
-					+ByteTool.getBinaryStringBigEndian(positiveArray));
-		}
-	}
 
 }
--- src/com/hotpads/datarouter/storage/field/imp/LongField.java
+++ src/com/hotpads/datarouter/storage/field/imp/LongField.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.hotpads.datarouter.storage.field.PrimitiveField;
+import com.hotpads.util.core.LongTool;
 
 public class LongField extends PrimitiveField<Long>{
 
@@ -35,43 +36,7 @@ public class LongField extends PrimitiveField<Long>{
 	
 	
 	public byte[] getComparableByteArray(){
-		byte[] out = new byte[8];
-		long shifted;
-		if(value >= 0){
-			shifted = value + Long.MIN_VALUE;
-		}else{
-			shifted = value - Long.MIN_VALUE;
-		}
-		out[7] = (byte) shifted;
-		out[6] = (byte) (shifted >>>= 8);
-		out[5] = (byte) (shifted >>>= 8);
-		out[4] = (byte) (shifted >>>= 8);
-		out[3] = (byte) (shifted >>>= 8);
-		out[2] = (byte) (shifted >>>= 8);
-		out[1] = (byte) (shifted >>>= 8);
-		out[0] = (byte) (shifted >>>= 8);
-		return out;
-	}
-	
-	
-	public static class Tests{
-		@Test public void testGetOrderedBytes(){
-			long a = Long.MIN_VALUE;
-			byte[] ab = new byte[]{0,0,0,0,0,0,0,0};
-			Assert.assertArrayEquals(ab, new LongField("", a).getComparableByteArray());
-
-			long b = Long.MAX_VALUE;
-			byte[] bb = new byte[]{-1,-1,-1,-1,-1,-1,-1,-1};
-			Assert.assertArrayEquals(bb, new LongField("", b).getComparableByteArray());
-
-			long c = Long.MIN_VALUE + 1;
-			byte[] cb = new byte[]{0,0,0,0,0,0,0,1};
-			Assert.assertArrayEquals(cb, new LongField("", c).getComparableByteArray());
-
-			long d = Long.MAX_VALUE - 3;
-			byte[] db = new byte[]{-1,-1,-1,-1,-1,-1,-1,-4};
-			Assert.assertArrayEquals(db, new LongField("", d).getComparableByteArray());
-		}
+		return LongTool.getComparableByteArray(value);
 	}
 
 }
