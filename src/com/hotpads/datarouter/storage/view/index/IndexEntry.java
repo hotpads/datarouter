10646d4d5955ae3b3f4428f7d14bc50de45dc5aa
mcorgan <mcorgan@hotpads.com>
2010-12-01 14:21:39 -0500

    commit again

--- src/com/hotpads/datarouter/storage/view/index/IndexEntry.java
+++ src/com/hotpads/datarouter/storage/view/index/IndexEntry.java
@@ -2,10 +2,15 @@ package com.hotpads.datarouter.storage.view.index;
 
 import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
+import com.hotpads.datarouter.storage.key.unique.UniqueKey;
 
-public interface IndexEntry<IK extends PrimaryKey<IK>,TK extends PrimaryKey<TK>> 
-extends Databean<IK>{
+public interface IndexEntry<
+		IK extends PrimaryKey<IK>,
+		PK extends PrimaryKey<PK>,
+		D extends Databean<PK>> 
+extends Databean<IK>, UniqueKey<PK>{
 
-	TK getTargetKey();
+	PK getTargetKey();
+	void fromDatabean(D target);
 	
 }
