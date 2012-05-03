d551fc2420fd1f01cf0e186b722e0d314bfd348a
moubenal <moubenal@hotpads.com>
2012-05-02 17:08:34 -0700

    created different methods to split the Sql querries : - get the header - the trail - the body - the column declarations - the column name, type, maxValue, nullability
    - added documentation to methods

--- src/com/hotpads/datarouter/client/imp/jdbc/ddl/SqlTable.java
+++ src/com/hotpads/datarouter/client/imp/jdbc/ddl/SqlTable.java
@@ -9,28 +9,76 @@ public class SqlTable {
 	// ATTRIBUTES
 	String name;
 	List<SqlColumn> columns;
+	SqlIndex primaryKey;
+	List<SqlIndex> indexes;
 	
-	
-	
+	// CONSTRUCTORS
+	public SqlTable(String name, List<SqlColumn> columns, SqlIndex primaryKey,
+			List<SqlIndex> indexes) {
+		super();
+		this.name = name;
+		this.columns = columns;
+		this.primaryKey = primaryKey;
+		this.indexes = indexes;
+	}
+
 	public SqlTable(String name, List<SqlColumn> columns) {
 		super();
 		this.name = name;
 		this.columns = columns;
+		this.indexes = ListTool.createArrayList();
 	}
 	
 	public SqlTable(String name) {
 		super();
 		this.name = name;
 		columns = ListTool.createArrayList();
+		this.indexes = ListTool.createArrayList();
 	}
 
 	public static SqlTable parseCreateTable(String createTableStatement){
 		return null;
-		
 	}
 	public String getCreateTable(){
 		return null;
-		
+	}
+
+	// GETTERS & SETTERS
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<SqlColumn> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<SqlColumn> columns) {
+		this.columns = columns;
+	}
+
+	public SqlIndex getPrimaryKey() {
+		return primaryKey;
+	}
+
+	public void setPrimaryKey(String s) {
+		for(SqlColumn col : columns){
+			if(col.getName().equals(s)){
+				//this.primaryKey =  col;
+				System.out.println("The primary key is " + col);
+			}
+		}
+			}
+
+	public List<SqlIndex> getIndexes() {
+		return indexes;
+	}
+
+	public void setIndexes(List<SqlIndex> indexes) {
+		this.indexes = indexes;
 	}
 
 	@Override
--- src/com/hotpads/datarouter/client/imp/jdbc/ddl/testParser.java
+++ src/com/hotpads/datarouter/client/imp/jdbc/ddl/testParser.java
@@ -3,34 +3,19 @@ package com.hotpads.datarouter.client.imp.jdbc.ddl;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import java.sql.Statement;
-
-import junit.framework.Assert;
-
-import org.antlr.grammar.v3.ANTLRv3Parser.throwsSpec_return;
-import org.apache.hadoop.hbase.regionserver.ColumnTracker;
-import org.junit.Test;
-import org.mockito.internal.stubbing.answers.ThrowsException;
-
-import sun.dc.pr.PRError;
-
-import com.hotpads.datarouter.client.imp.hibernate.util.JdbcTool;
 import com.hotpads.util.core.ListTool;
-import com.sun.org.apache.xml.internal.serializer.ToUnknownStream;
 
 public class testParser {
 
 	public static void main(String[] args) throws SQLException, IOException  {
+		testGetHeader();
+		 testGetTail();
+		 testGetBody();
 		FileInputStream fis = new FileInputStream("/home/moubenal/workspace/datarouter/src/com/hotpads/datarouter/client/imp/jdbc/ddl/test.txt");
 		// Get the object of DataInputStream
 		  DataInputStream in = new DataInputStream(fis);
@@ -39,7 +24,25 @@ public class testParser {
 		while((str=br.readLine()) != null) {
 			phrase+=str;
 		}
-		 SqlTable table = parseAndCreateTable(phrase);
+		for(String s:getColumns(getBody(phrase))){
+				if(isPrimaryKeyDeclaration(s)){
+					
+				}
+				else if(isKeyDeclaration(s)){
+					
+				}
+				else{
+					System.out.println("name : " +getNameOfColumn(s));
+				
+				System.out.println("type : " +getTypeOfColumn(s));
+				if(hasAMaxValue(s)){
+					System.out.println("max value : " +getMaxValueOfColumn(s));
+				}
+				System.out.println("nullable : " +getNullable(s));
+			}
+		}
+		
+		 //SqlTable table = parseAndCreateTable(phrase);
 		
 		//if(tokens[0].contains("CREATE TABLE")) System.out.println("Roger That!");
 		// TAKE THE STRING
@@ -49,24 +52,110 @@ public class testParser {
 			// FOR THE OTHER ONES USE CONTAINS TO KNOW WHAT TYPE OF QUERIE WE ARE DEALING WITH
 	}
 
-	 public static SqlTable parseAndCreateTable(String phrase){
+	/**
+	 * 
+	 * @param s  example:			KEY `index_yyyymmddhhmmss` (`year`,`month`,`date`,`hour`,`minute`,`second`),
+	 * @return true if it's a declaration of an index
+	 */
+	 private static boolean isKeyDeclaration(String s) {
+		// TODO Auto-generated method stub
+		 return s.toUpperCase().startsWith("KEY") || s.toUpperCase().startsWith("KEY", 1)  || s.toUpperCase().startsWith("KEY", 2) ;
+	}
+
+	 /**
+	  * 
+	  * @param s example:   			   PRIMARY KEY (`id`),
+	  * @return true if it's a primary key declaration
+	  */
+	private static boolean isPrimaryKeyDeclaration(String s) {
+		// TODO Auto-generated method stub
+		return s.toUpperCase().startsWith("PRIMARY") || s.toUpperCase().startsWith("PRIMARY", 1)  || s.toUpperCase().startsWith("PRIMARY", 2) ;
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return true if the column type can be null
+	 */
+	private static boolean getNullable(String s) {
+		// TODO Auto-generated method stub
+		return !s.contains("NOT");
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return true if the column type has a maximum value
+	 */
+	private static boolean hasAMaxValue(String s) {
+		// TODO Auto-generated method stub
+		return s.contains("(");
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return the maximum value for the column type
+	 */
+	private static String getMaxValueOfColumn(String s) {
+		// TODO Auto-generated method stub
+				 int index = s.lastIndexOf('`');
+				String[] tokens = s.substring(index+1).split("[ ()]+");
+				return tokens[2];
+	}
+
+	/**
+	 *  
+	 * @param s
+	 * @return the type name of the column 
+	 */
+	private static String getTypeOfColumn(String s) {
+		// TODO Auto-generated method stub
+		 int index = s.lastIndexOf('`');
+		String[] tokens = s.substring(index+1).split("[ ()]+");
+		return tokens[1];
+	}
+
+
+	 /**
+	  * 
+	  * @param s a column
+	  * @return the name of the column
+	  */
+	private static String getNameOfColumn(String s) {
+		// TODO Auto-generated method stub
+		 int index = s.indexOf('`');
+		 String[] tokens = s.substring(index+1).split("[`]+");
+		return tokens[0];
+	}
+
+	@Deprecated
+	/**
+	 * OLD VERSION OF THE PARSING 
+	 * @param phrase
+	 * @return
+	 */
+	public static SqlTable parseAndCreateTable(String phrase){
 		List<SqlColumn> columns = ListTool.createArrayList();
 		String delims = "[(),]+";					// THE DELIMITERS FOR TOKENIZING
 		String[] tokens = phrase.split(delims);		// TOKENIZING
 		
 		System.out.println("1st Parsing :");
 		System.out.println("\n /****** Getting the name of the table ******/");		
+		SqlTable table = new SqlTable("generated Sql Table");
 		
 		for(int i=1; i<tokens.length; i++){
 			if(tokens[i].contains("PRIMARY KEY")){
+				System.out.println();
+				table.setPrimaryKey(removeNonText(tokens[++i]));
+			}
+			else if(tokens[i].contains("KEY")){ // INDEXES 
 				
 			}
-			//if(tokens[i].contains("")){
-			//}
 			else{
 				String[] tempTokens = tokens[i].split("[`]");
 				String name=removeSpaces(tempTokens[1]), type;
-				if(tempTokens[2].length()<16){
+				if(tempTokens[2].contains("datetime")){ // WHEN THERE'S NO MAX LENGTH THISE TOKEN IS CONTAINS CERTAIN TYPES 
 					type = removeSpaces(tempTokens[2]);
 					
 					SqlColumn col = createAppropriateColumnType(name, type);
@@ -97,17 +186,86 @@ public class testParser {
 					System.out.println(col);
 					columns.add(col);
 				}
-				
+				table.setColumns(columns);
 			}
-			
 		}
-		return new SqlTable("generated Sql Table", columns);
+		return table;
 	}
 	
+	/**
+	 * 
+	 * @param phrase is what we got for queries of the type " show create table nameOfTheTable " 
+	 * @return the  header of the SQL phrase, i.e. things before the first "(" 
+	 */
+	 public static String  getHeader(String phrase){
+		    int index = phrase.indexOf('(');
+			return phrase.substring(0,index);
+	 }
+	 
+	 public static void testGetHeader(){ 
+		 System.out.println(getHeader("Header(blabla(blob()))trail"));
+	 }
+	 /**
+	  * 
+	  * @param phrase is what we got for queries of the type " show create table nameOfTheTable " 
+	  * @return the tail of the SQL phrase, i.e. things after the last ")" 
+	  */
+	 public static String getTail(String phrase){
+		 int index = phrase.lastIndexOf(')');
+			return phrase.substring(index+1);
+	 }
+	 public static void testGetTail(){ 
+		 System.out.println(getTail("Header(blabla(blob()))trail"));
+	 }
+	 
+	 
+	 /**
+	  * 
+	  * @param phrase is what we got for queries of the type " show create table nameOfTheTable " 
+	  * @return the body of the SQL phrase, i.e. things between the first "(" and the last ")" 
+	  */
+	 public static String getBody(String phrase){
+		    int index1 = phrase.indexOf('('), index2=phrase.lastIndexOf(')');
+			return phrase.substring(index1+1,index2);
+	 }
+	 public static void testGetBody(){
+		 System.out.println(getBody("Header(blabla(blob()))trail"));
+	 }
+	 
+	 /**
+	  * 
+	  * @param phrase is the body of an Sql  querie of the type " show create table nameOfTheTable " 
+	  * @return list of Strings containing the column declarations, primary key declaration and key/indexes declaration 
+	  */
+	 private static String[] getColumns(String phrase){
+		return phrase.split("[,]+"); 
+	 }
+	 private static void testGetColumns(){
+		 System.out.println(getBody("Header(blabla(blob()))trail"));
+	 }
+	 /**
+	  * 
+	  * @param s
+	  * @return s without occurrences of space : " "
+	  */
 	private static String removeSpaces(String s) {
 		String sResult="";
 		for (int i = 0; i < s.length(); i++) {
 			if(s.charAt(i)!=' '){
+				sResult+=s.charAt(i);
+			}
+		}
+		return sResult;
+	}
+	/**
+	 * 
+	 * @param s
+	 * @return s without occurrences of " " and "`" 
+	 */
+	private static String removeNonText(String s) {
+		String sResult="";
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i)!=' ' && s.charAt(i)!='`'){
 				sResult+=s.charAt(i);
 			}
 		}
