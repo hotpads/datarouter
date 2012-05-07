dc0cfd1313eb3fbdc7d99913710e2c928a84f5c8
moubenal <moubenal@hotpads.com>
2012-05-07 10:56:17 -0700

    - minor changes

--- src/com/hotpads/datarouter/client/imp/jdbc/ddl/SqlTableDiffGenerator.java
+++ src/com/hotpads/datarouter/client/imp/jdbc/ddl/SqlTableDiffGenerator.java
@@ -44,14 +44,18 @@ public class SqlTableDiffGenerator {
 	}
 
 	public boolean isTableModified() {
-		if (!current.getName().equals(requested)){
+		if (!current.getName().equals(requested.getName())){
+			System.out.println("The name of the table has changed.");
+			
 			return true;
 		}
 		if (current.getNumberOfColumns() != requested.getNumberOfColumns()){
+			System.out.println("The number of columns of the table has changed.");
 			return true;
 		}
 
-		if (current.getIndexes().size() == requested.getIndexes().size()){
+		if (current.getIndexes().size() != requested.getIndexes().size()){
+			System.out.println("The number of indexes has changed.");
 			return true;
 		}
 
@@ -102,7 +106,15 @@ public class SqlTableDiffGenerator {
 			SqlTableDiffGenerator diffAB = new SqlTableDiffGenerator(tableA, tableB), diffAA = new SqlTableDiffGenerator(tableA, tableA);
 			Assert.assertFalse(diffAA.isTableModified());
 			Assert.assertTrue(diffAB.isTableModified());
-			// TABLES ...
+			// TABLES WITH DIFFERENT NUMBER OF COLUMNS
+			SqlColumn col1 = new SqlColumn("col1", MySqlColumnType.BIGINT), 
+					col2 = new SqlColumn("col2", MySqlColumnType.BINARY), 
+					col3 = new SqlColumn("col3", MySqlColumnType.BIT);
+			tableA.addColumn(col1);
+			tableB.addColumn(col1);
+			tableB.addColumn(col2);
+			Assert.assertFalse(diffAA.isTableModified());
+			Assert.assertTrue(diffAB.isTableModified());
 		}
 	}
 }
