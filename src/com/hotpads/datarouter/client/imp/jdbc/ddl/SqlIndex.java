7a1449e40b8a56a0caf561e3bf5dc325b8fd36b9
moubenal <moubenal@hotpads.com>
2012-05-08 14:26:49 -0700

    - implementing Comparable

--- src/com/hotpads/datarouter/client/imp/jdbc/ddl/SqlIndex.java
+++ src/com/hotpads/datarouter/client/imp/jdbc/ddl/SqlIndex.java
@@ -2,10 +2,14 @@ package com.hotpads.datarouter.client.imp.jdbc.ddl;
 
 import java.util.List;
 
-import com.hotpads.util.core.CollectionTool;
-import com.hotpads.util.core.ListTool;
+import sun.org.mozilla.javascript.internal.ast.ThrowStatement;
 
-public class SqlIndex {
+import com.hotpads.util.core.CollectionTool;
+import com.hotpads.util.core.ComparableTool;
+import com.hotpads.util.core.ListTool;
+import com.hotpads.util.core.MathTool;
+
+public class SqlIndex implements Comparable{
 	
 	/********************** fields *************************/
 	
@@ -64,5 +68,56 @@ public class SqlIndex {
 	
 	public int getNumberOfColumns(){
 		return CollectionTool.size(columns);
+	}
+
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((columns == null) ? 0 : columns.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SqlIndex)) {
+			return false;
+		}
+		SqlIndex other = (SqlIndex) obj;
+		if (columns == null) {
+			if (other.columns != null) {
+				return false;
+			}
+		} else if (!columns.equals(other.columns)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+			int c = ComparableTool.nullFirstCompareTo(name, ((SqlIndex) o).name);
+			if(c!=0){return c;}
+			for(int i=0; i<MathTool.min(columns.size(), ((SqlIndex) o).columns.size()); i++){
+				c=ComparableTool.nullFirstCompareTo(columns.get(i),((SqlIndex) o).columns.get(i));
+				if(c!=0){return c;}
+			}
+			return 0;
 	}
 }
