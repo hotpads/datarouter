98901c96992b541e24e251dd9ff7b2be1ef26578
Mohcine Oubenal, guarana <moubenal@hotpads.com>
2012-06-18 19:10:33 -0700

    - fix

--- src/com/hotpads/datarouter/client/imp/jdbc/ddl/domain/MySqlColumnType.java
+++ src/com/hotpads/datarouter/client/imp/jdbc/ddl/domain/MySqlColumnType.java
@@ -47,11 +47,14 @@ public enum MySqlColumnType {
 	SET;
 	
 	public static final Long 
-			MAX_LENGTH_VARCHAR = 1L << 8 - 1,
-			MAX_LENGTH_TEXT = 1L << 16 - 1,
-			MAX_LENGTH_MEDIUMTEXT = 1L << 24 - 1,
-			MAX_LENGTH_LONGTEXT = 1L << 32 - 1;
+			MAX_LENGTH_VARCHAR = 1L << 9 - 1,
+			MAX_LENGTH_TEXT = 1L << 17 - 1,
+			MAX_LENGTH_MEDIUMTEXT = 1L << 25 - 1,
+			MAX_LENGTH_LONGTEXT = 1L << 33 - 1;
 
+	public static void main(String[] args){
+		System.out.println(MAX_LENGTH_VARCHAR + " " + MAX_LENGTH_TEXT + " " + " " + MAX_LENGTH_MEDIUMTEXT + " " + MAX_LENGTH_LONGTEXT);
+	}
 	public static MySqlColumnType parse(String a){
 		String upperCase = StringTool.nullSafe(a).toUpperCase();
 		for(MySqlColumnType type : values()) {
