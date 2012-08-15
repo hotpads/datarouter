a03bb0acc05e67942062e2ca43162d423121bbd9
David Peters <dpeters@hotpads.com>
2012-08-15 15:26:45 -0700

    wild goose chase

--- src/com/hotpads/handler/util/RequestTool.java
+++ src/com/hotpads/handler/util/RequestTool.java
@@ -19,6 +19,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
+
 import com.hotpads.util.core.BooleanTool;
 import com.hotpads.util.core.CollectionTool;
 import com.hotpads.util.core.Functor;
@@ -37,12 +38,13 @@ public class RequestTool {
 	
 	public static String getSubmitAction(HttpServletRequest request){
 		String submitAction = request.getParameter(SUBMIT_ACTION);
-		if(submitAction != null){ return submitAction; }
+		if(submitAction != null){ 
+			return submitAction; 
+		}
 		throw new NullPointerException("param "+SUBMIT_ACTION+" not found");
 	}
 	
-	public static String getSubmitAction(
-			HttpServletRequest request, String defaultAction){
+	public static String getSubmitAction(HttpServletRequest request, String defaultAction){
 		String action = get(request, SUBMIT_ACTION, defaultAction);
 		return action;
 	}
@@ -59,12 +61,13 @@ public class RequestTool {
 						} 
 					},
 					uriVars);
-		if(uriVars==null){ uriVars = new LinkedList<String>(); }
+		if(uriVars==null){ 
+			uriVars = new LinkedList<String>();
+		}
 		return uriVars;
 	}
 	
-	public static String getUriFromSlashedParts(
-			HttpServletRequest request, List<String> slashedParts){
+	public static String getUriFromSlashedParts(HttpServletRequest request, List<String> slashedParts){
 		String queryString = request.getQueryString();
 
 		String uri;
@@ -102,14 +105,12 @@ public class RequestTool {
 	public static String get(HttpServletRequest request, String paramName){
 		String stringVal = request.getParameter(paramName);
 		if(stringVal==null){
-			throw new IllegalArgumentException(
-					"expected String:"+paramName+", but was null");
+			throw new IllegalArgumentException("expected String:"+paramName+", but was null");
 		}
 		return stringVal;
 	}
 	
-	public static String get(
-			HttpServletRequest request, String paramName, String defaultValue){
+	public static String get(HttpServletRequest request, String paramName, String defaultValue){
 		String stringVal = request.getParameter(paramName);
 		return stringVal==null ? defaultValue : stringVal;
 	}
@@ -146,8 +147,7 @@ public class RequestTool {
 		throw new IllegalArgumentException("required Long "+paramName+" is invalid");
 	}
 	
-	public static Long getLong(
-			HttpServletRequest request, String paramName, Long defaultValue){
+	public static Long getLong(HttpServletRequest request, String paramName, Long defaultValue){
 		String stringVal = get(request, paramName, null);
 		return NumberTool.getLongNullSafe(stringVal,defaultValue);
 	}
@@ -164,26 +164,22 @@ public class RequestTool {
 		throw new IllegalArgumentException("required Integer "+paramName+" is invalid");
 	}
 	
-	public static Integer getInteger(
-			HttpServletRequest request, String paramName, Integer defaultValue){
+	public static Integer getInteger(HttpServletRequest request, String paramName, Integer defaultValue){
 		String stringVal = get(request, paramName, null);
 		return NumberTool.getIntegerNullSafe(stringVal,defaultValue);
 	}
 
-	public static Double getDouble(
-			HttpServletRequest request, String paramName, Double defaultValue){
+	public static Double getDouble(HttpServletRequest request, String paramName, Double defaultValue){
 		String stringVal = get(request, paramName, null);
 		return NumberTool.getDoubleNullSafe(stringVal,defaultValue);
 	}
 
-	public static List<String> getCsvList(
-			HttpServletRequest request, String paramName, List<String> defaultValue){
+	public static List<String> getCsvList(HttpServletRequest request, String paramName, List<String> defaultValue){
 		return getList(request, paramName, ",", defaultValue);
 	}
 	
-	public static List<String> getList(
-			HttpServletRequest request, String paramName, 
-			String delimiter, List<String> defaultValue){
+	public static List<String> getList(HttpServletRequest request, String paramName, String delimiter, 
+			List<String> defaultValue){
 		String stringVal = request.getParameter(paramName);
 		if(StringTool.isEmpty(stringVal)){
 			return defaultValue;
@@ -215,8 +211,8 @@ public class RequestTool {
 		}
 	}
 	
-	public static Integer getIntegerParameterNullSafe(
-			HttpServletRequest request, String paramName, Integer defaultValue){
+	public static Integer getIntegerParameterNullSafe(HttpServletRequest request, String paramName, 
+			Integer defaultValue){
 		String stringVal = get(request, paramName, null);
 		return NumberTool.getIntegerNullSafe(stringVal,defaultValue);
 	}
@@ -243,12 +239,9 @@ public class RequestTool {
 		}
 	}
 	
-	public static Boolean getBooleanParameterNullSafeStandardJava(
-			HttpServletRequest request, 
-			String paramName, 
+	public static Boolean getBooleanParameterNullSafeStandardJava(HttpServletRequest request, String paramName, 
 			Boolean defaultValue){
 		String stringVal = get(request, paramName, null);
-
 		try{
 			return Boolean.valueOf(stringVal);
 		}catch(Exception e){
@@ -278,8 +271,8 @@ public class RequestTool {
 		}
 	}
 	
-	public static Boolean getBooleanParameterNullSafeCustomValues(
-			HttpServletRequest request, String paramName, Boolean defaultValue){
+	public static Boolean getBooleanParameterNullSafeCustomValues(HttpServletRequest request, String paramName, 
+			Boolean defaultValue){
 		String stringVal = get(request, paramName, null);
 
 		if(StringTool.isEmpty(stringVal)){
@@ -319,8 +312,7 @@ public class RequestTool {
 		}
 	}
 	
-	public static void setAttributes(
-			HttpServletRequest request, Map<String,Object> attributeMap){
+	public static void setAttributes(HttpServletRequest request, Map<String,Object> attributeMap){
 		for(String key : CollectionTool.nullSafe(attributeMap.keySet())){
 			request.setAttribute(key, attributeMap.get(key));
 		}
@@ -331,28 +323,24 @@ public class RequestTool {
 	 * @param request
 	 * @return
 	 */
-	public static DefaultableMap<String, String> getParamMap(
-			HttpServletRequest request) {
-		DefaultableHashMap<String, String> map = 
-			new DefaultableHashMap<String,String>();
+	public static DefaultableMap<String, String> getParamMap(HttpServletRequest request) {
+		DefaultableHashMap<String, String> map = new DefaultableHashMap<String,String>();
 		return getParameterMap(map,request);
 	}
-	public static NavigableMap<String, String> getMapOfParameters(
-			HttpServletRequest request){
+	public static NavigableMap<String, String> getMapOfParameters(HttpServletRequest request){
 		NavigableMap<String, String> map = new TreeMap<String, String>();
 		return getParameterMap(map,request);
 	}
 	
-	public static <M extends Map<String,String>> M 
-	getParameterMap(M map, HttpServletRequest request){
+	public static <M extends Map<String,String>> M getParameterMap(M map, HttpServletRequest request){
 		for(Object paramName : request.getParameterMap().keySet()){
 			map.put((String)paramName, request.getParameter((String)paramName));
 		}
 		return map;
 	}
 	
-	public static NavigableMap<String, String> getMapOfParametersMatchingPrefix(
-			HttpServletRequest request, String prefix){
+	public static NavigableMap<String, String> getMapOfParametersMatchingPrefix(HttpServletRequest request, 
+			String prefix){
 		NavigableMap<String, String> allVars = getMapOfParameters(request);
 		NavigableMap<String, String> matches = new TreeMap<String,String>();
 		for(String key : allVars.keySet()){
@@ -380,20 +368,15 @@ public class RequestTool {
 		return map;
 	}
 	
-	public static boolean checkDouble(
-			Double d, boolean allowNull,  boolean allowNegative){
+	public static boolean checkDouble(Double d, boolean allowNull,  boolean allowNegative){
 		return checkDouble(d,allowNull,allowNegative,false,false);
 	}
 	
-	public static boolean checkDouble(
-			Double d, boolean allowNull, boolean allowNegative, 
-			boolean allowInfinity, boolean allowNaN){
+	public static boolean checkDouble(Double d, boolean allowNull, boolean allowNegative, boolean allowInfinity, 
+			boolean allowNaN){
 		if(d==null) return allowNull;
 		if(Double.isNaN(d)) return allowNaN; 
-		return (allowInfinity 
-					|| !Double.isInfinite(d)) 
-				&& (allowNegative 
-					|| d>=0);
+		return (allowInfinity || !Double.isInfinite(d))	&& (allowNegative || d>=0);
 	}
 	
 
@@ -402,28 +385,30 @@ public class RequestTool {
 		Enumeration<?> headersEnum = request.getHeaderNames();
 		while(headersEnum!=null && headersEnum.hasMoreElements()){
 			String h = (String)headersEnum.nextElement();
-			if(h==null) continue;
+			if(h==null) {
+				continue;
+			}
 			headers.put(h,request.getHeader(h));
 		}
 		return headers;
 	}
 	
-	public static List<String> getCheckedBoxes(
-			HttpServletRequest request, String prefix){
+	public static List<String> getCheckedBoxes(HttpServletRequest request, String prefix){
 		return getCheckedBoxes(request,prefix,new IdentityFunctor<String>());
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static <T> List<T> getCheckedBoxes(
-			HttpServletRequest request, String prefix, Functor<T,String> converter){
+	public static <T> List<T> getCheckedBoxes(HttpServletRequest request, String prefix, Functor<T,String> converter){
 		Enumeration<String> paramNames = request.getParameterNames();
 		List<T> selecteds = ListTool.createArrayList();
 		while(paramNames.hasMoreElements()){
 			String name = paramNames.nextElement();
-			if( ! name.startsWith(prefix)) continue;
-			selecteds.add(
-					converter.invoke(
-							name.substring(prefix.length())));
+			if( ! name.startsWith(prefix)) {
+				continue;
+			}
+			selecteds.add(converter.invoke(
+							name.substring(
+									prefix.length())));
 		}
 		return selecteds;
 	}
@@ -434,14 +419,13 @@ public class RequestTool {
 	 * @param request
 	 * @return redirect uri
 	 */
-	public static String makeRedirectUriIfTrailingSlash(
-			HttpServletRequest request){
-		return makeRedirectUriIfTrailingSlash(
-				request.getRequestURI(), request.getQueryString());
+	public static String makeRedirectUriIfTrailingSlash(HttpServletRequest request){
+		return makeRedirectUriIfTrailingSlash(request.getRequestURI(), request.getQueryString());
 	}
-	private static String makeRedirectUriIfTrailingSlash(
-			String uri, String queryString){
-		if( ! uri.endsWith("/")) return null;
+	private static String makeRedirectUriIfTrailingSlash(String uri, String queryString){
+		if( ! uri.endsWith("/")) {
+			return null;
+		}
 		uri = uri.substring(0, uri.length()-1);
 		return getRequestURIWithQueryString(uri, queryString);
 	}
@@ -451,44 +435,38 @@ public class RequestTool {
 	 * @param request
 	 * @return
 	 */
-	public static String getRequestURIWithQueryString(
-			HttpServletRequest request){
-		return getRequestURIWithQueryString(
-				request.getRequestURI(),request.getQueryString());
+	public static String getRequestURIWithQueryString(HttpServletRequest request){
+		return getRequestURIWithQueryString(request.getRequestURI(),request.getQueryString());
 	}
-	private static String getRequestURIWithQueryString(
-			String uri, String queryString){
+	private static String getRequestURIWithQueryString(String uri, String queryString){
 		return uri + (queryString==null?"":"?"+queryString);
 	}
 	
 	public static String getFileExtension(HttpServletRequest request){
 		String uri = request.getRequestURI();
 		Integer i = uri.lastIndexOf('.');
-		if(i==-1){ return null; }
+		if(i==-1){ 
+			return null;
+		}
 		return uri.substring(i+1, uri.length());
 	}
 	
 
-	public static URL getFullyQualifiedUrl(
-			String path, HttpServletRequest similarRequest){
+	public static URL getFullyQualifiedUrl(String path, HttpServletRequest similarRequest){
 		if(!path.startsWith("/")){
 			path = "/"+path;
 		}
 		String requestUrl = similarRequest.getRequestURL().toString();
-		return getFullyQualifiedUrl(path, requestUrl, 
-				similarRequest.getServerName(), 
-				similarRequest.getServerPort());
+		return getFullyQualifiedUrl(path, requestUrl, similarRequest.getServerName(), similarRequest.getServerPort());
 	}
 	
-	public static URL getFullyQualifiedUrl(
-			String path, String similarUrl, String serverName, int port){
+	public static URL getFullyQualifiedUrl(String path, String similarUrl, String serverName, int port){
 		if(!path.startsWith("/")){
 			path = "/"+path;
 		}
 		String protocol = similarUrl.split(":",2)[0];
 		try{
-			if((port==80 && "http".equals(protocol)) 
-					|| (port==443 && "https".equals(protocol))){
+			if((port==80 && "http".equals(protocol)) || (port==443 && "https".equals(protocol))){
 				return new URL(protocol,serverName,path);
 			}
 			return new URL(protocol,serverName,port,path);
@@ -508,18 +486,12 @@ public class RequestTool {
 			Assert.assertTrue(checkDouble(-0.01,false,true,false,false));
 			Assert.assertFalse(checkDouble(null,false,false,false,false));
 			Assert.assertTrue(checkDouble(null,true,false,false,false));
-			Assert.assertFalse(checkDouble(Double.NEGATIVE_INFINITY,
-											false,false,false,false));
-			Assert.assertFalse(checkDouble(Double.NEGATIVE_INFINITY,
-											false,true,false,false));
-			Assert.assertFalse(checkDouble(Double.NEGATIVE_INFINITY,
-											false,false,true,false));
-			Assert.assertTrue(checkDouble(Double.NEGATIVE_INFINITY,
-											false,true,true,false));
-			Assert.assertFalse(checkDouble(Double.POSITIVE_INFINITY,
-											false,false,false,false));
-			Assert.assertTrue(checkDouble(Double.POSITIVE_INFINITY,
-											false,false,true,false));
+			Assert.assertFalse(checkDouble(Double.NEGATIVE_INFINITY, false,false,false,false));
+			Assert.assertFalse(checkDouble(Double.NEGATIVE_INFINITY, false,true,false,false));
+			Assert.assertFalse(checkDouble(Double.NEGATIVE_INFINITY, false,false,true,false));
+			Assert.assertTrue(checkDouble(Double.NEGATIVE_INFINITY, false,true,true,false));
+			Assert.assertFalse(checkDouble(Double.POSITIVE_INFINITY, false,false,false,false));
+			Assert.assertTrue(checkDouble(Double.POSITIVE_INFINITY, false,false,true,false));
 			Assert.assertFalse(checkDouble(Double.NaN,false,false,false,false));
 			Assert.assertTrue(checkDouble(Double.NaN,false,false,false,true));
 		}
@@ -532,51 +504,31 @@ public class RequestTool {
 		}
 		
 		@Test public void testMakeRedirectUriIfTrailingSlash(){ 
-			Assert.assertEquals(null,
-					makeRedirectUriIfTrailingSlash("example.com",null));
-			Assert.assertEquals("example.com",
-					makeRedirectUriIfTrailingSlash("example.com/",null));
+			Assert.assertEquals(null, makeRedirectUriIfTrailingSlash("example.com",null));
+			Assert.assertEquals("example.com", makeRedirectUriIfTrailingSlash("example.com/",null));
 			Assert.assertEquals("example.com?stuff=things",
-					makeRedirectUriIfTrailingSlash(
-							"example.com/","stuff=things"));
+					makeRedirectUriIfTrailingSlash("example.com/","stuff=things"));
 			Assert.assertEquals(null,
-					makeRedirectUriIfTrailingSlash(
-							"example.com","stuff=things"));
+					makeRedirectUriIfTrailingSlash("example.com","stuff=things"));
 		}
 		
 		@Test public void testGetFullyQualifiedUrl(){
-			Assert.assertEquals(
-					"http://x.com/aurl",
-					getFullyQualifiedUrl(
-							"aurl","http://x.com","x.com",80).toString());
-			Assert.assertEquals(
-					"http://x.com/aurl",
-					getFullyQualifiedUrl(
-							"/aurl","http://x.com","x.com",80).toString());
-			Assert.assertEquals(
-					"https://x.com:80/aurl",
-					getFullyQualifiedUrl(
-							"/aurl","https://x.com","x.com",80).toString());
-			Assert.assertEquals(
-					"https://x.com/aurl",
-					getFullyQualifiedUrl(
-							"/aurl","https://x.com","x.com",443).toString());
-			Assert.assertEquals(
-					"https://x.com/",
-					getFullyQualifiedUrl(
-							"/","https://x.com","x.com",443).toString());
-			Assert.assertEquals(
-					"https://x.com/",
-					getFullyQualifiedUrl(
-							"","https://x.com","x.com",443).toString());
-			Assert.assertEquals(
-					"http://x.com:8080/",
-					getFullyQualifiedUrl(
-							"","http://x.com:8080","x.com",8080).toString());
-			Assert.assertEquals(
-					"https://x.com:8443/snack",
-					getFullyQualifiedUrl(
-							"snack","https://x.com","x.com",8443).toString());
+			Assert.assertEquals("http://x.com/aurl",
+					getFullyQualifiedUrl("aurl","http://x.com","x.com",80).toString());
+			Assert.assertEquals("http://x.com/aurl",
+					getFullyQualifiedUrl("/aurl","http://x.com","x.com",80).toString());
+			Assert.assertEquals("https://x.com:80/aurl",
+					getFullyQualifiedUrl("/aurl","https://x.com","x.com",80).toString());
+			Assert.assertEquals("https://x.com/aurl",
+					getFullyQualifiedUrl("/aurl","https://x.com","x.com",443).toString());
+			Assert.assertEquals("https://x.com/", 
+					getFullyQualifiedUrl("/","https://x.com","x.com",443).toString());
+			Assert.assertEquals("https://x.com/",
+					getFullyQualifiedUrl("","https://x.com","x.com",443).toString());
+			Assert.assertEquals("http://x.com:8080/",
+					getFullyQualifiedUrl("","http://x.com:8080","x.com",8080).toString());
+			Assert.assertEquals("https://x.com:8443/snack",
+					getFullyQualifiedUrl("snack","https://x.com","x.com",8443).toString());
 		}
 	}	
 }
