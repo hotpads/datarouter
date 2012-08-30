4532ac32cf121efc3e17f2b31b88631cc066911c
shsieh <shsieh@hotpads.com>
2012-08-29 17:27:26 -0700

    some blank doublearrayfield methods

--- src/com/hotpads/datarouter/storage/field/imp/array/DoubleArrayField.java
+++ src/com/hotpads/datarouter/storage/field/imp/array/DoubleArrayField.java
@@ -1,8 +1,78 @@
 package com.hotpads.datarouter.storage.field.imp.array;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 
-public class DoubleArrayField {
+import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
+import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SqlColumn;
+import com.hotpads.datarouter.storage.field.BaseListField;
+import com.hotpads.util.core.ArrayTool;
+import com.hotpads.util.core.ListTool;
+import com.hotpads.util.core.bytes.DoubleByteTool;
+import com.hotpads.util.core.bytes.LongByteTool;
+import com.hotpads.util.core.collections.arrays.LongArray;
+import com.hotpads.util.core.exception.NotImplementedException;
+
+
+public class DoubleArrayField extends BaseListField<Double,List<Double>>{
+
+	public DoubleArrayField(String name, List<Double> value){
+		super(name, value);
+		// TODO Auto-generated constructor stub
+	}
 	
+	public DoubleArrayField(String prefix, String name, List<Double> value){
+		super(prefix, name, value);
+	}
 
+	@Override
+	public byte[] getBytes(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Double> fromBytesButDoNotSet(byte[] bytes, int byteOffset){
+		List<Double> doubles = ListTool.create();
+		for(byte b : bytes){
+			doubles.add(DoubleByteTool.fromBytes(bytes, byteOffset));
+		}
+		return doubles;
+	}
+
+	@Override
+	public int numBytesWithSeparator(byte[] bytes, int byteOffset){
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void fromString(String s){
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public SqlColumn getSqlColumnDefinition(){
+		return new SqlColumn(columnName, MySqlColumnType.LONGBLOB, 2147483647 , true);
+	}
+
+	@Override
+	public List<Double> parseJdbcValueButDoNotSet(Object col){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Double> fromJdbcResultSetButDoNotSet(ResultSet rs){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPreparedStatementValue(PreparedStatement ps, int parameterIndex){
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
