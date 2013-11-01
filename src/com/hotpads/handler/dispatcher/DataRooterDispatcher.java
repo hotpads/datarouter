d3970828f8c195e7b365bc3347508d06a666cfdc
sdevineau <sdevineau@hotpads.com>
2013-11-01 16:21:33 -0700

    add "dr project" to every project

---
+++ src/com/hotpads/handler/dispatcher/DataRooterDispatcher.java
@@ -0,0 +1,28 @@
+package com.hotpads.handler.dispatcher;
+
+import com.google.inject.Injector;
+import com.hotpads.handler.BaseDispatcher;
+import com.hotpads.handler.admin.DatarouterRoutersAndClientsHandler;
+import com.hotpads.handler.admin.DrDefaultHandler;
+import com.hotpads.handler.admin.StackTracesManagerHandler;
+import com.hotpads.handler.datarouter.ViewNodeDataHandler;
+
+public class DataRooterDispatcher extends BaseDispatcher{
+
+	private static final String ROUTERS = "/routers";
+	public static final String URL_STACKTRACES = "/stackTraces";
+	public static final String URL_DR = "/dr";
+	public static final String URL_DATAROUTER_VIEW_NODE_DATA = URL_DR + "/viewNodeData";
+
+
+	public DataRooterDispatcher(Injector injector, String servletContextPath, String urlPrefix){
+		super(injector, servletContextPath, urlPrefix);
+		// DataRouter
+		handle(URL_DATAROUTER_VIEW_NODE_DATA, ViewNodeDataHandler.class);
+		handle(URL_DR + URL_STACKTRACES, StackTracesManagerHandler.class);
+		handle(URL_DR + ROUTERS, DatarouterRoutersAndClientsHandler.class);
+		handle(URL_DR + "*", DrDefaultHandler.class);
+
+	}
+
+}
