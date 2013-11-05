0fba9a9a51218fea3fa96e227ed74ea971cdca73
sdevineau <sdevineau@hotpads.com>
2013-11-05 12:07:12 -0800

    modify ExceptionHandlingFilter to onlu use exception.jsp located in datarouter. redirect the request to contextPath/error No need to configure web.xml @codereview

--- src/META-INF/resources/jsp/generic/exception.jsp
+++ src/META-INF/resources/jsp/generic/exception.jsp
@@ -1,27 +1,25 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="ISO-8859-1"%>
+<%@ include file="/jsp/generic/prelude.jspf"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Error processing request</title>
+<c:import url="/jsp/css/css-import.jsp" />
 </head>
 <body>
-	<pre>
-We're sorry, but there was an error processing your request.
-test
-HTTP error code: ${statusCode}
-
+	<div class="container on-top">
+		<div class="alert alert-danger">We're sorry, but there was an error processing your request.</div>
+		<pre>
+<b>HTTP error code:</b> ${statusCode}
 <c:if test="${not empty message}">
 ${message}
 </c:if>
-
 <c:if test="${not empty stackTraceString}">
-stack trace:
-
+<b>stack trace:</b>
 ${stackTraceString}
 </c:if>
-
 </pre>
+	</div>
 </body>
 </html>
--- src/com/hotpads/handler/exception/ExceptionHandlingFilter.java
+++ src/com/hotpads/handler/exception/ExceptionHandlingFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
@@ -18,25 +19,25 @@ import com.hotpads.util.core.ExceptionTool;
 import com.hotpads.util.core.exception.http.HttpException;
 import com.hotpads.util.core.exception.http.imp.Http500InternalServerErrorException;
 
-@Singleton
-public class ExceptionHandlingFilter implements Filter {
+@Singleton public class ExceptionHandlingFilter implements Filter{
 	Logger logger = Logger.getLogger(ExceptionHandlingFilter.class);
 
 	public static final String PARAM_DISPLAY_EXCEPTION_INFO = "displayExceptionInfo";
-	
+
+	private static final String ERROR = "/error";
+
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
+	public void init(FilterConfig arg0) throws ServletException{
 	}
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc)
-			throws IOException, ServletException {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain fc) throws IOException, ServletException{
 		HttpServletRequest request = (HttpServletRequest)req;
 		HttpServletResponse response = (HttpServletResponse)res;
-		
-		try {
+
+		try{
 			fc.doFilter(req, res);
-			
+
 		}catch(Exception e){
 			HttpException httpException;
 			if(e instanceof HttpException){
@@ -45,32 +46,29 @@ public class ExceptionHandlingFilter implements Filter {
 				httpException = new Http500InternalServerErrorException(null, e);
 			}
 			logger.warn(ExceptionTool.getStackTraceAsString(httpException));
-			
-			request.setAttribute("statusCode", httpException.getStatusCode());
-			
-			//something else needs to set this, like an AuthenticationFilter
-			Object displayExceptionInfo = request.getAttribute(PARAM_DISPLAY_EXCEPTION_INFO);
-			if(displayExceptionInfo!=null && ((Boolean)displayExceptionInfo)){
-				String message = httpException.getClass().getSimpleName()+": "+e.getMessage();
-				request.setAttribute("message", message);
-				
-				request.setAttribute("stackTrace", httpException.getStackTrace());
-				request.setAttribute("stackTraceString", 
-						ExceptionTool.getStackTraceStringForHtmlPreBlock(httpException));
-			}
-//			response.sendError(httpException.getStatusCode());
-			
-			//the error pages specified in web.xml will not work without calling this
-//			request.getRequestDispatcher(null).include(request, response);
-			response.sendRedirect(request.getContextPath()+"/error");
-			
-		}finally {
+			HttpSession session = request.getSession();
+			session.setAttribute("statusCode", httpException.getStatusCode());
+
+			// something else needs to set this, like an AuthenticationFilter
+//			Object displayExceptionInfo = request.getAttribute(PARAM_DISPLAY_EXCEPTION_INFO);
+//			if(displayExceptionInfo != null && ((Boolean)displayExceptionInfo)){
+				String message = httpException.getClass().getSimpleName() + ": " + e.getMessage();
+				session.setAttribute("message", message);
+
+				session.setAttribute("stackTrace", httpException.getStackTrace());
+				session.setAttribute("stackTraceString", ExceptionTool
+						.getStackTraceStringForHtmlPreBlock(httpException));
+//			}
+			// RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/generic/exception.jsp");
+			// dispatcher.forward(request, response);
+			response.sendRedirect(request.getContextPath() + ERROR);
+
+		}finally{
 		}
 	}
 
-	
 	@Override
-	public void destroy() {
+	public void destroy(){
 	}
 
 }
