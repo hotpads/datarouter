4c0e7470102587440443d483b22ac0261f8e4cd5
Matt Corgan <mcorgan@hotpads.com>
2014-02-10 19:29:57 -0800

    fix double checked locking

--- src/main/java/com/hotpads/datarouter/client/LazyClientProvider.java
+++ src/main/java/com/hotpads/datarouter/client/LazyClientProvider.java
@@ -2,19 +2,23 @@ package com.hotpads.datarouter.client;
 
 import java.util.concurrent.Callable;
 
-public class LazyClientInitializer
+/*
+ * call a bunch of these in parallel
+ */
+public class LazyClientProvider
 implements Callable<Client>{
 	
 	private ClientFactory clientFactory;
 	private volatile Client client;
 
-	public LazyClientInitializer(ClientFactory clientFactory){
+	public LazyClientProvider(ClientFactory clientFactory){
 		this.clientFactory = clientFactory;
 	}
 
-	
+	//http://en.wikipedia.org/wiki/Double-checked_locking#Usage_in_Java
 	@Override
 	public Client call(){
+		Client result = client;//local variable so we only have to read the volatile once
 		if(client != null){ return client; }//lightweight volatile check
 		synchronized(this){
 			if(client != null){ return client; }
