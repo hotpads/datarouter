54cc03aaeec26c99dabb13ada4812f31fe8a7386
cguillaume <cguillaume@hotpads.com>
2014-03-28 16:19:42 -0700

    remove bad https shits update API call to the new version

--- src/main/java/com/hotpads/handler/exception/ExceptionHandlingFilter.java
+++ src/main/java/com/hotpads/handler/exception/ExceptionHandlingFilter.java
@@ -4,19 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
 import java.util.List;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -30,18 +20,11 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 
@@ -62,12 +45,19 @@ public class ExceptionHandlingFilter implements Filter {
 	private static String serverName;
 
 	public static final String PARAM_DISPLAY_EXCEPTION_INFO = "displayExceptionInfo";
-	public static final String NOTIFICATION_API_ENDPOINT = "https://localhost:8443/job/api/notification";
-	public static final String NOTIFICATION_API_REQUESTS = "requests";
-	public static final String NOTIFICATION_API_USERTOKEN = "usertoken";
-	public static final String NOTIFICATION_API_TIME = "time";
-	public static final String NOTIFICATION_API_TYPE = "type";
-	public static final String NOTIFICATION_API_DATA = "data";
+	public static final String NOTIFICATION_API_ENDPOINT = "http://localhost:8080/job/api/notification";
+	
+	public static final String NOTIFICATION_TYPE = "com.hotpads.notification.type.ServerExceptionNotification";
+	public static final String NOTIFICATION_RECIPENT_TYPE = "EMAIL";
+	public static final String NOTIFICATION_RECIPENT_EMAIL = "cguillaume@hotpads.com";
+	
+	public static final String 
+			NOTIFICATION_API_PARAM_NAME = "requests",
+			NOTIFICATION_API_USER_TYPE = "usertype",
+			NOTIFICATION_API_USER_ID = "userid",
+			NOTIFICATION_API_TIME = "time",
+			NOTIFICATION_API_TYPE = "type",
+			NOTIFICATION_API_DATA = "data";
 
 	// private static final String ERROR = "/error";
 
@@ -129,11 +119,9 @@ public class ExceptionHandlingFilter implements Filter {
 				node.put(exceptionRecord, null);
 
 				// call the notification api
-				String userToken = null;// = ????
-				String type = "com.hotpads.notification.type.ServerExceptionNotification";
 				String data = exceptionRecord.getKey().getId();
 				String time = Long.toString(System.currentTimeMillis());
-				callNotificationApi(userToken, time, type, data);
+				callNotificationApi(time, data);
 
 			} catch (Exception ex) {
 				ex.printStackTrace();
@@ -155,7 +143,6 @@ public class ExceptionHandlingFilter implements Filter {
 					}
 					br.close();
 				}
-				logger.info("Notification request sent");
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
@@ -164,60 +151,27 @@ public class ExceptionHandlingFilter implements Filter {
 		}
 	}
 
-	private void callNotificationApi(String userToken, String time, String type, String data)
-			throws ClientProtocolException, IOException, NoSuchAlgorithmException, KeyManagementException {
-
-		SSLContext sslContext = SSLContext.getInstance("SSL");
-		
-		sslContext.init(null, new TrustManager[] {new X509TrustManager() {
-			
-			@Override
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-			
-			@Override
-			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-				
-			}
-			
-			@Override
-			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-				
-			}
-		}}, new SecureRandom());
-		
-		SSLSocketFactory sf = new SSLSocketFactory(sslContext);
-		Scheme httpsScheme = new Scheme("https", 8443, sf);
-		SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(httpsScheme);
-
-		// apache HttpClient version >4.2 should use BasicClientConnectionManager
-		ClientConnectionManager cm = new SingleClientConnManager(schemeRegistry);
-		
+	private void callNotificationApi(String time, String data) throws IOException {
 		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(NOTIFICATION_API_ENDPOINT);
 		List<NameValuePair> params = ListTool.create();
 
 		JSONArray requests = new JSONArray();
 		JSONObject notification = new JSONObject();
-		notification.put(NOTIFICATION_API_USERTOKEN, userToken);
+		notification.put(NOTIFICATION_API_USER_TYPE, NOTIFICATION_RECIPENT_TYPE);
+		notification.put(NOTIFICATION_API_USER_ID, NOTIFICATION_RECIPENT_EMAIL);
 		notification.put(NOTIFICATION_API_TIME, time);
-		notification.put(NOTIFICATION_API_TYPE, type);
+		notification.put(NOTIFICATION_API_TYPE, NOTIFICATION_TYPE);
 		notification.put(NOTIFICATION_API_DATA, data);
+		requests.add(notification);
 
-		params.add(new BasicNameValuePair(NOTIFICATION_API_REQUESTS, requests.toString()));
+		params.add(new BasicNameValuePair(NOTIFICATION_API_PARAM_NAME, requests.toString()));
 		post.setEntity(new UrlEncodedFormEntity(params));
-		HttpResponse response = client.execute(post);
-		System.out.println(Arrays.toString(response.getAllHeaders()));
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			System.out.println(line);
-		}
+		client.execute(post);
 	}
 
 	@Override
 	public void destroy() {
+
 	}
 }
