4aada44a464e72a2402edfc76283d8e3c1088ac0
cguillaume <cguillaume@hotpads.com>
2014-04-03 13:40:37 -0700

    improvement and fix

--- src/main/java/com/hotpads/handler/exception/NotificationApiCaller.java
+++ src/main/java/com/hotpads/handler/exception/NotificationApiCaller.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -17,30 +18,31 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
 import com.hotpads.util.core.ListTool;
-import com.hotpads.util.core.ObjectTool;
 
+@Singleton
 public class NotificationApiCaller {
 
 	private static Logger logger = Logger.getLogger(NotificationApiCaller.class);
 	
-	@Inject
 	private NotificationApiConfig notificationApiConfig;
 	
 	private HttpClient client;
 
-	public NotificationApiCaller() {
-
+	@Inject
+	public NotificationApiCaller(NotificationApiConfig notificationApiConfig) {
+		this.notificationApiConfig = notificationApiConfig;
 	}
 
-	public NotificationApiCaller(HttpClient client) {
+	public void setClient(HttpClient client) {
 		this.client = client;
 	}
 
 	public void call(String userType, String userId, Long time, String type, String data) throws IOException {
-		if (ObjectTool.anyNull(client))
+		if (client == null)
 			client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(notificationApiConfig.getEndPoint());
 		List<NameValuePair> params = ListTool.create();
@@ -57,7 +59,8 @@ public class NotificationApiCaller {
 		params.add(new BasicNameValuePair(notificationApiConfig.getParamName(), requests.toString()));
 		post.setEntity(new UrlEncodedFormEntity(params));
 		HttpResponse response = client.execute(post);
-
+		EntityUtils.consume(response.getEntity());
+		
 		if (response.getStatusLine().getStatusCode() != 200) {
 			logger.error("Error calling notification API (status code " + response.getStatusLine().getStatusCode() + ")");
 			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
@@ -65,6 +68,8 @@ public class NotificationApiCaller {
 			while ((line = rd.readLine()) != null) {
 				System.out.println(line);
 			}
+			rd.close();
 		}
 	}
+
 }
