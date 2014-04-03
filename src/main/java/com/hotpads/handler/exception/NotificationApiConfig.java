e928a929616e2a518f5d4aa212bd2feda25f6d68
cguillaume <cguillaume@hotpads.com>
2014-04-02 19:29:41 -0700

    create a config instead of CONSTANTS

---
+++ src/main/java/com/hotpads/handler/exception/NotificationApiConfig.java
@@ -0,0 +1,15 @@
+package com.hotpads.handler.exception;
+
+public interface NotificationApiConfig {
+
+	String getEndPoint();
+
+	String getParamName();
+
+	String getUserTypeKey();
+	String getUserIdKey();
+	String getTimeKey();
+	String getTypeKey();
+	String getDataKey();
+
+}
--- src/main/java/com/hotpads/handler/exception/NotificationApiConstants.java
+++ src/main/java/com/hotpads/handler/exception/NotificationApiConstants.java
@@ -1,17 +1,7 @@
 package com.hotpads.handler.exception;
 
 public class NotificationApiConstants {
-	public static final String
-		NOTIFICATION_API_ENDPOINT = "http://localhost:8080/job/api/notification";//TODO valide only for dev ?
-	
-	public static final String 
-		NOTIFICATION_API_PARAM_NAME = "requests",
-		NOTIFICATION_API_USER_TYPE = "usertype",
-		NOTIFICATION_API_USER_ID = "userid",
-		NOTIFICATION_API_TIME = "time",
-		NOTIFICATION_API_TYPE = "type",
-		NOTIFICATION_API_DATA = "data";
-	
+
 	public static final String
 		SERVER_EXCEPTION_NOTIFICATION_TYPE = "com.hotpads.notification.type.ServerExceptionNotification";
 	
