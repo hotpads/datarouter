4dd9390ae3a8d93f4a1ae1a6bf824d9e09ce2987
cguillaume <cguillaume@hotpads.com>
2014-04-10 10:25:27 -0700

    refactor

--- src/main/java/com/hotpads/handler/exception/NotificationApiConstants.java
+++ src/main/java/com/hotpads/handler/exception/NotificationApiConstants.java
@@ -3,7 +3,7 @@ package com.hotpads.handler.exception;
 public class NotificationApiConstants {
 
 	public static final String
-		SERVER_EXCEPTION_NOTIFICATION_TYPE = "com.hotpads.notification.type.ServerExceptionNotification";
+		SERVER_EXCEPTION_NOTIFICATION_TYPE = "com.hotpads.notification.type.ServerExceptionNotificationType";
 	
 	public static final String
 		NOTIFICATION_RECIPENT_TYPE_TOKEN = "TOKEN",
