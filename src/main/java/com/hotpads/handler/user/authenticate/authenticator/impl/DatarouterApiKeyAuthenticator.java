15c3930b66623a2a38a38ac022fed3ef6d12a4bf
Josh Lindsay <jlindsay@hotpads.com>
2014-04-17 13:46:23 -0700

    deleted DatarouterUserDao since it could not be injected into instantiated authenticator objects

--- src/main/java/com/hotpads/handler/user/authenticate/authenticator/impl/DatarouterApiKeyAuthenticator.java
+++ src/main/java/com/hotpads/handler/user/authenticate/authenticator/impl/DatarouterApiKeyAuthenticator.java
@@ -4,22 +4,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.hotpads.handler.user.DatarouterUser;
+import com.hotpads.handler.user.DatarouterUser.DatarouterUserByApiKeyLookup;
 import com.hotpads.handler.user.DatarouterUserNodes;
 import com.hotpads.handler.user.authenticate.authenticator.BaseDatarouterAuthenticator;
 import com.hotpads.handler.user.authenticate.config.DatarouterAuthenticationConfig;
-import com.hotpads.handler.user.dao.DatarouterUserDao;
 import com.hotpads.handler.user.session.DatarouterSession;
+import com.hotpads.util.core.BooleanTool;
+import com.hotpads.util.core.StringTool;
+import com.hotpads.util.core.exception.InvalidApiCallException;
 
 public class DatarouterApiKeyAuthenticator extends BaseDatarouterAuthenticator {
 	
-	private DatarouterUserDao userDao;
 	private DatarouterAuthenticationConfig authenticationConfig;
+	private DatarouterUserNodes userNodes;
 	
 	public DatarouterApiKeyAuthenticator(HttpServletRequest request, HttpServletResponse response,
 			DatarouterAuthenticationConfig authenticationConfig, DatarouterUserNodes userNodes) {
 		super(request, response);
-		userDao = new DatarouterUserDao();
 		this.authenticationConfig = authenticationConfig;
+		this.userNodes = userNodes;
 	}
 	
 	@Override
@@ -28,9 +31,29 @@ public class DatarouterApiKeyAuthenticator extends BaseDatarouterAuthenticator {
 			return null;
 		}
 		String apiKey = request.getParameter(authenticationConfig.getApiKeyParam());
-		DatarouterUser user = userDao.lookupUserByApiKey(apiKey);
+		DatarouterUser user = lookupUserByApiKey(apiKey);
 		DatarouterSession session = DatarouterSession.createFromUser(user);
 		session.setIncludeSessionCookie(false);
 		return session;
+	}
+	
+	private DatarouterUser lookupUserByApiKey(String apiKey) {
+		if (StringTool.isNullOrEmpty(apiKey)) {
+			throw new InvalidApiCallException("no api key specified");
+		}
+
+		DatarouterUser user = userNodes.getUserNode().lookupUnique(new DatarouterUserByApiKeyLookup(apiKey), null);
+
+		if (user == null) {
+			throw new InvalidApiCallException("no user found with provided api key");
+		}
+		if (BooleanTool.isFalseOrNull(user.getEnabled())) {
+			throw new InvalidApiCallException("user is not enabled");
+		}
+		if (BooleanTool.isFalseOrNull(user.getApiEnabled())) {
+			throw new InvalidApiCallException("user does not have api authorization");
+		}
+
+		return user;
 	}
 }
