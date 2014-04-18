6b44bfc06e5149306d7e446ab26291183ff34025
cguillaume <cguillaume@hotpads.com>
2014-04-17 18:14:26 -0700

    add old version and boolean switch

--- src/main/java/com/hotpads/handler/exception/ExceptionHandlingFilter.java
+++ src/main/java/com/hotpads/handler/exception/ExceptionHandlingFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
@@ -24,6 +25,8 @@ import com.hotpads.notification.databean.NotificationUserId;
 import com.hotpads.notification.databean.NotificationUserType;
 import com.hotpads.util.core.ExceptionTool;
 import com.hotpads.util.core.ObjectTool;
+import com.hotpads.util.core.exception.http.HttpException;
+import com.hotpads.util.core.exception.http.imp.Http500InternalServerErrorException;
 
 @Singleton
 public class ExceptionHandlingFilter implements Filter {
@@ -33,6 +36,8 @@ public class ExceptionHandlingFilter implements Filter {
 	private static final String SERVER_EXCEPTION_NOTIFICATION_TYPE = "com.hotpads.notification.type.ServerExceptionNotificationType";
 	private static final String NOTIFICATION_RECIPENT_EMAIL = "cguillaume@hotpads.com"; //TODO only for dev
 	public static final String PARAM_DISPLAY_EXCEPTION_INFO = "displayExceptionInfo";
+	private static final String ERROR = "/error";
+	private static final boolean NOTIFICATION_REPORTING = false;
 
 	private IndexedSortedMapStorageNode<ExceptionRecordKey, ExceptionRecord> exceptionRecordNode;
 	private String serverName;
@@ -44,11 +49,13 @@ public class ExceptionHandlingFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		if (notificationApiClient == null) {//no spring here
-			notificationApiClient = new NotificationApiClient();
+		if (NOTIFICATION_REPORTING) {
+			if (notificationApiClient == null) {//no spring here
+				notificationApiClient = new NotificationApiClient();
+			}
+			pac = new ParallelApiCalling(notificationApiClient);
+			initiateIfNeed(filterConfig.getServletContext());
 		}
-		pac = new ParallelApiCalling(notificationApiClient);
-		initiateIfNeed(filterConfig.getServletContext());
 	}
 
 	@SuppressWarnings("unchecked")
@@ -72,45 +79,72 @@ public class ExceptionHandlingFilter implements Filter {
 		try {
 			fc.doFilter(req, res);
 		} catch (Exception e) {
-			pac.warmupApiClient();//to be sure than the first notificationRequest take less than 1s
 			HttpServletRequest request = (HttpServletRequest) req;
 			HttpServletResponse response = (HttpServletResponse) res;
-			logger.warn(ExceptionTool.getStackTraceAsString(e));
-			try {
-				PrintWriter out = response.getWriter();
-				if (exceptionHandlingConfig.shouldDisplayStackTrace(request, e)) {
-					out.println("<pre>");
-					out.println(ExceptionTool.getStackTraceStringForHtmlPreBlock(e));
-					out.println("</pre>");
-				} else {
-					out.println(exceptionHandlingConfig.getHtmlErrorMessage(e));
-				}
-			} catch (Exception ex) {
-				logger.error("Exception while writing html output");
-				ex.printStackTrace();
-			}
-
-			if (exceptionHandlingConfig.shouldLogException(request, e)) {
-				initiateIfNeed(request.getServletContext());
+			
+			if (NOTIFICATION_REPORTING) {
+				pac.warmupApiClient();//to be sure than the first notificationRequest take less than 1s
+				logger.warn(ExceptionTool.getStackTraceAsString(e));
 				try {
-					ExceptionRecord exceptionRecord = new ExceptionRecord(
-							serverName,
-							ExceptionUtils.getStackTrace(e));
-					exceptionRecordNode.put(exceptionRecord, null);
-					if (exceptionHandlingConfig.shouldRepportError(request, e)) {
-						pac.add(new NotificationRequest(
-								new NotificationUserId(
-										NotificationUserType.EMAIL,
-										NOTIFICATION_RECIPENT_EMAIL),
-								SERVER_EXCEPTION_NOTIFICATION_TYPE,
-								exceptionRecord.getKey().getId(),
-								e.getClass().getName()));
+					PrintWriter out = response.getWriter();
+					if (exceptionHandlingConfig.shouldDisplayStackTrace(request, e)) {
+						out.println("<pre>");
+						out.println(ExceptionTool.getStackTraceStringForHtmlPreBlock(e));
+						out.println("</pre>");
+					} else {
+						out.println(exceptionHandlingConfig.getHtmlErrorMessage(e));
 					}
-
 				} catch (Exception ex) {
-					logger.error("Exception while loging");
+					logger.error("Exception while writing html output");
 					ex.printStackTrace();
 				}
+	
+				if (exceptionHandlingConfig.shouldLogException(request, e)) {
+					initiateIfNeed(request.getServletContext());
+					try {
+						ExceptionRecord exceptionRecord = new ExceptionRecord(
+								serverName,
+								ExceptionUtils.getStackTrace(e));
+						exceptionRecordNode.put(exceptionRecord, null);
+						if (exceptionHandlingConfig.shouldRepportError(request, e)) {
+							pac.add(new NotificationRequest(
+									new NotificationUserId(
+											NotificationUserType.EMAIL,
+											NOTIFICATION_RECIPENT_EMAIL),
+									SERVER_EXCEPTION_NOTIFICATION_TYPE,
+									exceptionRecord.getKey().getId(),
+									e.getClass().getName()));
+						}
+	
+					} catch (Exception ex) {
+						logger.error("Exception while loging");
+						ex.printStackTrace();
+					}
+				}
+			} else {
+				HttpException httpException;
+				if(e instanceof HttpException){
+					httpException = (HttpException)e;
+				}else{
+					httpException = new Http500InternalServerErrorException(null, e);
+				}
+				logger.warn(ExceptionTool.getStackTraceAsString(httpException));
+				HttpSession session = request.getSession();
+				session.setAttribute("statusCode", httpException.getStatusCode());
+
+				// something else needs to set this, like an AuthenticationFilter
+//				Object displayExceptionInfo = request.getAttribute(PARAM_DISPLAY_EXCEPTION_INFO);
+//				if(displayExceptionInfo != null && ((Boolean)displayExceptionInfo)){
+					String message = httpException.getClass().getSimpleName() + ": " + e.getMessage();
+					session.setAttribute("message", message);
+
+					session.setAttribute("stackTrace", httpException.getStackTrace());
+					session.setAttribute("stackTraceString", ExceptionTool
+							.getStackTraceStringForHtmlPreBlock(httpException));
+//				}
+				// RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/generic/exception.jsp");
+				// dispatcher.forward(request, response);
+				response.sendRedirect(request.getContextPath() + ERROR);
 			}
 		}
 	}
