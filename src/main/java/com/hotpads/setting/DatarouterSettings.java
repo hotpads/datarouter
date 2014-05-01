9e0649c253eaef0f26d6c3982d00183aa08b502e
cguillaume <cguillaume@hotpads.com>
2014-05-01 10:05:33 -0700

    add analytics settings

--- src/main/java/com/hotpads/setting/DatarouterSettings.java
+++ src/main/java/com/hotpads/setting/DatarouterSettings.java
@@ -2,9 +2,11 @@ package com.hotpads.setting;
 
 import javax.inject.Inject;
 
+import com.google.inject.Singleton;
 import com.hotpads.setting.cluster.ClusterSettingFinder;
 import com.hotpads.setting.cluster.SettingNode;
 
+@Singleton
 public class DatarouterSettings extends SettingNode {
 
 	@Inject
