99a451b890b6f2d54b3f3e0dfe8a24fa2c24b15d
cguillaume <cguillaume@hotpads.com>
2014-05-01 10:27:04 -0700

    notification and apiendpoint setting

--- src/main/java/com/hotpads/setting/DatarouterSettings.java
+++ src/main/java/com/hotpads/setting/DatarouterSettings.java
@@ -9,9 +9,17 @@ import com.hotpads.setting.cluster.SettingNode;
 @Singleton
 public class DatarouterSettings extends SettingNode {
 
+	private NotificationSettings notificationSettings;
+
 	@Inject
-	public DatarouterSettings(ClusterSettingFinder finder) {
+	public DatarouterSettings(ClusterSettingFinder finder, NotificationSettings notificationSettings) {
 		super(finder, "datarouter.", "");
+		this.notificationSettings = notificationSettings;
+		children.put(notificationSettings.getName(), notificationSettings);
+	}
+
+	public NotificationSettings getNotificationSettings() {
+		return notificationSettings;
 	}
 
 }
---
+++ src/main/java/com/hotpads/setting/NotificationSettings.java
@@ -0,0 +1,28 @@
+package com.hotpads.setting;
+
+import javax.inject.Inject;
+
+import com.google.inject.Singleton;
+import com.hotpads.setting.cached.imp.StringCachedSetting;
+import com.hotpads.setting.cluster.ClusterSettingFinder;
+import com.hotpads.setting.cluster.SettingNode;
+
+@Singleton
+public class NotificationSettings extends SettingNode {
+
+	private Setting<String> apiEndPoint;
+	
+	@Inject
+	public NotificationSettings(ClusterSettingFinder finder) {
+		super(finder, "datarouter.notification.", "datarouter.");
+		regidterSettings();
+	}
+
+	private void regidterSettings() {
+		apiEndPoint = register(new StringCachedSetting(finder, "apiEndPoint", "https://localhost:8443/job/notification"));
+	}
+
+	public Setting<String> getEndPoint() {
+		return apiEndPoint;
+	}
+}
