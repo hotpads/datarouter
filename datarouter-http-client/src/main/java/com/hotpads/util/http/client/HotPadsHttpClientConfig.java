6d5047e144e7f64866abe91fa054d6cd4e555e12
cbonsart <cbonsart@hotpads.com>
2014-07-22 11:21:40 -0700

    Put and Get on Salesforce API

--- datarouter-http-client/src/main/java/com/hotpads/util/http/client/HotPadsHttpClientConfig.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/client/HotPadsHttpClientConfig.java
@@ -1,6 +1,9 @@
 package com.hotpads.util.http.client;
 
+import java.util.List;
+
 public interface HotPadsHttpClientConfig{
 	public String getDtoParameterName();
 	public String getDtoTypeParameterName();
+	public List<Integer> getSuccessStatusCodes();
 }
