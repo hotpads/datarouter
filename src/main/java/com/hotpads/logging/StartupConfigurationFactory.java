984f1ca13b90c73f09282265b5de3c291b254074
cguillaume <cguillaume@hotpads.com>
2014-07-30 17:22:13 -0700

    factory working

--- src/main/java/com/hotpads/logging/StartupConfigurationFactory.java
+++ src/main/java/com/hotpads/logging/StartupConfigurationFactory.java
@@ -5,22 +5,23 @@ import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.apache.logging.log4j.core.config.Order;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
 import org.apache.logging.log4j.core.config.xml.XmlConfigurationFactory;
 
 @Plugin(category = "ConfigurationFactory", name = "StartupConfigurationFactory")
-@Order(10)
+@Order(5)
 public class StartupConfigurationFactory extends XmlConfigurationFactory{
 
 	@Override
 	public String[] getSupportedTypes(){
-		System.out.println("b");
-		return new String[]{".xml"};
+		return new String[]{".hotpads"};
 	}
 
 	@Override
 	public Configuration getConfiguration(ConfigurationSource source){
 		System.out.println("a");
-		return new DefaultConfiguration();
+		Configuration configuration = new DefaultConfiguration();
+		return new XmlConfiguration(source);
 	}
 
 }
