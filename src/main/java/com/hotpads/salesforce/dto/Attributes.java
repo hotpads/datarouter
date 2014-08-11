5b1690c0fb3f5f35d1b2248c86da15d376a99ecf
cbonsart <cbonsart@hotpads.com>
2014-08-11 16:05:36 -0700

    Delete renamed salesforce classses

--- src/main/java/com/hotpads/salesforce/dto/Attributes.java
+++
@@ -1,5 +0,0 @@
-package com.hotpads.salesforce.dto;
-
-public class Attributes{
-	public String url;
-}
--- src/main/java/com/hotpads/salesforce/dto/AuthenticationResponse.java
+++
@@ -1,6 +0,0 @@
-package com.hotpads.salesforce.dto;
-
-public class AuthenticationResponse{
-	public String access_token;
-	public String instance_url;
-}
