4c738fffdab5b84de24a5eda6f8bf97b6380e725
cguillaume <cguillaume@hotpads.com>
2014-08-11 17:45:36 -0700

    add logging in the loggingconfig job

--- src/main/java/com/hotpads/logging/LoggingConfigUpdaterJob.java
+++ src/main/java/com/hotpads/logging/LoggingConfigUpdaterJob.java
@@ -1,10 +1,16 @@
 package com.hotpads.logging;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Singleton
 public class LoggingConfigUpdaterJob implements Runnable{
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Inject
 	private HotPadsLoggingConfigDao hotPadsLoggingConfigDao;
 	@Inject
@@ -23,7 +29,10 @@ public class LoggingConfigUpdaterJob implements Runnable{
 	@Override
 	public void run(){
 		LoggingConfig config = hotPadsLoggingConfigDao.loadConfig();
+		logger.debug("Logging config updater is running");
+		logger.debug(config.getSignature());
 		if(!config.getSignature().equals(previousSignature)){
+			logger.info("Logging config apllied");
 			hotPadsLog4j2Configurator.appConfig(config);
 			previousSignature = config.getSignature();
 		}
