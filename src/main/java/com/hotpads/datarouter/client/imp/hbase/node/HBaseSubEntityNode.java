598a10ca6a7051d00a684fad3b05f582eb3de510
Matt Corgan <mcorgan@hotpads.com>
2014-08-15 17:56:22 -0700

    tweaks

--- src/main/java/com/hotpads/datarouter/client/imp/hbase/node/HBaseSubEntityNode.java
+++ src/main/java/com/hotpads/datarouter/client/imp/hbase/node/HBaseSubEntityNode.java
@@ -12,7 +12,6 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Row;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
-import org.apache.log4j.Logger;
 
 import com.hotpads.datarouter.client.imp.hbase.task.HBaseMultiAttemptTask;
 import com.hotpads.datarouter.client.imp.hbase.task.HBaseTask;
@@ -37,6 +36,7 @@ import com.hotpads.util.core.ByteTool;
 import com.hotpads.util.core.CollectionTool;
 import com.hotpads.util.core.ListTool;
 import com.hotpads.util.core.MapTool;
+import com.hotpads.util.core.profile.PhaseTimer;
 
 public class HBaseSubEntityNode<
 		EK extends EntityKey<EK>,
@@ -78,10 +78,12 @@ implements SubEntitySortedMapStorageNode<EK,PK,D,F>,
 		final Config config = Config.nullSafe(pConfig);
 		new HBaseMultiAttemptTask<Void>(new HBaseTask<Void>(getDataRouterContext(), getTaskNameParams(), "putMulti", config){
 				public Void hbaseCall() throws Exception{
+					PhaseTimer timer = new PhaseTimer();
 					List<Row> actions = ListTool.createArrayList();
 					int numCellsPut = 0, numCellsDeleted = 0;
 					long batchStartTime = System.currentTimeMillis();
 					Map<EK,List<D>> databeansByEntityKey = EntityTool.getDatabeansByEntityKey(databeans);
+					timer.add("group by EK "+MapTool.size(databeansByEntityKey));
 					for(EK ek : databeansByEntityKey.keySet()){
 						byte[] ekBytes = queryBuilder.getRowBytesWithPartition(ek);
 						Put put = new Put(ekBytes);
@@ -121,10 +123,14 @@ implements SubEntitySortedMapStorageNode<EK,PK,D,F>,
 					DRCounters.incSuffixClientNode(client.getType(), "cells delete", getClientName(), getNodeName(), numCellsDeleted);
 					DRCounters.incSuffixClientNode(client.getType(), "databeans put", getClientName(), getNodeName(), numDatabeansPut);
 					DRCounters.incSuffixClientNode(client.getType(), "entities put", getClientName(), getNodeName(), numEntitiesPut);
+					timer.add("built puts "+CollectionTool.size(actions));
 					if(CollectionTool.notEmpty(actions)){
 						hTable.batch(actions);
+						timer.add("batch");
 						hTable.flushCommits();
+						timer.add("flush");
 					}
+					logger.warn(timer.toString());
 					return null;
 				}
 			}).call();
