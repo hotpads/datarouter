95394da1b47ccdf50d69e8441001dd1b06fa682a
Matt Corgan <mcorgan@hotpads.com>
2014-08-19 18:17:50 -0700

    disable BackupIntegrationTests -> Tester

--- src/main/java/com/hotpads/datarouter/test/node/basic/backup/test/BackupIntegrationTester.java
+++ src/main/java/com/hotpads/datarouter/test/node/basic/backup/test/BackupIntegrationTester.java
@@ -43,8 +43,8 @@ import com.hotpads.util.core.SetTool;
 import com.hotpads.util.core.profile.PhaseTimer;
 
 @RunWith(Parameterized.class)
-public class BackupIntegrationTests{
-	static Logger logger = LoggerFactory.getLogger(BackupIntegrationTests.class);
+public class BackupIntegrationTester{
+	static Logger logger = LoggerFactory.getLogger(BackupIntegrationTester.class);
 	
 	/****************************** client types ***********************************/
 
@@ -64,7 +64,7 @@ public class BackupIntegrationTests{
 	
 	@BeforeClass
 	public static void init() throws IOException{	
-		Class<?> cls = BackupIntegrationTests.class;
+		Class<?> cls = BackupIntegrationTester.class;
 
 		if(clientTypes.contains(HibernateClientType.INSTANCE)){
 			routerByClientType.put(
@@ -132,7 +132,7 @@ public class BackupIntegrationTests{
 		return clientTypeObjectArrays;
 	}
 	
-	public BackupIntegrationTests(ClientType clientType){//passed in by junit from the "parameters"
+	public BackupIntegrationTester(ClientType clientType){//passed in by junit from the "parameters"
 		this.clientType = clientType;
 		this.router = routerByClientType.get(clientType);
 	}
