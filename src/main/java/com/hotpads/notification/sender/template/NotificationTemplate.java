4e52a61fe014ed6fcabae8273e5a6525c9fe55be
cguillaume <cguillaume@hotpads.com>
2014-08-25 15:11:25 -0700

    fix counter place on filter + UI

--- src/main/java/com/hotpads/notification/sender/template/NotificationTemplate.java
+++ src/main/java/com/hotpads/notification/sender/template/NotificationTemplate.java
@@ -3,8 +3,9 @@ package com.hotpads.notification.sender.template;
 import java.util.List;
 
 import com.hotpads.notification.databean.NotificationRequest;
+import com.hotpads.notification.sender.NotificationSender;
 
-public interface NotificationTemplate<T> {
+public interface NotificationTemplate<T extends NotificationSender> {
 
 	void setRequests(List<NotificationRequest> requests);
 
