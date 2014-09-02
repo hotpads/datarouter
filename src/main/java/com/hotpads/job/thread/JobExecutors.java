075a5453b534a979aba5cba8011d184dc8db8466
cbonsart <cbonsart@hotpads.com>
2014-09-02 11:46:09 -0700

    Limit reputationJobExecutor

--- src/main/java/com/hotpads/job/thread/JobExecutors.java
+++ src/main/java/com/hotpads/job/thread/JobExecutors.java
@@ -32,8 +32,8 @@ public class JobExecutors{
 		jobExecutor = createScheduled(jobScheduler, "jobExecutor", 10).get();
 	
 	public static final ExecutorService 
-		reputationJobExecutor = new ThreadPoolExecutor(20, 20, 0L, TimeUnit.MILLISECONDS,
-				new ArrayBlockingQueue<Runnable>(2000), new ThreadPoolExecutor.CallerRunsPolicy());
+		reputationJobExecutor = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<Runnable>(10), new ThreadPoolExecutor.CallerRunsPolicy());
 	
 	/************************* providers **********************************/
 	
