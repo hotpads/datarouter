9e7ee562e5862859b799bb45128f2926860bc108
mcorgan <mcorgan@hotpads.com>
2014-11-15 00:35:51 -0800

    injectable MasterSlaveNodeFactory

--- src/main/java/com/hotpads/datarouter/node/factory/MasterSlaveNodeFactory.java
+++ src/main/java/com/hotpads/datarouter/node/factory/MasterSlaveNodeFactory.java
@@ -3,6 +3,9 @@ package com.hotpads.datarouter.node.factory;
 import java.util.Collection;
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import com.hotpads.datarouter.node.BaseNode;
 import com.hotpads.datarouter.node.adapter.IndexedSortedMapStorageAdapterNode;
 import com.hotpads.datarouter.node.adapter.MapStorageAdapterNode;
@@ -18,11 +21,21 @@ import com.hotpads.datarouter.serialize.fielder.DatabeanFielder;
 import com.hotpads.datarouter.storage.StorageType;
 import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
+import com.hotpads.setting.DatarouterSettings;
 import com.hotpads.util.core.CollectionTool;
 import com.hotpads.util.core.ListTool;
 
+@Singleton
 public class MasterSlaveNodeFactory{
+
+	private final DatarouterSettings drSettings;
 	
+	@Inject
+	public MasterSlaveNodeFactory(DatarouterSettings drSettings){
+		this.drSettings = drSettings;
+	}
+
+
 	//no Fielder
 	public static <PK extends PrimaryKey<PK>,D extends Databean<PK,D>>
 	BaseNode<PK,D,?> 
