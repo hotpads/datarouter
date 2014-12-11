7923c83a86f940dd9034191b3f50de1639c8ae86
mcorgan <mcorgan@hotpads.com>
2014-12-11 13:19:05 -0800

    EntityKey javadoc

--- src/main/java/com/hotpads/datarouter/storage/key/entity/EntityKey.java
+++ src/main/java/com/hotpads/datarouter/storage/key/entity/EntityKey.java
@@ -2,6 +2,16 @@ package com.hotpads.datarouter.storage.key.entity;
 
 import com.hotpads.datarouter.storage.key.Key;
 
+/**
+ * 
+ * @author mcorgan
+ * 
+ *         EntityKey defines a unique key for a collection of related databeans. Any databeans in the Entity will have a
+ *         PrimaryKey whose leftmost fields are defined by the EntityKey. The PrimaryKeys are then distinguished by
+ *         their type and any additional fields they add to the EntityKey.
+ * 
+ * @param <K>
+ */
 public interface EntityKey<K extends Key<K>>
 extends Key<K>{
 	
