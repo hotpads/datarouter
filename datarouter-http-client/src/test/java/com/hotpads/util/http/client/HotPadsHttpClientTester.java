7dc35e01539d1b96c910611a4f694fd7ad200bad
Josh Lindsay <jlindsay@hotpads.com>
2014-12-20 20:48:11 -0800

    added signature validator test

--- datarouter-http-client/src/test/java/com/hotpads/util/http/client/HotPadsHttpClientTester.java
+++ datarouter-http-client/src/test/java/com/hotpads/util/http/client/HotPadsHttpClientTester.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
 import junit.framework.Assert;
 
+import org.apache.commons.codec.binary.Base64;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,12 +25,17 @@ import com.hotpads.util.http.response.exception.HotPadsHttpException;
 import com.hotpads.util.http.response.exception.HotPadsHttpRequestExecutionException;
 import com.hotpads.util.http.response.exception.HotPadsHttpRequestTimeoutException;
 import com.hotpads.util.http.response.exception.HotPadsHttpRuntimeException;
+import com.hotpads.util.http.security.CsrfValidator;
+import com.hotpads.util.http.security.DefaultApiKeyPredicate;
+import com.hotpads.util.http.security.SecurityParameters;
+import com.hotpads.util.http.security.SignatureValidator;
 
 public class HotPadsHttpClientTester {
 	private static final int PORT = 9091;
 	private static final String URL = "http://localhost:" + PORT + "/";
 	private static final HotPadsHttpClient CLIENT = new HotPadsHttpClientBuilder().build();
 	private static final int[] STATI = new int[] { 200, 403, 404, 408, 500, 503 };
+	private static final Random RANDOM = new Random(115509410414623L);
 
 	private static final class ServSocket extends Thread {
 		private static ServerSocket server;
@@ -103,53 +112,105 @@ public class HotPadsHttpClientTester {
 	@Test(expected = HotPadsHttpRuntimeException.class)
 	public void testUncheckedException() throws HotPadsHttpException {
 		HotPadsHttpRequest request = new HotPadsHttpRequest(HttpRequestMethod.GET, "invalidLocation", false);
-		executeRequest("unchecked exception - requesting invalid location", request, false);
+		executeRequest(CLIENT, "unchecked exception - requesting invalid location", request, false);
 	}
 
 	@Test(expected = HotPadsHttpRequestExecutionException.class)
 	public void testExecutionException() throws HotPadsHttpException {
 		HotPadsHttpRequest request = new HotPadsHttpRequest(HttpRequestMethod.POST, "invalidLocation", false);
-		executeRequest("unchecked exception from checked call - request invalid location", request, true);
+		executeRequest(CLIENT, "unchecked exception from checked call - request invalid location", request, true);
 	}
 
 	@Test(expected = HotPadsHttpRequestTimeoutException.class)
 	public void testRequestTimeoutException() throws HotPadsHttpException {
 		HotPadsHttpRequest request = new HotPadsHttpRequest(HttpRequestMethod.GET, URL, false).setTimeoutMs(0);
-		executeRequest("http request timeout", request, true);
+		executeRequest(CLIENT, "http request timeout", request, true);
 	}
 
 	@Test(expected = HotPadsHttpConnectionAbortedException.class)
 	public void testHttpConnectionAbortedException() throws HotPadsHttpException {
 		HotPadsHttpRequest request = new HotPadsHttpRequest(HttpRequestMethod.GET, "localhost:" + PORT, false);
-		executeRequest("connection aborted exception - invalid host URI", request, true);
+		executeRequest(CLIENT, "connection aborted exception - invalid host URI", request, true);
 	}
 
 	@Test(expected = HotPadsHttpConnectionAbortedException.class)
 	public void testInvalidRequestHeader() throws HotPadsHttpException {
-		SERVER.setResponse(301, "the 301 status code will throw an exception when not provided a location header");
+		SERVER.setResponse(301, "301 status code throws exception when not provided a location header");
 		HotPadsHttpRequest request = new HotPadsHttpRequest(HttpRequestMethod.GET, URL, false);
-		executeRequest("connection aborted exception - invalid headers", request, true);
+		executeRequest(CLIENT, "connection aborted exception - invalid headers", request, true);
 	}
 
 	@Test
 	public void testSuccessfulRequest() throws HotPadsHttpException {
-		int status = STATI[new Random().nextInt(STATI.length)];
-		String response = UUID.randomUUID().toString();
-		SERVER.setResponse(status, response);
+		int status = STATI[RANDOM.nextInt(STATI.length)];
+		String expectedResponse = UUID.randomUUID().toString();
+		SERVER.setResponse(status, expectedResponse);
 
 		HotPadsHttpRequest request = new HotPadsHttpRequest(HttpRequestMethod.GET, URL, false);
-		HotPadsHttpResponse httpResponse = executeRequest("TEST - successful request", request, true);
-		System.out.println("status " + httpResponse.getStatusCode());
-		System.out.println("entity " + httpResponse.getEntity());
-		Assert.assertEquals(response, httpResponse.getEntity());
-		Assert.assertEquals(status, httpResponse.getStatusCode());
+		HotPadsHttpResponse response = executeRequest(CLIENT, "successful request", request, true);
+		Assert.assertEquals(expectedResponse, response.getEntity());
+		Assert.assertEquals(status, response.getStatusCode());
 	}
 
-	private HotPadsHttpResponse executeRequest(String message, HotPadsHttpRequest request, boolean checked)
-			throws HotPadsHttpException {
+	@Test
+	public void testClientSignatureValidator() throws HotPadsHttpException {
+		String salt = "some super secure salty salt " + UUID.randomUUID().toString();
+		SignatureValidator signatureValidator = new SignatureValidator(salt);
+		HotPadsHttpClient client = new HotPadsHttpClientBuilder().setSignatureValidator(signatureValidator).build();
+		HotPadsHttpRequest request;
+		HotPadsHttpResponse response;
+
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("1", UUID.randomUUID().toString());
+		params.put("2", Integer.toString(RANDOM.nextInt()));
+		params.put("3", "Everything is awesome! Everything is cool when you're part of a team!");
+		params.put("4", "$*&^)}\r\n\\\t~\" drop table User");
+		params.put("5", Base64.encodeBase64String(signatureValidator.sign(params)));
+
+		int status = STATI[RANDOM.nextInt(STATI.length)];
+		String expectedResponse = Arrays.toString(params.entrySet().toArray());
+		SERVER.setResponse(status, expectedResponse);
+
+		// GET request cannot be signed since it has no entity/payload
+		request = new HotPadsHttpRequest(HttpRequestMethod.GET, URL, false).addPostParams(params);
+		response = executeRequest(client, "signature validator with GET request", request, true);
+		Assert.assertEquals(expectedResponse, response.getEntity());
+		Assert.assertEquals(params.size(), request.getPostParams().size());
+
+		request = new HotPadsHttpRequest(HttpRequestMethod.POST, URL, false).addPostParams(params);
+		response = executeRequest(client, "signature validator with POST request", request, true);
+
+		String expectedSignature = Base64.encodeBase64String(signatureValidator.sign(params));
+		Map<String, String> postParams = request.getPostParams();
+		String signature = postParams.get(SecurityParameters.SIGNATURE);
+		Assert.assertEquals(expectedResponse, response.getEntity());
+		Assert.assertEquals(params.size() + 1, postParams.size());
+		Assert.assertNotNull(signature);
+		Assert.assertEquals(expectedSignature, signature);
+	}
+
+	@Test
+	public void testClientCsrfValidator() throws HotPadsHttpException {
+		String cipherKey = "cipherKey";
+		String cipherIv = "cipherIv";
+		CsrfValidator csrfValidator = new CsrfValidator(cipherKey, cipherIv);
+		HotPadsHttpClient client = new HotPadsHttpClientBuilder().setCsrfValidator(csrfValidator).build();
+	}
+
+	@Test
+	public void testClientApiKeyPredicate() throws HotPadsHttpException {
+		DefaultApiKeyPredicate apiKeyPredicate = new DefaultApiKeyPredicate("apiKey");
+		HotPadsHttpClient client = new HotPadsHttpClientBuilder().setApiKeyPredicate(apiKeyPredicate).build();
+	}
+
+	private HotPadsHttpResponse executeRequest(HotPadsHttpClient client, String message, HotPadsHttpRequest request,
+			boolean checked) throws HotPadsHttpException {
 		System.out.println("\n" + message);
 		try {
-			return checked ? CLIENT.executeChecked(request) : CLIENT.execute(request);
+			HotPadsHttpResponse response = checked ? client.executeChecked(request) : CLIENT.execute(request);
+			System.out.println("status " + response.getStatusCode());
+			System.out.println("entity " + response.getEntity());
+			return response;
 		} catch (HotPadsHttpException | HotPadsHttpRuntimeException e) {
 			e.printStackTrace();
 			throw e;
