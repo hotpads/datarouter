a5f8940f337bae7acd75119a940b3db137484986
smenon <smenon@hotpads.com>
2015-01-15 15:04:21 -0800

    created a class DatarouterUserService, that creates a admin user

---
+++ src/main/java/com/hotpads/handler/user/session/DataRouterUserService.java
@@ -0,0 +1,57 @@
+package com.hotpads.handler.user.session;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hotpads.datarouter.node.op.combo.IndexedSortedMapStorage.IndexedSortedMapStorageNode;
+import com.hotpads.handler.user.DatarouterUser;
+import com.hotpads.handler.user.DatarouterUserKey;
+import com.hotpads.handler.user.DatarouterUserNodes;
+import com.hotpads.handler.user.authenticate.DatarouterPasswordService;
+import com.hotpads.handler.user.authenticate.DatarouterTokenGenerator;
+import com.hotpads.handler.user.role.DatarouterUserRole;
+
+public class DataRouterUserService{
+	private static final Logger logger = LoggerFactory.getLogger(DataRouterUserService.class);
+	
+	private static final String ADMIN_USERNAME =  "admin@hotpads.com";
+	private static final String RAW_PW = "tempAdminPassword";
+	
+	//injected fields	
+	private DatarouterUserNodes userNodes;
+	private DatarouterPasswordService passwordService;
+	
+	@Inject
+	public DataRouterUserService(DatarouterUserNodes userNodes, DatarouterPasswordService passwordService){
+		this.userNodes = userNodes;
+		this.passwordService = passwordService;
+	}
+
+	
+	public void createAdminUser(long admin_id, Collection<DatarouterUserRole> roles){
+		String salt = passwordService.generateSaltForNewUser();		
+		String digest = passwordService.digest(salt, RAW_PW);		
+		DatarouterUser user = new DatarouterUser();
+		user.setCreated(new Date());
+		user.setUsername(ADMIN_USERNAME);
+		user.setEnabled(true);
+		user.setId(admin_id);
+		user.setLastLoggedIn(new Date());
+		user.setPasswordDigest(digest);
+		user.setPasswordSalt(salt);
+		user.setRoles(roles);
+		user.setUserToken(DatarouterTokenGenerator.generateRandomToken());
+		IndexedSortedMapStorageNode<DatarouterUserKey, DatarouterUser> nodes = userNodes.getUserNode();
+		System.out.println("dskjfkds "+nodes);
+		userNodes.getUserNode().put(user, null);
+		logger.warn("Created default admin user account");
+	
+		
+	}
+
+}
