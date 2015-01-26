c0b28b9d176053aabb77726c097e0c1454098265
smenon <smenon@hotpads.com>
2015-01-26 11:29:38 -0800

    renamed raw_pw to default_password

--- src/main/java/com/hotpads/handler/user/session/DatarouterUserService.java
+++ src/main/java/com/hotpads/handler/user/session/DatarouterUserService.java
@@ -18,8 +18,8 @@ public class DatarouterUserService{
 	private static final Logger logger = LoggerFactory.getLogger(DatarouterUserService.class);
 	
 	private static final String ADMIN_USERNAME =  "admin@hotpads.com";
-	private static final String RAW_PW = "tempAdminPassword";
-	private static final long ADMIN_ID = 1L;
+	private static final String DEFAULT_PASSWORD = "tempAdminPassword";
+	public static final long ADMIN_ID = 1L;
 	
 	//injected fields	
 	private DatarouterUserNodes userNodes;
@@ -33,7 +33,7 @@ public class DatarouterUserService{
 	
 	public void createAdminUser(Collection<DatarouterUserRole> roles){		
 		String salt = passwordService.generateSaltForNewUser();		
-		String digest = passwordService.digest(salt, RAW_PW);		
+		String digest = passwordService.digest(salt, DEFAULT_PASSWORD);		
 		DatarouterUser user = new DatarouterUser();
 		user.setCreated(new Date());
 		user.setUsername(ADMIN_USERNAME);
