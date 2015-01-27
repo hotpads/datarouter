54ac7fcf0dcdb4e8044060aa371bf0c60fa0431d
cbonsart <cbonsart@hotpads.com>
2015-01-27 13:57:31 -0800

    Shutdown executors when server shuts down

--- src/main/java/com/hotpads/guice/GuiceDatarouterLoader.java
+++ src/main/java/com/hotpads/guice/GuiceDatarouterLoader.java
@@ -1,10 +1,17 @@
 package com.hotpads.guice;
 
-import javax.servlet.ServletContext;
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutorService;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+import com.google.inject.Binding;
 import com.google.inject.Injector;
+import com.google.inject.Key;
 import com.hotpads.DatarouterInjector;
 import com.hotpads.DatarouterLoader;
+import com.hotpads.util.core.java.ReflectionTool;
 
 public class GuiceDatarouterLoader extends DatarouterLoader{
 
@@ -18,6 +25,19 @@ public class GuiceDatarouterLoader extends DatarouterLoader{
 	@Override
 	protected DatarouterInjector getInjector(){
 		return injector.getInstance(DatarouterInjector.class);
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent event){
+		super.contextDestroyed(event);
+		
+		for(Entry<Key<?>, Binding<?>> bindingEntry : injector.getAllBindings().entrySet()){
+			Class<?> bindedType = bindingEntry.getKey().getTypeLiteral().getRawType();
+			if (ReflectionTool.getAllSuperClassesAndInterfaces(bindedType).contains(ExecutorService.class)){
+				ExecutorService executor = (ExecutorService) bindingEntry.getValue().getProvider().get();
+				executor.shutdownNow();
+			}
+		}
 	}
 
 }
