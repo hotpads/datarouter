465da1541e411b24f2e96da95b13f844fd6b8601
cbonsart <cbonsart@hotpads.com>
2015-01-27 15:43:21 -0800

    Factorize code that shutdown executors

--- src/main/java/com/hotpads/GuiceInjector.java
+++ src/main/java/com/hotpads/GuiceInjector.java
@@ -1,9 +1,12 @@
 package com.hotpads;
 
+import java.util.Collection;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import com.google.inject.Injector;
+import com.hotpads.guice.GuiceTool;
 
 @Singleton
 public class GuiceInjector implements DatarouterInjector{
@@ -18,6 +21,11 @@ public class GuiceInjector implements DatarouterInjector{
 	@Override
 	public <T>T getInstance(Class<? extends T> clazz){
 		return injector.getInstance(clazz);
+	}
+
+	@Override
+	public <T> Collection<T> getInstancesOfType(Class<T> type){
+		return GuiceTool.getInstancesOfType(injector, type);
 	}
 
 }
--- src/main/java/com/hotpads/guice/GuiceDatarouterLoader.java
+++ src/main/java/com/hotpads/guice/GuiceDatarouterLoader.java
@@ -1,17 +1,10 @@
 package com.hotpads.guice;
 
-import java.util.Map.Entry;
-import java.util.concurrent.ExecutorService;
-
 import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
 
-import com.google.inject.Binding;
 import com.google.inject.Injector;
-import com.google.inject.Key;
 import com.hotpads.DatarouterInjector;
 import com.hotpads.DatarouterLoader;
-import com.hotpads.util.core.java.ReflectionTool;
 
 public class GuiceDatarouterLoader extends DatarouterLoader{
 
@@ -25,19 +18,6 @@ public class GuiceDatarouterLoader extends DatarouterLoader{
 	@Override
 	protected DatarouterInjector getInjector(){
 		return injector.getInstance(DatarouterInjector.class);
-	}
-	
-	@Override
-	public void contextDestroyed(ServletContextEvent event){
-		super.contextDestroyed(event);
-		
-		for(Entry<Key<?>, Binding<?>> bindingEntry : injector.getAllBindings().entrySet()){
-			Class<?> bindedType = bindingEntry.getKey().getTypeLiteral().getRawType();
-			if (ReflectionTool.getAllSuperClassesAndInterfaces(bindedType).contains(ExecutorService.class)){
-				ExecutorService executor = (ExecutorService) bindingEntry.getValue().getProvider().get();
-				executor.shutdownNow();
-			}
-		}
 	}
 
 }
