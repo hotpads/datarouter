d580bdd46ce74121181398333e4b41056fe2e732
mcorgan <mcorgan@hotpads.com>
2015-02-22 15:55:39 -0800

    enable scattering prefix

--- src/main/java/com/hotpads/trace/key/TraceEntityKey.java
+++ src/main/java/com/hotpads/trace/key/TraceEntityKey.java
@@ -6,7 +6,7 @@ import com.hotpads.datarouter.storage.field.Field;
 import com.hotpads.datarouter.storage.field.FieldTool;
 import com.hotpads.datarouter.storage.field.imp.comparable.LongField;
 import com.hotpads.datarouter.storage.key.entity.base.BaseEntityKey;
-import com.hotpads.datarouter.storage.key.entity.base.NoOpEntityPartitioner;
+import com.hotpads.datarouter.storage.key.entity.base.BaseEntityPartitioner;
 
 @SuppressWarnings("serial")
 public class TraceEntityKey 
@@ -28,20 +28,20 @@ extends BaseEntityKey<TraceEntityKey>{
 				new LongField(Fields.traceId, traceId));
 	}
 
-	public static class TraceEntityPartitioner
-	extends NoOpEntityPartitioner<TraceEntityKey>{
-	}
-	
-//	public static class TraceEntityPartitioner implements EntityPartitioner<TraceEntityKey>{
-//		@Override
-//		public int getNumPartitions(){
-//			return 1;
-//		}
-//		@Override
-//		public int getPartition(TraceEntityKey ek){
-//			return (int)HashMethods.longDJBHash(ek.getTraceId().toString());
-//		}
+//	public static class TraceEntityPartitioner
+//	extends NoOpEntityPartitioner<TraceEntityKey>{
 //	}
+	
+	public static class TraceEntityPartitioner extends BaseEntityPartitioner<TraceEntityKey>{
+		@Override
+		public int getNumPartitions(){
+			return 16;
+		}
+		@Override
+		public int getPartition(TraceEntityKey ek){
+			return (int)(ek.getTraceId() % getNumPartitions());
+		}
+	}
 
 	
 	/****************** construct *******************/
