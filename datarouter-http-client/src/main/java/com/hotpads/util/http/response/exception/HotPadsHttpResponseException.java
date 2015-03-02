338bfea508409e75b8b2db19faf2370f581943e7
Josh Lindsay <jlindsay@hotpads.com>
2015-03-02 14:30:45 -0800

    added getter for exception

--- datarouter-http-client/src/main/java/com/hotpads/util/http/response/exception/HotPadsHttpResponseException.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/response/exception/HotPadsHttpResponseException.java
@@ -5,7 +5,13 @@ import com.hotpads.util.http.response.HotPadsHttpResponse;
 @SuppressWarnings("serial")
 public class HotPadsHttpResponseException extends HotPadsHttpException {
 
+	private HotPadsHttpResponse response;
+	
 	public HotPadsHttpResponseException(HotPadsHttpResponse response) {
 		super("Request returned with status code " + response.getStatusCode(), null);
+	}
+	
+	public HotPadsHttpResponse getResponse() {
+		return response;
 	}
 }
