2dca8f7e34604d7ae8fc9bf6940811ce1a09bc75
Matt Corgan <mcorgan@hotpads.com>
2015-03-02 12:23:40 -0800

    re-enable and modify tests to use scan/scanKeys

--- src/main/java/com/hotpads/datarouter/test/node/basic/sorted/BaseSortedNodeIntegrationTests.java
+++ src/main/java/com/hotpads/datarouter/test/node/basic/sorted/BaseSortedNodeIntegrationTests.java
@@ -3,7 +3,6 @@ package com.hotpads.datarouter.test.node.basic.sorted;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -181,53 +180,59 @@ public abstract class BaseSortedNodeIntegrationTests{
 		Assert.assertTrue(DrListTool.isSorted(result));
 	}
 	
-//	@Test
-//	public void testGetKeysInRange(){
-//		SortedBeanKey alp1 = new SortedBeanKey(SortedBeans.RANGE_alp, null, null, null);
-//		SortedBeanKey emu1 = new SortedBeanKey(SortedBeans.RANGE_emu, null, null, null);
-//		List<SortedBeanKey> result1 = sortedNode.getKeysInRange(alp1, true, emu1, true, null);
-//		int expectedSize1 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS 
-//				* SortedBeans.NUM_ELEMENTS;
-//		Assert.assertEquals(expectedSize1, DrCollectionTool.size(result1));
-//		Assert.assertTrue(DrListTool.isSorted(result1));
-//		
-//		List<SortedBeanKey> result1b = sortedNode.getKeysInRange(alp1, true, emu1, false, null);
-//		int expectedSize1b = (SortedBeans.RANGE_LENGTH_alp_emu_inc - 1) * SortedBeans.NUM_ELEMENTS 
-//				* SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
-//		Assert.assertEquals(expectedSize1b, DrCollectionTool.size(result1b));
-//		Assert.assertTrue(DrListTool.isSorted(result1b));
-//		
-//		SortedBeanKey alp2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_alp, null, null);
-//		SortedBeanKey emu2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_emu, null, null);
-//		List<SortedBeanKey> result2 = sortedNode.getKeysInRange(alp2, true, emu2, true, null);
-//		int expectedSize2 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
-//		Assert.assertEquals(expectedSize2, DrCollectionTool.size(result2));
-//		Assert.assertTrue(DrListTool.isSorted(result2));
-//	}
-//	
-//	@Test
-//	public void testGetInRange(){
-//		SortedBeanKey alp1 = new SortedBeanKey(SortedBeans.RANGE_alp, null, null, null);
-//		SortedBeanKey emu1 = new SortedBeanKey(SortedBeans.RANGE_emu, null, null, null);
-//		List<SortedBean> result1 = sortedNode.getRange(alp1, true, emu1, true, null);
-//		int expectedSize1 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS 
-//				* SortedBeans.NUM_ELEMENTS;
-//		Assert.assertEquals(expectedSize1, DrCollectionTool.size(result1));
-//		Assert.assertTrue(DrListTool.isSorted(result1));
-//		
-//		List<SortedBean> result1b = sortedNode.getRange(alp1, true, emu1, false, null);
-//		int expectedSize1b = (SortedBeans.RANGE_LENGTH_alp_emu_inc-1) * SortedBeans.NUM_ELEMENTS 
-//				* SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
-//		Assert.assertEquals(expectedSize1b, DrCollectionTool.size(result1b));
-//		Assert.assertTrue(DrListTool.isSorted(result1b));
-//		
-//		SortedBeanKey alp2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_alp, null, null);
-//		SortedBeanKey emu2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_emu, null, null);
-//		List<SortedBean> result2 = sortedNode.getRange(alp2, true, emu2, true, null);
-//		int expectedSize2 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
-//		Assert.assertEquals(expectedSize2, DrCollectionTool.size(result2));
-//		Assert.assertTrue(DrListTool.isSorted(result2));
-//	}
+	@Test
+	public void testGetKeysInRange(){
+		SortedBeanKey alp1 = new SortedBeanKey(SortedBeans.RANGE_alp, null, null, null);
+		SortedBeanKey emu1 = new SortedBeanKey(SortedBeans.RANGE_emu, null, null, null);
+		Range<SortedBeanKey> range1 = new Range<>(alp1, true, emu1, true);
+		List<SortedBeanKey> result1 = DrListTool.createArrayList(sortedNode.scanKeys(range1, null));
+		int expectedSize1 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS 
+				* SortedBeans.NUM_ELEMENTS;
+		Assert.assertEquals(expectedSize1, DrCollectionTool.size(result1));
+		Assert.assertTrue(DrListTool.isSorted(result1));
+
+		Range<SortedBeanKey> range1b = new Range<>(alp1, true, emu1, false);
+		List<SortedBeanKey> result1b = DrListTool.createArrayList(sortedNode.scanKeys(range1b, null));
+		int expectedSize1b = (SortedBeans.RANGE_LENGTH_alp_emu_inc - 1) * SortedBeans.NUM_ELEMENTS 
+				* SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
+		Assert.assertEquals(expectedSize1b, DrCollectionTool.size(result1b));
+		Assert.assertTrue(DrListTool.isSorted(result1b));
+		
+		SortedBeanKey alp2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_alp, null, null);
+		SortedBeanKey emu2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_emu, null, null);
+		Range<SortedBeanKey> range2 = new Range<>(alp2, true, emu2, true);
+		List<SortedBeanKey> result2 = DrListTool.createArrayList(sortedNode.scanKeys(range2, null));
+		int expectedSize2 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
+		Assert.assertEquals(expectedSize2, DrCollectionTool.size(result2));
+		Assert.assertTrue(DrListTool.isSorted(result2));
+	}
+	
+	@Test
+	public void testGetInRange(){
+		SortedBeanKey alp1 = new SortedBeanKey(SortedBeans.RANGE_alp, null, null, null);
+		SortedBeanKey emu1 = new SortedBeanKey(SortedBeans.RANGE_emu, null, null, null);
+		Range<SortedBeanKey> range1 = new Range<>(alp1, true, emu1, true);
+		List<SortedBean> result1 = DrListTool.createArrayList(sortedNode.scan(range1, null));
+		int expectedSize1 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS 
+				* SortedBeans.NUM_ELEMENTS;
+		Assert.assertEquals(expectedSize1, DrCollectionTool.size(result1));
+		Assert.assertTrue(DrListTool.isSorted(result1));
+		
+		Range<SortedBeanKey> range1b = new Range<>(alp1, true, emu1, false);
+		List<SortedBean> result1b = DrListTool.createArrayList(sortedNode.scan(range1b, null));
+		int expectedSize1b = (SortedBeans.RANGE_LENGTH_alp_emu_inc-1) * SortedBeans.NUM_ELEMENTS 
+				* SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
+		Assert.assertEquals(expectedSize1b, DrCollectionTool.size(result1b));
+		Assert.assertTrue(DrListTool.isSorted(result1b));
+		
+		SortedBeanKey alp2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_alp, null, null);
+		SortedBeanKey emu2 = new SortedBeanKey(SortedBeans.STRINGS.first(), SortedBeans.RANGE_emu, null, null);
+		Range<SortedBeanKey> range2 = new Range<>(alp2, true, emu2, true);
+		List<SortedBean> result2 = DrListTool.createArrayList(sortedNode.scan(range2, null));
+		int expectedSize2 = SortedBeans.RANGE_LENGTH_alp_emu_inc * SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
+		Assert.assertEquals(expectedSize2, DrCollectionTool.size(result2));
+		Assert.assertTrue(DrListTool.isSorted(result2));
+	}
 	
 	@Test //small batch sizes to make sure we're resuming each batch from the correct spot
 	public void testIncrementalScan(){
