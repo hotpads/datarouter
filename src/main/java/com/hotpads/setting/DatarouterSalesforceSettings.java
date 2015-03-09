d40b53850afd525ef20576f38e21d17fe5c3c2d5
jdea <jdea@hotpads.com>
2015-03-09 16:37:37 -0700

    add cluster settings for proxy server use

--- src/main/java/com/hotpads/setting/DatarouterSalesforceSettings.java
+++ src/main/java/com/hotpads/setting/DatarouterSalesforceSettings.java
@@ -3,6 +3,8 @@ package com.hotpads.setting;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.hotpads.setting.cached.imp.BooleanCachedSetting;
+import com.hotpads.setting.cached.imp.IntegerCachedSetting;
 import com.hotpads.setting.cached.imp.StringCachedSetting;
 import com.hotpads.setting.cluster.ClusterSettingFinder;
 import com.hotpads.setting.cluster.SettingNode;
@@ -10,11 +12,17 @@ import com.hotpads.setting.cluster.SettingNode;
 @Singleton
 public class DatarouterSalesforceSettings extends SettingNode{
 
+	private static final int DEFAULT_SQUID_PROXY_PORT = 3128;
+	
 	private StringCachedSetting clientId;
 	private StringCachedSetting clientSecret;
 	private StringCachedSetting username;
 	private StringCachedSetting password;
 	private StringCachedSetting loginEndpoint;
+
+	private StringCachedSetting proxyHost;
+	private IntegerCachedSetting proxyPort;	
+	private BooleanCachedSetting useProxy;
 
 	@Inject
 	public DatarouterSalesforceSettings(ClusterSettingFinder finder){
@@ -28,6 +36,9 @@ public class DatarouterSalesforceSettings extends SettingNode{
 		username = registerString("username", "");
 		password = registerString("password", "");
 		loginEndpoint = registerString("loginEndpoint", "https://test.salesforce.com/services/oauth2/token");
+		useProxy = registerBoolean("useProxy", false);
+		proxyHost= registerString("proxyHost", "");
+		proxyPort= registerInteger("proxyHostPort", DEFAULT_SQUID_PROXY_PORT);		
 	}
 
 	public StringCachedSetting getClientId(){
@@ -48,6 +59,18 @@ public class DatarouterSalesforceSettings extends SettingNode{
 
 	public StringCachedSetting getLoginEndpoint(){
 		return loginEndpoint;
+	}
+
+	public StringCachedSetting getProxyHost() {
+		return proxyHost;
+	}
+
+	public IntegerCachedSetting getProxyPort() {
+		return proxyPort;
+	}
+	
+	public BooleanCachedSetting getUseProxy() {
+		return useProxy;
 	}
 
 }
