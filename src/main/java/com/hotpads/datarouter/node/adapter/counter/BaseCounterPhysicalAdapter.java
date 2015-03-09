c458a359e6f97c7cd9b563bc16465ea7e4f92884
mcorgan <mcorgan@hotpads.com>
2015-03-08 18:07:41 -0700

    BaseCounterAdapters

---
+++ src/main/java/com/hotpads/datarouter/node/adapter/counter/BaseCounterPhysicalAdapter.java
@@ -0,0 +1,85 @@
+package com.hotpads.datarouter.node.adapter.counter;
+
+import java.util.List;
+
+import com.hotpads.datarouter.client.Client;
+import com.hotpads.datarouter.node.NodeParams;
+import com.hotpads.datarouter.node.type.index.ManagedNode;
+import com.hotpads.datarouter.node.type.physical.PhysicalNode;
+import com.hotpads.datarouter.serialize.fielder.DatabeanFielder;
+import com.hotpads.datarouter.storage.databean.Databean;
+import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
+import com.hotpads.datarouter.storage.view.index.IndexEntry;
+import com.hotpads.datarouter.util.DRCounters;
+
+public /*abstract*/ class BaseCounterPhysicalAdapter<
+		PK extends PrimaryKey<PK>,
+		D extends Databean<PK,D>,
+		F extends DatabeanFielder<PK,D>,
+		N extends PhysicalNode<PK,D>>
+extends BaseCounterAdapter<PK,D,F,N>
+implements PhysicalNode<PK,D>{
+	
+	protected final N backingNode;
+	
+	public BaseCounterPhysicalAdapter(NodeParams<PK,D,F> params, N backingNode){
+		super(params, backingNode);
+		this.backingNode = backingNode;
+	}
+
+	
+	/******************** PhysicalNode *************************/
+
+	@Override
+	public String getClientName(){
+		return backingNode.getClientName();
+	}
+
+
+	@Override
+	public Client getClient(){
+		return backingNode.getClient();
+	}
+
+
+	@Override
+	public String getTableName(){
+		return backingNode.getTableName();
+	}
+
+
+	@Override
+	public String getPackagedTableName(){
+		return backingNode.getPackagedTableName();
+	}
+
+
+	@Override
+	public <IK extends PrimaryKey<IK>,
+			IE extends IndexEntry<IK,IE,PK,D>,
+			IF extends DatabeanFielder<IK,IE>,
+			MN extends ManagedNode<IK,IE,IF>>
+	MN registerManaged(MN managedNode){
+		return backingNode.registerManaged(managedNode);
+	}
+
+
+	@Override
+	public <IK extends PrimaryKey<IK>,
+			IE extends IndexEntry<IK,IE,PK,D>,
+			IF extends DatabeanFielder<IK,IE>>
+	List<ManagedNode<IK,IE,IF>> getManagedNodes(){
+		return backingNode.getManagedNodes();
+	}
+	
+	
+	/******************* counter ************************/
+
+	//override to count PhysicalNode stuff
+	@Override
+	public void count(String key, long delta){
+		DRCounters.incSuffixClientNode(backingNode.getClient().getType(), key, backingNode.getClientName(), 
+				backingNode.getName(), delta);
+	}
+	
+}
