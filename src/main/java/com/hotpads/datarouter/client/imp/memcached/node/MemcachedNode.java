0a56fadd1e8e30e66e3554cc31f0f4667d982639
mcorgan <mcorgan@hotpads.com>
2015-03-08 20:47:50 -0700

    delete custom memcached counters

--- src/main/java/com/hotpads/datarouter/client/imp/memcached/node/MemcachedNode.java
+++ src/main/java/com/hotpads/datarouter/client/imp/memcached/node/MemcachedNode.java
@@ -14,7 +14,6 @@ import com.hotpads.datarouter.serialize.fielder.DatabeanFielder;
 import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.databean.DatabeanTool;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
-import com.hotpads.datarouter.util.DRCounters;
 import com.hotpads.datarouter.util.core.DrCollectionTool;
 import com.hotpads.datarouter.util.core.DrListTool;
 import com.hotpads.trace.TraceContext;
@@ -81,9 +80,6 @@ implements PhysicalMapStorageNode<PK,D>
 				logger.error("memached error on " + key,e);
 			}
 		}
-		String opName = "putMulti";
-		DRCounters.incClientNodeCustom(getClient().getType(), opName, getClientName(), getName());
-		DRCounters.incClientNodeCustom(getClient().getType(), opName+" objects", getClientName(), getName(), databeans.size());
 		TraceContext.appendToSpanInfo(DrCollectionTool.size(databeans)+"");
 	}
 	
@@ -111,9 +107,6 @@ implements PhysicalMapStorageNode<PK,D>
 			}
 		}
 		TraceContext.appendToSpanInfo(DrCollectionTool.size(keys)+"");
-		String opName = "deleteMulti";
-		DRCounters.incClientNodeCustom(getClient().getType(), opName, getClientName(), getName());
-		DRCounters.incClientNodeCustom(getClient().getType(), opName+" objects", getClientName(), getName(), keys.size());
 	}
 	
 	
