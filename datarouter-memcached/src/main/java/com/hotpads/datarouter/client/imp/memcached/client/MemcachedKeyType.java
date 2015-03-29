9dd83e0e253bc84f862271caee00d6bdcc489098
mcorgan <mcorgan@hotpads.com>
2015-03-29 13:36:04 -0700

    remove unused memcached key types

--- datarouter-memcached/src/main/java/com/hotpads/datarouter/client/imp/memcached/client/MemcachedKeyType.java
+++ datarouter-memcached/src/main/java/com/hotpads/datarouter/client/imp/memcached/client/MemcachedKeyType.java
@@ -4,20 +4,13 @@ import com.hotpads.util.core.enums.HpEnum;
 
 public enum MemcachedKeyType implements HpEnum{
 	HIB(0,"hib","Hibernate"), 
-	GEO(1,"geo","Geocoder"),
 	HIB_QUERY(2, "hibQuery", "Hibernate Query"), //HIB_QUERY and HIB_TIMESTAMP are for hibernate query cacheing
 	HIB_TIMESTAMP(3, "hibTimestamp", "Hibernate Timestamp"),
-	SESSION(4, "session", "Session"),
-	COORDINATES(5, "coordinates", "Area Coordinates"),
-	USER_REAL_ESTATE_LISTING_IDS_BY_TYPE(6, "userRealEstateListingIdsByType", "User Real Estate Listing Ids by Type"),
-	WIKIPEDIA(7, "wikipedia", "Wikipedia Articles"),
 	RATE_LIMITER(8, "rate_limiter", "Rate limiter"),
 	USER_LISTING_RECORDS_BY_TYPE(9, "userListingRecordsByType", "User Listing Records by Type"), 
 	USER_SEARCH(10, "userSearch", "User Search"),
-	EXPERIMENT(11, "experiment", "Experiment"),
 	ROBOT_DETECTOR(12, "robotDetector", "Robot Detector"),
 	EXCEPTIONAL_IP(13,"exceptionalIP","Exceptional IP"),
-	AREABOUNDARIES(14, "areaBoundaries", "Area Boundaries")
 	;
 	
 	private Integer value;
