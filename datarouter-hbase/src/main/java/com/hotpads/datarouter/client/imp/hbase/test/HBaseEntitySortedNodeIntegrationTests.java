4079a771745c5978a4299a3e86e43b2cf4a7d169
mcorgan <mcorgan@hotpads.com>
2015-04-03 00:02:16 -0700

    remove accidental @Test annotation

--- datarouter-hbase/src/main/java/com/hotpads/datarouter/client/imp/hbase/test/HBaseEntitySortedNodeIntegrationTests.java
+++ datarouter-hbase/src/main/java/com/hotpads/datarouter/client/imp/hbase/test/HBaseEntitySortedNodeIntegrationTests.java
@@ -1,12 +1,16 @@
 package com.hotpads.datarouter.client.imp.hbase.test;
 
+import java.util.List;
+
 import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.hotpads.datarouter.node.entity.EntityNode;
 import com.hotpads.datarouter.test.DRTestConstants;
 import com.hotpads.datarouter.test.node.basic.sorted.BaseSortedNodeIntegrationTests;
+import com.hotpads.datarouter.test.node.basic.sorted.SortedBean;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanEntity;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanEntityKey;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeans;
@@ -31,10 +35,12 @@ public class HBaseEntitySortedNodeIntegrationTests extends BaseSortedNodeIntegra
 	@Test
 	public void testGetEntity(){
 		SortedBeanEntityKey ek1 = new SortedBeanEntityKey(SortedBeans.S_albatross, SortedBeans.S_ostrich);
-		SortedBeanEntity albatrossOstrich = entityNode.entity().getEntity(ek1, null);
+		EntityNode<SortedBeanEntityKey,SortedBeanEntity> entityNode = sortedBeanEntityNode.entity();
+		SortedBeanEntity albatrossOstrich = entityNode.getEntity(ek1, null);
 		int numExpected = SortedBeans.NUM_ELEMENTS * SortedBeans.NUM_ELEMENTS;
-		AssertJUnit.assertEquals(numExpected, albatrossOstrich.getSortedBeans().size());
-		AssertJUnit.assertEquals(SortedBeans.S_albatross, DrCollectionTool.getFirst(albatrossOstrich.getSortedBeans()).getA());
-		AssertJUnit.assertEquals(SortedBeans.S_ostrich, DrCollectionTool.getFirst(albatrossOstrich.getSortedBeans()).getB());
+		List<SortedBean> results = albatrossOstrich.getSortedBeans();
+		AssertJUnit.assertEquals(numExpected, results.size());
+		AssertJUnit.assertEquals(SortedBeans.S_albatross, DrCollectionTool.getFirst(results).getA());
+		AssertJUnit.assertEquals(SortedBeans.S_ostrich, DrCollectionTool.getFirst(results).getB());
 	}
 }
