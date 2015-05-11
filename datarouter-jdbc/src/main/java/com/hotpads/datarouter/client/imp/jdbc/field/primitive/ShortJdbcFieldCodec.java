037b3a5eebb563d3c0efdb15e9dc50699279590c
mcorgan <mcorgan@hotpads.com>
2015-05-11 01:30:11 -0700

    migrage primitive fields to jdbc codecs

---
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/field/primitive/ShortJdbcFieldCodec.java
@@ -0,0 +1,60 @@
+package com.hotpads.datarouter.client.imp.jdbc.field.primitive;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
+import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SqlColumn;
+import com.hotpads.datarouter.client.imp.jdbc.field.base.BasePrimitiveJdbcFieldCodec;
+import com.hotpads.datarouter.exception.DataAccessException;
+import com.hotpads.datarouter.storage.field.imp.comparable.ShortField;
+
+public class ShortJdbcFieldCodec
+extends BasePrimitiveJdbcFieldCodec<Short,ShortField>{
+	
+	public ShortJdbcFieldCodec(ShortField field){
+		super(field);
+	}
+
+	@Override
+	public Class<ShortField> getFieldType(){
+		return ShortField.class;
+	}
+
+
+
+	@Override
+	public SqlColumn getSqlColumnDefinition(){
+		return new SqlColumn(field.getColumnName(), MySqlColumnType.SMALLINT, 5, field.getNullable(), false);
+	}
+	
+	@Override
+	public Short parseJdbcValueButDoNotSet(Object obj){
+		return obj==null?null:(Short)obj;
+	}
+	
+	@Override
+	public void setPreparedStatementValue(PreparedStatement ps, int parameterIndex){
+		try{
+			if(field.getValue()==null){
+				ps.setNull(parameterIndex, Types.SMALLINT);
+			}else{
+				ps.setShort(parameterIndex, field.getValue());
+			}
+		}catch(SQLException e){
+			throw new DataAccessException(e);
+		}
+	}
+	
+	@Override
+	public Short fromJdbcResultSetButDoNotSet(ResultSet rs){
+		try{
+			short value = rs.getShort(field.getColumnName());
+			return rs.wasNull()?null:value;
+		}catch(SQLException e){
+			throw new DataAccessException(e);
+		}
+	}
+}
---
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/field/primitive/SignedByteJdbcFieldCodec.java
@@ -0,0 +1,60 @@
+package com.hotpads.datarouter.client.imp.jdbc.field.primitive;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
+import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SqlColumn;
+import com.hotpads.datarouter.client.imp.jdbc.field.base.BasePrimitiveJdbcFieldCodec;
+import com.hotpads.datarouter.exception.DataAccessException;
+import com.hotpads.datarouter.storage.field.imp.comparable.SignedByteField;
+
+public class SignedByteJdbcFieldCodec
+extends BasePrimitiveJdbcFieldCodec<Byte,SignedByteField>{
+	
+	public SignedByteJdbcFieldCodec(SignedByteField field){
+		super(field);
+	}
+
+	@Override
+	public Class<SignedByteField> getFieldType(){
+		return SignedByteField.class;
+	}
+
+	
+
+	@Override
+	public SqlColumn getSqlColumnDefinition(){
+		return new SqlColumn(field.getColumnName(), MySqlColumnType.TINYINT, 1 , field.getNullable(), false);
+	}
+	
+	@Override
+	public Byte parseJdbcValueButDoNotSet(Object obj){
+		return obj==null?null:(Byte)obj;
+	}
+	
+	@Override
+	public void setPreparedStatementValue(PreparedStatement ps, int parameterIndex){
+		try{
+			if(field.getValue()==null){
+				ps.setNull(parameterIndex, Types.TINYINT);
+			}else{
+				ps.setByte(parameterIndex, field.getValue());
+			}
+		}catch(SQLException e){
+			throw new DataAccessException(e);
+		}
+	}
+	
+	@Override
+	public Byte fromJdbcResultSetButDoNotSet(ResultSet rs){
+		try{
+			byte value = rs.getByte(field.getColumnName());
+			return rs.wasNull()?null:value;
+		}catch(SQLException e){
+			throw new DataAccessException(e);
+		}
+	}
+}
