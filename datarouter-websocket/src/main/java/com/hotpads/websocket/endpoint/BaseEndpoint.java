f16b25387767f2a9be8c625a25d9bf4920e14cb1
cguillaume <cguillaume@hotpads.com>
2015-05-12 19:24:55 -0700

    log and report websocket errors

--- datarouter-websocket/src/main/java/com/hotpads/websocket/endpoint/BaseEndpoint.java
+++ datarouter-websocket/src/main/java/com/hotpads/websocket/endpoint/BaseEndpoint.java
@@ -10,6 +10,7 @@ import javax.websocket.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hotpads.handler.exception.ExceptionRecorder;
 import com.hotpads.websocket.ServerAddressProvider;
 import com.hotpads.websocket.WebSocketConnectionStore;
 import com.hotpads.websocket.auth.WebSocketAuthenticationFilter;
@@ -25,6 +26,8 @@ public abstract class BaseEndpoint extends Endpoint{
 	private ServerAddressProvider serverAddressProvider;
 	@Inject
 	private WebSocketConnectionStore webSocketConnectionStore;
+	@Inject
+	private ExceptionRecorder exceptionRecorder;
 
 	private WebSocketSession webSocketSession;
 
@@ -46,6 +49,12 @@ public abstract class BaseEndpoint extends Endpoint{
 	public void onClose(Session session, CloseReason closeReason){
 		logger.info("Closing websocket session {} because {}", webSocketSession, closeReason);
 		pushService.unregister(webSocketSession.getKey());
+	}
+
+	@Override
+	public void onError(Session session, Throwable thr){
+		logger.error("Error on websocket session {}", webSocketSession, thr);
+		exceptionRecorder.tryRecordException((Exception)thr, getClass().getName());
 	}
 
 }
