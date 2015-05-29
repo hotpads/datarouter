1119fc4039e06fa45637b5e516f284a9588fc527
yfang <yfang@hotpads.com>
2015-05-28 17:02:06 -0700

    clean up.

---
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/security/UrlScheme.java
@@ -0,0 +1,42 @@
+package com.hotpads.util.http.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum UrlScheme {
+	HTTP("http"), 
+	HTTPS("https"),
+	ANY("any");
+	
+	public static final String LOCAL_HOST = "localhost";
+	public static final String DOMAIN_NAME = "hotpads.com";
+	
+	public static final int 
+	PORT_HTTP_STANDARD = 80,
+	PORT_HTTPS_STANDARD = 443,
+	PORT_HTTP_DEV = 8080,
+	PORT_HTTPS_DEV = 8443;
+	
+	public static final String LOCAL_DEV_SERVER = LOCAL_HOST + ":" + PORT_HTTP_DEV;
+	public static final String LOCAL_DEV_SERVER_HTTPS = LOCAL_HOST + ":" + PORT_HTTPS_DEV;
+	public static final String LOCAL_DEV_SERVER_URL = HTTP.stringRepresentation +"://" + LOCAL_HOST + ":" + PORT_HTTP_DEV;
+	public static final String LOCAL_DEV_SERVER_HTTPS_URL = HTTPS.stringRepresentation + "://" + LOCAL_HOST + ":" + PORT_HTTPS_DEV;
+	
+	private final String stringRepresentation;
+	
+	private UrlScheme(String stringRepresentation){
+		this.stringRepresentation = stringRepresentation;
+	}
+
+	public String getStringRepresentation() {
+		return stringRepresentation;
+	}
+
+	
+	protected static Set<Integer> STANDARD_PORTS = new HashSet<Integer>();
+	static{
+		STANDARD_PORTS.add(PORT_HTTP_STANDARD);
+		STANDARD_PORTS.add(PORT_HTTPS_STANDARD);
+	}
+	
+}
