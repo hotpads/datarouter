34c367a8b34691e3a11cb084063b6b559a86c811
yfang <yfang@hotpads.com>
2015-05-28 17:33:29 -0700

    add tab.

--- datarouter-http-client/src/main/java/com/hotpads/util/http/security/UrlScheme.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/security/UrlScheme.java
@@ -12,10 +12,10 @@ public enum UrlScheme {
 	public static final String DOMAIN_NAME = "hotpads.com";
 	
 	public static final int 
-	PORT_HTTP_STANDARD = 80,
-	PORT_HTTPS_STANDARD = 443,
-	PORT_HTTP_DEV = 8080,
-	PORT_HTTPS_DEV = 8443;
+		PORT_HTTP_STANDARD = 80,
+		PORT_HTTPS_STANDARD = 443,
+		PORT_HTTP_DEV = 8080,
+		PORT_HTTPS_DEV = 8443;
 	
 	public static final String LOCAL_DEV_SERVER = LOCAL_HOST + ":" + PORT_HTTP_DEV;
 	public static final String LOCAL_DEV_SERVER_HTTPS = LOCAL_HOST + ":" + PORT_HTTPS_DEV;
