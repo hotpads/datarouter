ce9885fd2f6717fdedfc6641dba46c781ed79cfa
cbonsart <cbonsart@hotpads.com>
2015-06-02 05:58:02 -0700

    add explicit binding module to sqs modules

--- datarouter-sqs/src/main/java/com/hotpads/datarouter/client/imp/sqs/config/SqsGuiceModule.java
+++ datarouter-sqs/src/main/java/com/hotpads/datarouter/client/imp/sqs/config/SqsGuiceModule.java
@@ -1,6 +1,7 @@
 package com.hotpads.datarouter.client.imp.sqs.config;
 
 import com.google.inject.AbstractModule;
+import com.hotpads.datarouter.client.imp.sqs.SqsClientType;
 import com.hotpads.datarouter.client.imp.sqs.encode.JsonSqsEncoder;
 import com.hotpads.datarouter.client.imp.sqs.encode.SqsEncoder;
 
@@ -9,6 +10,6 @@ public class SqsGuiceModule extends AbstractModule{
 	@Override
 	protected void configure(){
 		bind(SqsEncoder.class).to(JsonSqsEncoder.class);
+		bind(SqsClientType.class);
 	}
-
 }
