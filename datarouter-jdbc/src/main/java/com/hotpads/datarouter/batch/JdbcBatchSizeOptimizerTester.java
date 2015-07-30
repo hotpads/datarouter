0c3d1f4db5349fe6d123aedb450afcb6192f7f19
cbonsart <cbonsart@hotpads.com>
2015-07-29 17:00:27 -0700

    remove nodeName from opperformancekey

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/batch/JdbcBatchSizeOptimizerTester.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/batch/JdbcBatchSizeOptimizerTester.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 
 import javax.inject.Inject;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Guice;
@@ -23,10 +22,8 @@ import com.hotpads.util.core.iterable.BatchingIterable;
 import com.hotpads.util.core.profile.PhaseTimer;
 
 @Guice(moduleFactory=TestDatarouterJdbcModuleFactory.class)
-public class JdbcBatchSizeOptimizerIntegrationTests{
+public class JdbcBatchSizeOptimizerTester{
 	
-	private static final int DEFAULT_BATCH_SIZE = BatchSizeOptimizer.DEFAULT_BATCH_SIZE;
-	private static final double DEFAULT_CURIOSITY = BatchSizeOptimizer.DEFAULT_CURIOSITY;
 	private static final Random random = new Random();
 	
 	@Inject
@@ -43,35 +40,25 @@ public class JdbcBatchSizeOptimizerIntegrationTests{
 		((WriteBehindSortedMapStorageNode<?,?,?>) nodes.getOpPerformanceRecordNode()).flush();
 	}
 	
-	@Test
-	public void testDefaultGetBatchSize(){
-		Assert.assertTrue(batchSizeOptimizer.getRecommendedBatchSize("", "", 
-				2 * DEFAULT_BATCH_SIZE) <= (1 + DEFAULT_CURIOSITY) * DEFAULT_BATCH_SIZE);
-		Assert.assertTrue(batchSizeOptimizer.getRecommendedBatchSize("", "", 
-				2 * DEFAULT_BATCH_SIZE) >= (1 - DEFAULT_CURIOSITY) * DEFAULT_BATCH_SIZE);
-	}
-	
 	@Test(enabled = false)
 	public void testRecordOpPerformance(){
-		String nodeName = nodes.getOpPerformanceRecordNode().getName();
 		String opName = "";
 		int targetOptimal = 210;
 		int rowCount = 10000;
-		while(batchSizeOptimizer.getOptimalBatchSize(nodeName, opName) != targetOptimal){
+		while(batchSizeOptimizer.getOptimalBatchSize(opName) != targetOptimal){
 			for(int count = 0 ; count < 1000 ; count++){
-				int batchSize = batchSizeOptimizer.getRecommendedBatchSize(nodeName, opName, Integer.MAX_VALUE);
+				int batchSize = batchSizeOptimizer.getRecommendedBatchSize(opName, Integer.MAX_VALUE);
 				int timeSpent = random.nextInt(10 * (1 + Math.abs(batchSize - targetOptimal)));
-				batchSizeOptimizer.recordBatchSizeAndTime(nodeName, opName, batchSize, rowCount, timeSpent);
+				batchSizeOptimizer.recordBatchSizeAndTime(opName, batchSize, rowCount, timeSpent);
 			}
 			((WriteBehindSortedMapStorageNode<?,?,?>) nodes.getOpPerformanceRecordNode()).flush();
-			batchSizeOptimizer.computeAndCacheRecommendedBatchSize(nodeName, opName);
-			System.out.println(batchSizeOptimizer.getOptimalBatchSize(nodeName, opName));
+			batchSizeOptimizer.computeAndCacheRecommendedBatchSize(opName);
+			System.out.println(batchSizeOptimizer.getOptimalBatchSize(opName));
 		}
 	}
 	
 	@Test(enabled=false)
 	public void testWithGetMulti(){
-		String nodeName = router.testDatabean.getName();
 		String opName = "getMulti";
 		List<TestDatabean> databeans = new ArrayList<>();
 		for(int i = 0 ; i < 10000 ; i++){
@@ -82,24 +69,23 @@ public class JdbcBatchSizeOptimizerIntegrationTests{
 		//warm up
 		router.testDatabean.getMulti(keys, 
 				new Config().setIterateBatchSize(
-						batchSizeOptimizer.getRecommendedBatchSize(nodeName, opName, keys.size())));
+						batchSizeOptimizer.getRecommendedBatchSize(opName, keys.size())));
 		for(int j = 0 ; j < 100 ; j++){
 			for(int i = 0 ; i < 20 ; i ++){
-				int batchSize = batchSizeOptimizer.getRecommendedBatchSize(nodeName, opName, keys.size());
+				int batchSize = batchSizeOptimizer.getRecommendedBatchSize(opName, keys.size());
 				long start = System.currentTimeMillis();
 				router.testDatabean.getMulti(keys, new Config().setIterateBatchSize(batchSize));
 				long timeSpent = System.currentTimeMillis() - start;
-				batchSizeOptimizer.recordBatchSizeAndTime(nodeName, opName, batchSize, keys.size(), timeSpent);
+				batchSizeOptimizer.recordBatchSizeAndTime(opName, batchSize, keys.size(), timeSpent);
 			}
-			batchSizeOptimizer.computeAndCacheRecommendedBatchSize(nodeName, opName);
-			System.out.println(batchSizeOptimizer.getOptimalBatchSize(nodeName, opName));
+			batchSizeOptimizer.computeAndCacheRecommendedBatchSize(opName);
+			System.out.println(batchSizeOptimizer.getOptimalBatchSize(opName));
 		}
 		
 	}
 	
-	@Test
+	@Test(enabled=false)
 	public void testWithPutMulti(){
-		String nodeName = router.testDatabean.getName();
 		String opName = "putMulti";
 		for(int j = 0 ; j < 100 ; j++){
 			PhaseTimer timer = new PhaseTimer();
@@ -108,19 +94,19 @@ public class JdbcBatchSizeOptimizerIntegrationTests{
 				for(int k = 0 ; k < 2000 ; k++){
 					databeans.add(new TestDatabean(randomString(), randomString(), randomString()));
 				}
-				int batchSize = batchSizeOptimizer.getRecommendedBatchSize(nodeName, opName, databeans.size());
+				int batchSize = batchSizeOptimizer.getRecommendedBatchSize(opName, databeans.size());
 				long start = System.currentTimeMillis();
 				for(List<TestDatabean> databeanBatch : new BatchingIterable<>(databeans, batchSize)){
 					router.testDatabean.putMulti(databeanBatch, null);
 				}
 				long timeSpent = System.currentTimeMillis() - start;
-				batchSizeOptimizer.recordBatchSizeAndTime(nodeName, opName, batchSize, databeans.size(), timeSpent);
+				batchSizeOptimizer.recordBatchSizeAndTime(opName, batchSize, databeans.size(), timeSpent);
 			}
 			timer.add("record");
-			batchSizeOptimizer.computeAndCacheRecommendedBatchSize(nodeName, opName);
+			batchSizeOptimizer.computeAndCacheRecommendedBatchSize(opName);
 			timer.add("analyse");
-			System.out.println(batchSizeOptimizer.getOptimalBatchSize(nodeName, opName) + " "
-					+ batchSizeOptimizer.getCuriosity(nodeName, opName));
+			System.out.println(batchSizeOptimizer.getOptimalBatchSize(opName) + " "
+					+ batchSizeOptimizer.getCuriosity(opName));
 			System.out.println(timer.toString());
 		}
 	}
