10edbf192995fe3e0e57ee599168cc5fea277fc5
cbonsart <cbonsart@hotpads.com>
2015-08-18 06:48:34 -0700

    Factorize ManyFieldBean and ManyFieldTypeBean2

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/test/FieldSqlTableGeneratorTester.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/test/FieldSqlTableGeneratorTester.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
+import com.hotpads.datarouter.client.imp.jdbc.TestDatarouterJdbcModuleFactory;
 import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SchemaUpdateOptions;
 import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SqlTable;
 import com.hotpads.datarouter.client.imp.jdbc.ddl.generate.SqlAlterTableGenerator;
@@ -15,18 +16,17 @@ import com.hotpads.datarouter.client.imp.jdbc.ddl.generate.SqlCreateTableGenerat
 import com.hotpads.datarouter.client.imp.jdbc.ddl.generate.imp.FieldSqlTableGenerator;
 import com.hotpads.datarouter.client.imp.jdbc.field.codec.factory.JdbcFieldCodecFactory;
 import com.hotpads.datarouter.storage.field.Field;
-import com.hotpads.datarouter.test.DatarouterTestModuleFactory;
 import com.hotpads.datarouter.test.node.basic.manyfield.ManyFieldBean;
 import com.hotpads.datarouter.test.node.basic.manyfield.ManyFieldBean.ManyFieldTypeBeanFielder;
 import com.hotpads.datarouter.test.node.basic.manyfield.ManyFieldTypeBean2;
 import com.hotpads.datarouter.test.node.basic.manyfield.ManyFieldTypeBean2.ManyFieldTypeBean2Fielder;
 
-@Guice(moduleFactory = DatarouterTestModuleFactory.class)
+@Guice(moduleFactory = TestDatarouterJdbcModuleFactory.class)
 public class FieldSqlTableGeneratorTester{
 	@Inject
 	private JdbcFieldCodecFactory fieldCodecFactory;
 
-	@Test 
+	@Test
 	public void testGenerate(){
 		String tableName = "ManyFieldTypeBean";
 		List<Field<?>> primaryKeyFields = new ArrayList<>();
@@ -39,15 +39,15 @@ public class FieldSqlTableGeneratorTester{
 		ManyFieldTypeBean2 mftBean2 = new ManyFieldTypeBean2();
 		ManyFieldTypeBeanFielder mftFielder = new ManyFieldTypeBeanFielder();
 		ManyFieldTypeBean2Fielder mftFielder2 = new ManyFieldTypeBean2Fielder();
-		
+
 		primaryKeyFields = mftBean.getKeyFields();
 		nonKeyFields = mftFielder.getNonKeyFields(mftBean);
-		FieldSqlTableGenerator fstGenerator = new FieldSqlTableGenerator(fieldCodecFactory, tableName, primaryKeyFields, 
+		FieldSqlTableGenerator fstGenerator = new FieldSqlTableGenerator(fieldCodecFactory, tableName, primaryKeyFields,
 				nonKeyFields);
 		SqlTable table = fstGenerator.generate();
 		SqlCreateTableGenerator ctGenerator = new SqlCreateTableGenerator(table);
 		System.out.println(ctGenerator.generateDdl());
-		
+
 		primaryKeyFields2 = mftBean2.getKeyFields();
 		nonKeyFields2 = mftFielder2.getNonKeyFields(mftBean2);
 		FieldSqlTableGenerator fstGenerator2 = new FieldSqlTableGenerator(fieldCodecFactory, tableName + "2",
