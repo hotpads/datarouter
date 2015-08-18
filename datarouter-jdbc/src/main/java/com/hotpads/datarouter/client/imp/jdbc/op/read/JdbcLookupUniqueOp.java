427c0c9b24ff93758d7ca3470d87f7a9361f73f2
mattc <mattc@hotpads.com>
2015-08-18 10:45:10 -0700

    remove orderBy from JdbcLookup; other cleanup

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/op/read/JdbcLookupUniqueOp.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/op/read/JdbcLookupUniqueOp.java
@@ -2,7 +2,6 @@ package com.hotpads.datarouter.client.imp.jdbc.op.read;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import com.hotpads.datarouter.client.imp.hibernate.util.JdbcTool;
@@ -16,7 +15,6 @@ import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 import com.hotpads.datarouter.storage.key.unique.UniqueKey;
 import com.hotpads.datarouter.util.core.DrCollectionTool;
-import com.hotpads.datarouter.util.core.DrListTool;
 
 public class JdbcLookupUniqueOp<
 		PK extends PrimaryKey<PK>,
@@ -41,10 +39,8 @@ extends BaseJdbcOp<List<D>>{
 	@Override
 	public List<D> runOnce(){
 		if(DrCollectionTool.isEmpty(uniqueKeys)){
-			return new LinkedList<>();
+			return Collections.emptyList();
 		}
-		List<? extends UniqueKey<PK>> sortedKeys = DrListTool.createArrayList(uniqueKeys);
-		Collections.sort(sortedKeys);
 		String sql = SqlBuilder.getMulti(fieldCodecFactory, config, node.getTableName(), node.getFieldInfo()
 				.getFields(), uniqueKeys);
 		List<D> result = JdbcTool.selectDatabeans(fieldCodecFactory, getConnection(node.getClientId().getName()), node
