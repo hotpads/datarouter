9a6ecdce3a95d9904acb1b90dad25180ea4cb627
cbonsart <cbonsart@hotpads.com>
2015-08-19 06:21:40 -0700

    abstract the byte fields codecs to a base codec

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/field/codec/positive/UInt7JdbcFieldCodec.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/field/codec/positive/UInt7JdbcFieldCodec.java
@@ -1,60 +1,27 @@
 package com.hotpads.datarouter.client.imp.jdbc.field.codec.positive;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Types;
-
 import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
-import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SqlColumn;
-import com.hotpads.datarouter.client.imp.jdbc.field.codec.base.BasePrimitiveJdbcFieldCodec;
-import com.hotpads.datarouter.exception.DataAccessException;
+import com.hotpads.datarouter.client.imp.jdbc.field.codec.primitive.BaseByteJdbcFieldCodec;
 import com.hotpads.datarouter.storage.field.imp.positive.UInt7Field;
 
-public class UInt7JdbcFieldCodec
-extends BasePrimitiveJdbcFieldCodec<Byte,UInt7Field>{
-	
+public class UInt7JdbcFieldCodec extends BaseByteJdbcFieldCodec<UInt7Field>{
+
 	public UInt7JdbcFieldCodec(){//no-arg for reflection
 		this(null);
 	}
-	
+
 	public UInt7JdbcFieldCodec(UInt7Field field){
 		super(field);
 	}
 
+	@Override
+	protected Integer getMaxColumnLength(){
+		return 3;
+	}
 
 	@Override
-	public SqlColumn getSqlColumnDefinition(){
-		return new SqlColumn(field.getKey().getColumnName(), MySqlColumnType.TINYINT, 3, field.getKey().isNullable(),
-				false);
+	protected MySqlColumnType getMysqlColumnType(){
+		return MySqlColumnType.TINYINT;
 	}
-	
-	@Override
-	public void setPreparedStatementValue(PreparedStatement ps, int parameterIndex){
-		try{
-			if(field.getValue()==null){
-				ps.setNull(parameterIndex, Types.TINYINT);
-			}else{
-				ps.setByte(parameterIndex, field.getValue());
-			}
-		}catch(SQLException e){
-			throw new DataAccessException(e);
-		}
-	}
-	
-	@Override
-	public Byte parseJdbcValueButDoNotSet(Object obj){
-		return obj==null?null:(Byte)obj;
-	}
-	
-	@Override
-	public Byte fromJdbcResultSetButDoNotSet(ResultSet rs){
-		try{
-			byte value = rs.getByte(field.getKey().getColumnName());
-			return rs.wasNull()?null:value;
-		}catch(SQLException e){
-			throw new DataAccessException(e);
-		}
-	}
-	
+
 }
--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/field/codec/primitive/SignedByteJdbcFieldCodec.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/field/codec/primitive/SignedByteJdbcFieldCodec.java
@@ -1,59 +1,25 @@
 package com.hotpads.datarouter.client.imp.jdbc.field.codec.primitive;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Types;
-
 import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
-import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.SqlColumn;
-import com.hotpads.datarouter.client.imp.jdbc.field.codec.base.BasePrimitiveJdbcFieldCodec;
-import com.hotpads.datarouter.exception.DataAccessException;
-import com.hotpads.datarouter.storage.field.Field;
+import com.hotpads.datarouter.storage.field.imp.comparable.SignedByteField;
 
-public class SignedByteJdbcFieldCodec
-extends BasePrimitiveJdbcFieldCodec<Byte,Field<Byte>>{
-	
+public class SignedByteJdbcFieldCodec extends BaseByteJdbcFieldCodec<SignedByteField>{
+
 	public SignedByteJdbcFieldCodec(){//no-arg for reflection
 		this(null);
 	}
-	
-	public SignedByteJdbcFieldCodec(Field<Byte> field){
+
+	public SignedByteJdbcFieldCodec(SignedByteField field){
 		super(field);
 	}
 
-	
 	@Override
-	public SqlColumn getSqlColumnDefinition(){
-		return new SqlColumn(field.getKey().getColumnName(), MySqlColumnType.TINYINT, 1 , field.getKey().isNullable(), 
-				false);
+	protected Integer getMaxColumnLength(){
+		return 1;
 	}
-	
+
 	@Override
-	public Byte parseJdbcValueButDoNotSet(Object obj){
-		return obj==null?null:(Byte)obj;
-	}
-	
-	@Override
-	public void setPreparedStatementValue(PreparedStatement ps, int parameterIndex){
-		try{
-			if(field.getValue()==null){
-				ps.setNull(parameterIndex, Types.TINYINT);
-			}else{
-				ps.setByte(parameterIndex, field.getValue());
-			}
-		}catch(SQLException e){
-			throw new DataAccessException(e);
-		}
-	}
-	
-	@Override
-	public Byte fromJdbcResultSetButDoNotSet(ResultSet rs){
-		try{
-			byte value = rs.getByte(field.getKey().getColumnName());
-			return rs.wasNull()?null:value;
-		}catch(SQLException e){
-			throw new DataAccessException(e);
-		}
+	protected MySqlColumnType getMysqlColumnType(){
+		return MySqlColumnType.TINYINT;
 	}
 }
