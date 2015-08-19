a41c9ae4cb232c793907091919faa5033bf87e89
cbonsart <cbonsart@hotpads.com>
2015-08-19 05:34:28 -0700

    Clean HBaseNode

--- datarouter-hbase/src/main/java/com/hotpads/datarouter/client/imp/hbase/node/HBaseNode.java
+++ datarouter-hbase/src/main/java/com/hotpads/datarouter/client/imp/hbase/node/HBaseNode.java
@@ -38,158 +38,179 @@ import com.hotpads.datarouter.util.core.DrListTool;
 public class HBaseNode<
 		PK extends PrimaryKey<PK>,
 		D extends Databean<PK,D>,
-		F extends DatabeanFielder<PK,D>> 
+		F extends DatabeanFielder<PK,D>>
 extends HBaseReaderNode<PK,D,F>
-implements PhysicalSortedMapStorageNode<PK,D>, HBaseIncrement<PK>
-{
-	
+implements PhysicalSortedMapStorageNode<PK,D>, HBaseIncrement<PK>{
+
 	public HBaseNode(NodeParams<PK,D,F> params){
 		super(params);
 	}
-	
+
 	@Override
 	public Node<PK,D> getMaster() {
 		return this;
 	}
-	
-	
+
+
 	/************************************ MapStorageWriter methods ****************************/
-	
+
 	public static final byte[] FAM = HBaseSimpleClientFactory.DEFAULT_FAMILY_QUALIFIER;
 	public static final String DUMMY = HBaseSimpleClientFactory.DUMMY_COL_NAME;
-	
-	
+
+
 	@Override
 	public void put(final D databean, final Config config) {
-		if(databean==null){ return; }
+		if(databean==null){
+			return;
+		}
 		putMulti(DrListTool.wrap(databean), config);
 	}
 
-	
+
 	@Override
-	public void putMulti(final Collection<D> databeans, final Config pConfig) {
-		if(DrCollectionTool.isEmpty(databeans)){ return; }
-		final Config config = Config.nullSafe(pConfig);
-		new HBaseMultiAttemptTask<Void>(new HBaseTask<Void>(getDatarouterContext(), getClientTableNodeNames(), "putMulti", config){
-				public Void hbaseCall(HTable hTable, HBaseClient client, ResultScanner managedResultScanner) throws Exception{					
-					List<Row> actions = new ArrayList<>();
-					int numCellsPut = 0, numCellsDeleted = 0, numRowsPut = 0;;
-					long batchStartTime = System.currentTimeMillis();
-					for(D databean : databeans){//TODO obey Config.commitBatchSize
-						if(databean==null){ continue; }
-						PK key = databean.getKey();
-						byte[] keyBytes = getKeyBytesWithScatteringPrefix(null, key, false);
-						Put put = new Put(keyBytes);
-						Delete delete = new Delete(keyBytes);
-						List<Field<?>> fields = fieldInfo.getNonKeyFieldsWithValues(databean);
-						for(Field<?> field : fields){//TODO only put modified fields
-							byte[] fieldBytes = field.getBytes();
-							if(fieldBytes==null){
-								if(DrBooleanTool.isFalseOrNull(config.getIgnoreNullFields())){
-									delete.deleteColumn(FAM, field.getKey().getColumnNameBytes(), batchStartTime);
-									++numCellsDeleted;
+	public void putMulti(final Collection<D> databeans, final Config config) {
+		if(DrCollectionTool.isEmpty(databeans)){
+			return;
+		}
+		final Config nullSafeConfig = Config.nullSafe(config);
+		new HBaseMultiAttemptTask<>(new HBaseTask<Void>(getDatarouterContext(), getClientTableNodeNames(), "putMulti",
+				nullSafeConfig){
+					@Override
+					public Void hbaseCall(HTable htable, HBaseClient client, ResultScanner managedResultScanner)
+					throws Exception{
+						List<Row> actions = new ArrayList<>();
+						int numCellsPut = 0, numCellsDeleted = 0;
+						long batchStartTime = System.currentTimeMillis();
+						for(D databean : databeans){//TODO obey Config.commitBatchSize
+							if(databean==null){
+								continue;
+							}
+							PK key = databean.getKey();
+							byte[] keyBytes = getKeyBytesWithScatteringPrefix(null, key, false);
+							Put put = new Put(keyBytes);
+							Delete delete = new Delete(keyBytes);
+							List<Field<?>> fields = fieldInfo.getNonKeyFieldsWithValues(databean);
+							for(Field<?> field : fields){//TODO only put modified fields
+								byte[] fieldBytes = field.getBytes();
+								if(fieldBytes==null){
+									if(DrBooleanTool.isFalseOrNull(config.getIgnoreNullFields())){
+										delete.deleteColumn(FAM, field.getKey().getColumnNameBytes(), batchStartTime);
+										++numCellsDeleted;
+									}
+								}else{
+									put.add(FAM, field.getKey().getColumnNameBytes(), field.getBytes());
+									++numCellsPut;
 								}
-							}else{
-								put.add(FAM, field.getKey().getColumnNameBytes(), field.getBytes());
-								++numCellsPut;
+							}
+							if(put.isEmpty()){
+								Field<?> dummyField = new SignedByteField(DUMMY, (byte)0);
+								put.add(FAM, dummyField.getKey().getColumnNameBytes(), dummyField.getBytes());
+							}
+							put.setWriteToWAL(config.getPersistentPut());
+							actions.add(put);
+							if(!delete.isEmpty()){
+								actions.add(delete);
 							}
 						}
-						if(put.isEmpty()){ 
-							Field<?> dummyField = new SignedByteField(DUMMY, (byte)0);
-							put.add(FAM, dummyField.getKey().getColumnNameBytes(), dummyField.getBytes());
+						DRCounters.incClientNodeCustom(client.getType(), "cells put", getClientName(), getName(),
+								numCellsPut);
+						DRCounters.incClientNodeCustom(client.getType(), "cells delete", getClientName(), getName(),
+								numCellsDeleted);
+						if(DrCollectionTool.notEmpty(actions)){
+							htable.batch(actions);
+							htable.flushCommits();
 						}
-						put.setWriteToWAL(config.getPersistentPut());
-						actions.add(put);
-						if(!delete.isEmpty()){ actions.add(delete); }
-						++numRowsPut;
+						return null;
 					}
-					DRCounters.incClientNodeCustom(client.getType(), "cells put", getClientName(), getName(), numCellsPut);
-					DRCounters.incClientNodeCustom(client.getType(), "cells delete", getClientName(), getName(), numCellsDeleted);
-					if(DrCollectionTool.notEmpty(actions)){
-						hTable.batch(actions);
-						hTable.flushCommits();
-					}
-					return null;
-				}
-			}).call();
+		}).call();
 	}
-	
+
 //	@Override  //not in the parent interface yet
-	public void increment(Map<PK,Map<String,Long>> countByColumnByKey, Config pConfig){
-		final Config config = Config.nullSafe(pConfig);
-		new HBaseMultiAttemptTask<Void>(new HBaseIncrementOp<PK,D,F>(this, countByColumnByKey, config)).call();
+	@Override
+	public void increment(Map<PK,Map<String,Long>> countByColumnByKey, Config config){
+		final Config nullSafeConfig = Config.nullSafe(config);
+		new HBaseMultiAttemptTask<>(new HBaseIncrementOp<>(this, countByColumnByKey, nullSafeConfig)).call();
 	}
-	
+
 
 	//alternative method would be to truncate the table
 	@Override
-	public void deleteAll(final Config pConfig) {
-		final Config config = Config.nullSafe(pConfig);
-		new HBaseMultiAttemptTask<Void>(new HBaseTask<Void>(getDatarouterContext(), getClientTableNodeNames(), "deleteAll", config){
-				public Void hbaseCall(HTable hTable, HBaseClient client, ResultScanner managedResultScanner) throws Exception{
-					managedResultScanner = hTable.getScanner(new Scan());
-					List<Row> batchToDelete = new ArrayList<>(1000);
-					for(Result row : managedResultScanner){
-						if(row.isEmpty()){ continue; }
-						batchToDelete.add(new Delete(row.getRow()));
-						if(batchToDelete.size() % 1000 == 0){
-							hTable.batch(batchToDelete);
-							hTable.flushCommits();
-							batchToDelete.clear();
+	public void deleteAll(final Config config) {
+		final Config nullSafeConfig = Config.nullSafe(config);
+		new HBaseMultiAttemptTask<>(new HBaseTask<Void>(getDatarouterContext(), getClientTableNodeNames(),
+				"deleteAll", nullSafeConfig){
+					@Override
+					public Void hbaseCall(HTable htable, HBaseClient client, ResultScanner managedResultScanner)
+					throws Exception{
+						managedResultScanner = htable.getScanner(new Scan());
+						List<Row> batchToDelete = new ArrayList<>(1000);
+						for(Result row : managedResultScanner){
+							if(row.isEmpty()){
+								continue;
+							}
+							batchToDelete.add(new Delete(row.getRow()));
+							if(batchToDelete.size() % 1000 == 0){
+								htable.batch(batchToDelete);
+								htable.flushCommits();
+								batchToDelete.clear();
+							}
 						}
+						if(DrCollectionTool.notEmpty(batchToDelete)){
+							htable.batch(batchToDelete);
+							htable.flushCommits();
+						}
+						return null;
 					}
-					if(DrCollectionTool.notEmpty(batchToDelete)){
-						hTable.batch(batchToDelete);
-						hTable.flushCommits();
-					}
-					return null;
-				}
-			}).call();
+		}).call();
 	}
 
-	
+
 	@Override
-	public void delete(PK key, Config pConfig) {
-		deleteMulti(DrListTool.wrap(key), pConfig);
+	public void delete(PK key, Config config) {
+		deleteMulti(DrListTool.wrap(key), config);
 	}
 
-	
+
 	@Override
-	public void deleteMulti(final Collection<PK> keys, final Config pConfig){
-		if(DrCollectionTool.isEmpty(keys)){ return; }
-		final Config config = Config.nullSafe(pConfig);
-		new HBaseMultiAttemptTask<Void>(new HBaseTask<Void>(getDatarouterContext(), getClientTableNodeNames(), "deleteMulti", config){
-				public Void hbaseCall(HTable hTable, HBaseClient client, ResultScanner managedResultScanner) throws Exception{
-					hTable.setAutoFlush(false);
-					List<Row> deletes = DrListTool.createArrayListWithSize(keys);//api requires ArrayList
-					for(PK key : keys){
-						byte[] keyBytes = getKeyBytesWithScatteringPrefix(null, key, false);
-						Delete delete = new Delete(keyBytes);
-//						Delete delete = new Delete(key.getBytes(false));
-						deletes.add(delete);
+	public void deleteMulti(final Collection<PK> keys, final Config config){
+		if(DrCollectionTool.isEmpty(keys)){
+			return;
+		}
+		final Config nullSafeConfig = Config.nullSafe(config);
+		new HBaseMultiAttemptTask<>(new HBaseTask<Void>(getDatarouterContext(), getClientTableNodeNames(),
+				"deleteMulti", nullSafeConfig){
+					@Override
+					public Void hbaseCall(HTable htable, HBaseClient client, ResultScanner managedResultScanner)
+					throws Exception{
+						htable.setAutoFlush(false);
+						List<Row> deletes = DrListTool.createArrayListWithSize(keys);//api requires ArrayList
+						for(PK key : keys){
+							byte[] keyBytes = getKeyBytesWithScatteringPrefix(null, key, false);
+							Delete delete = new Delete(keyBytes);
+							deletes.add(delete);
+						}
+						htable.batch(deletes);
+						htable.flushCommits();
+						return null;
 					}
-					hTable.batch(deletes);
-					hTable.flushCommits();
-					return null;
-				}
-			}).call();
+		}).call();
 	}
-	
-	
+
+
 	/************************** Sorted ************************************/
 
 	@Override
 	public void deleteRangeWithPrefix(PK prefix, boolean wildcardLastField, Config config){
-		//TODO need a method getKeysWithPrefix	
+		//TODO need a method getKeysWithPrefix
 		List<D> databeansToDelete = getWithPrefix(prefix, wildcardLastField, config);
 		if(DrCollectionTool.notEmpty(databeansToDelete)){
 			deleteMulti(DatabeanTool.getKeys(databeansToDelete), null);
 		}
 	}
-	
-	
+
+
 	/*************************** util **************************************/
-	
+
 	public D getDatabean(Result row) {
 		return HBaseResultTool.getDatabean(row, fieldInfo);
 	}
