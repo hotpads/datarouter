5a115726a6e4643e595455ba266d2d57b4c0cc96
cbonsart <cbonsart@hotpads.com>
2015-08-20 18:20:25 -0700

    Merge BaseHibernateScanner and BaseJdbcScanner

--- datarouter-hibernate/src/main/java/com/hotpads/datarouter/client/imp/hibernate/scan/HibernatePrimaryKeyScanner.java
+++ datarouter-hibernate/src/main/java/com/hotpads/datarouter/client/imp/hibernate/scan/HibernatePrimaryKeyScanner.java
@@ -3,21 +3,25 @@ package com.hotpads.datarouter.client.imp.hibernate.scan;
 import java.util.List;
 
 import com.hotpads.datarouter.client.imp.hibernate.node.HibernateReaderNode;
+import com.hotpads.datarouter.client.imp.jdbc.scan.BaseJdbcScanner;
 import com.hotpads.datarouter.config.Config;
 import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 import com.hotpads.util.core.collections.Range;
 
 public class HibernatePrimaryKeyScanner<PK extends PrimaryKey<PK>,D extends Databean<PK,D>>
-extends BaseHibernateScanner<PK,D,PK>{
-	
+extends BaseJdbcScanner<PK,D,PK>{
+
+	private HibernateReaderNode<PK,D,?> node;
+
 	public HibernatePrimaryKeyScanner(HibernateReaderNode<PK,D,?> node, Range<PK> range, Config config){
-		super(node, range, config);
+		super(range, config);
+		this.node = node;
 	}
 
 	@Override
 	protected List<PK> doLoad(Range<PK> range, Config config){
-		return node.getKeysInRange(range.getStart(), range.getStartInclusive(), range.getEnd(), 
+		return node.getKeysInRange(range.getStart(), range.getStartInclusive(), range.getEnd(),
 				range.getEndInclusive(), config);
 	}
 
@@ -25,7 +29,7 @@ extends BaseHibernateScanner<PK,D,PK>{
 	protected PK getPrimaryKey(PK pk){
 		return pk;
 	}
-	
+
 	@Override
 	protected void setCurrentFromResult(PK result) {
 		current = result;
