754547f53ba94f6d8d78692b353927aa564b5e2b
cguillaume <cguillaume@hotpads.com>
2015-09-24 08:08:46 -0700

    use Arrays.asList

--- datarouter-websocket/src/main/java/com/hotpads/websocket/session/WebSocketSessionKey.java
+++ datarouter-websocket/src/main/java/com/hotpads/websocket/session/WebSocketSessionKey.java
@@ -1,10 +1,10 @@
 package com.hotpads.websocket.session;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
 import com.hotpads.datarouter.storage.field.Field;
-import com.hotpads.datarouter.storage.field.FieldTool;
 import com.hotpads.datarouter.storage.field.encoding.FieldGeneratorType;
 import com.hotpads.datarouter.storage.field.imp.StringField;
 import com.hotpads.datarouter.storage.field.imp.positive.UInt63Field;
@@ -24,7 +24,7 @@ public class WebSocketSessionKey extends BasePrimaryKey<WebSocketSessionKey> {
 
 	@Override
 	public List<Field<?>> getFields(){
-		return FieldTool.createList(
+		return Arrays.asList(
 			new StringField(F.userToken, userToken, MySqlColumnType.MAX_LENGTH_VARCHAR),
 			new UInt63Field(null, F.id, false, FieldGeneratorType.RANDOM, id));
 	}
