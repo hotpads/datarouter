ba6af9ff039e691fa1e75aa036ae796651ff441b
cguillaume <cguillaume@hotpads.com>
2015-10-01 23:16:47 -0700

    hightlight bug

---
+++ datarouter-hbase/src/test/java/com/hotpads/datarouter/client/imp/hbase/test/HBaseSubEntityReaderNodeIntegrationTests.java
@@ -0,0 +1,63 @@
+package com.hotpads.datarouter.client.imp.hbase.test;
+
+import javax.inject.Inject;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Guice;
+import org.testng.annotations.Test;
+
+import com.hotpads.datarouter.node.factory.EntityNodeFactory;
+import com.hotpads.datarouter.node.factory.NodeFactory;
+import com.hotpads.datarouter.node.op.combo.SortedMapStorage;
+import com.hotpads.datarouter.routing.Datarouter;
+import com.hotpads.datarouter.test.DatarouterTestModuleFactory;
+import com.hotpads.datarouter.test.DrTestConstants;
+import com.hotpads.datarouter.test.node.basic.sorted.SortedBean;
+import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanKey;
+import com.hotpads.datarouter.test.node.basic.sorted.SortedNodeTestRouter;
+
+@Guice(moduleFactory=DatarouterTestModuleFactory.class)
+public class HBaseSubEntityReaderNodeIntegrationTests{
+
+	@Inject
+	protected Datarouter datarouter;
+	@Inject
+	private EntityNodeFactory entityNodeFactory;
+	@Inject
+	private NodeFactory nodeFactory;
+
+	private SortedMapStorage<SortedBeanKey,SortedBean> sortedBean;
+
+	@BeforeClass
+	public void setup(){
+		SortedNodeTestRouter router = new SortedNodeTestRouter(datarouter, entityNodeFactory, nodeFactory,
+				DrTestConstants.CLIENT_drTestHBase, true, true);
+		sortedBean = router.sortedBean();
+	}
+
+	@Test
+	public void scanTest(){
+		sortedBean.put(new SortedBean("a", "b", 1, "d", "f1", 2L, "f3", 4D), null);
+		sortedBean.put(new SortedBean("a", "b", 2, "d", "f1", 2L, "f3", 4D), null);
+		sortedBean.put(new SortedBean("a", "c", 1, "d", "f1", 2L, "f3", 4D), null);
+		sortedBean.put(new SortedBean("a", "c", 2, "d", "f1", 2L, "f3", 4D), null);
+		sortedBean.put(new SortedBean("a", "c", 2, "dd", "f1", 2L, "f3", 4D), null);
+		sortedBean.put(new SortedBean("b", "b", 1, "d", "f1", 2L, "f3", 4D), null);
+
+		Assert.assertEquals(sortedBean.stream(null, null).count(), 6);
+//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", null, null, null)), null)
+//				.count(), 5); // fail
+//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", null, null)), null)
+//				.count(), 3); // fail
+//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", 2, null)), null)
+//				.count(), 2); // fail
+		Assert.assertEquals(sortedBean.stream(null, null).count(), 6);
+	}
+
+	@AfterMethod
+	public void cleanup(){
+		sortedBean.deleteAll(null);
+	}
+}
