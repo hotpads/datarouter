195e5cbaf86a7011be9acd5f02f02c3382b89e9d
cbonsart <cbonsart@hotpads.com>
2015-10-05 08:48:36 -0700

    Rename SortedStorageCountingHelper to Tool

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/op/read/SortedStorageCountingTool.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/op/read/SortedStorageCountingTool.java
@@ -8,7 +8,7 @@ import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 import com.hotpads.util.core.collections.Range;
 
-public class SortedStorageCountingHelper{
+public class SortedStorageCountingTool{
 
 	private static final int BATCH_SIZE = 10000;
 
