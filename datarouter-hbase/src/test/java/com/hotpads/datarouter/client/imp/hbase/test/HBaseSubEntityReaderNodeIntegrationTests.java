d85caf7770050bcade70e1859b16064a96b7faa2
cguillaume <cguillaume@hotpads.com>
2015-10-06 13:10:28 -0700

    fix non full entity fix inclusive end range

--- datarouter-hbase/src/test/java/com/hotpads/datarouter/client/imp/hbase/test/HBaseSubEntityReaderNodeIntegrationTests.java
+++ datarouter-hbase/src/test/java/com/hotpads/datarouter/client/imp/hbase/test/HBaseSubEntityReaderNodeIntegrationTests.java
@@ -1,5 +1,8 @@
 package com.hotpads.datarouter.client.imp.hbase.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.testng.Assert;
@@ -12,11 +15,13 @@ import com.hotpads.datarouter.node.factory.EntityNodeFactory;
 import com.hotpads.datarouter.node.factory.NodeFactory;
 import com.hotpads.datarouter.node.op.combo.SortedMapStorage;
 import com.hotpads.datarouter.routing.Datarouter;
+import com.hotpads.datarouter.storage.databean.DatabeanTool;
 import com.hotpads.datarouter.test.DatarouterTestModuleFactory;
 import com.hotpads.datarouter.test.DrTestConstants;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBean;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanKey;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedNodeTestRouter;
+import com.hotpads.util.core.collections.KeyRangeTool;
 import com.hotpads.util.core.collections.Range;
 
 @Guice(moduleFactory=DatarouterTestModuleFactory.class)
@@ -30,40 +35,60 @@ public class HBaseSubEntityReaderNodeIntegrationTests{
 	private NodeFactory nodeFactory;
 
 	private SortedMapStorage<SortedBeanKey,SortedBean> sortedBean;
+	private List<SortedBean> sortedBeans;
 
 	@BeforeClass
 	public void setup(){
 		SortedNodeTestRouter router = new SortedNodeTestRouter(datarouter, entityNodeFactory, nodeFactory,
 				DrTestConstants.CLIENT_drTestHBase, true, true);
 		sortedBean = router.sortedBean();
+		sortedBeans = new ArrayList<>();
 	}
 
 	@Test
 	public void scanTest(){
-		sortedBean.put(new SortedBean("a", "b", 1, "d", "f1", 2L, "f3", 4D), null);
-		sortedBean.put(new SortedBean("a", "b", 2, "d", "f1", 2L, "f3", 4D), null);
-		sortedBean.put(new SortedBean("a", "c", 1, "d", "f1", 2L, "f3", 4D), null);
-		sortedBean.put(new SortedBean("a", "c", 2, "d", "f1", 2L, "f3", 4D), null);
-		sortedBean.put(new SortedBean("a", "c", 2, "dd", "f1", 2L, "f3", 4D), null);
-		sortedBean.put(new SortedBean("b", "b", 1, "d", "f1", 2L, "f3", 4D), null);
-		sortedBean.put(new SortedBean("c", "b", 1, "d", "f1", 2L, "f3", 4D), null);
+		sortedBeans.add(new SortedBean("a", "b", 1, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "b", 2, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "b", 25, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "b", 3, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "b", 4, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "b", 4, "dj", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "b", 4, "e", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "c", 1, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "c", 2, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("a", "c", 2, "dd", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("b", "b", 1, "d", "f1", 2L, "f3", 4D));
+		sortedBeans.add(new SortedBean("c", "b", 1, "d", "f1", 2L, "f3", 4D));
 
-		Assert.assertEquals(sortedBean.stream(null, null).count(), 7);
+		sortedBean.putMulti(sortedBeans, null);
+
 		Assert.assertEquals(sortedBean.stream(new Range<>(new SortedBeanKey("a", null, null, null), true,
-				new SortedBeanKey("b", null, null, null), true), null).count(), 6);
-//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", null, null, null)), null)
-//				.count(), 5); // fail, return 0 databean
-//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", null, null)), null)
-//				.count(), 3); // fail, return 0 databean
-//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", 2, null)), null)
-//				.count(), 2); // fail, return 0 databean
-//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", 2, "d")), null)
-//				.count(), 2); // fail, return 0 databean
-		Assert.assertEquals(sortedBean.stream(null, null).count(), 7);
+				new SortedBeanKey("b", null, null, null), true), null).count(), 11);
+
+		Assert.assertEquals(sortedBean.stream(new Range<>(new SortedBeanKey("a", null, null, null),
+				new SortedBeanKey("a", "c", null, null)), null).count(), 7);
+
+		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", null, null, null)), null)
+				.count(), 10);
+		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", null, null)), null)
+				.count(), 3);
+		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", 2, null)), null)
+				.count(), 2);
+		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "b", 2, null)), null)
+				.count(), 1);
+//		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "b", 4, "d")), null)
+//				.count(), 1); // failed, got 2 results because it wildcardsLastField (found the "dj")
+		Assert.assertEquals(sortedBean.stream(new Range<>(new SortedBeanKey("a", "c", 1, null),
+				new SortedBeanKey("a", "c", 2, null)), null).count(), 1);
+		Assert.assertEquals(sortedBean.stream(new Range<>(new SortedBeanKey("a", "c", 1, null), true,
+				new SortedBeanKey("a", "c", 2, null), true), null).count(), 3);
+		Assert.assertEquals(sortedBean.stream(KeyRangeTool.forPrefix(new SortedBeanKey("a", "c", 2, "d")), null)
+				.count(), 2);
 	}
 
 	@AfterMethod
 	public void cleanup(){
-		sortedBean.deleteAll(null);
+		sortedBean.deleteMulti(DatabeanTool.getKeys(sortedBeans), null);
 	}
+
 }
