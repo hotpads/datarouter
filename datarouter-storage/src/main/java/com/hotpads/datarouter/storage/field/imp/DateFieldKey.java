1030c637245ee5ce5e166b57f27cd190ad2d5410
cguillaume <cguillaume@hotpads.com>
2015-12-23 17:05:33 -0800

    NotificationPreference storage, api and visu tool

---
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/DateFieldKey.java
@@ -0,0 +1,13 @@
+package com.hotpads.datarouter.storage.field.imp;
+
+import java.util.Date;
+
+import com.hotpads.datarouter.storage.field.PrimitiveFieldKey;
+
+public class DateFieldKey extends PrimitiveFieldKey<Date>{
+
+	public DateFieldKey(String name){
+		super(name);
+	}
+
+}
