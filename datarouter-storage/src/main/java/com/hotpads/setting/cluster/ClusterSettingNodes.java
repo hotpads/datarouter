0da4786e1233b846ee8408de43f4ab970de7f9b9
mcorgan <mcorgan@hotpads.com>
2015-12-27 12:44:44 -0800

    move ClusterSetting implementation of Settings from datarouter-storage to datarouter-core

--- datarouter-storage/src/main/java/com/hotpads/setting/cluster/ClusterSettingNodes.java
+++
@@ -1,9 +0,0 @@
-package com.hotpads.setting.cluster;
-
-import com.hotpads.datarouter.node.op.combo.SortedMapStorage;
-
-public interface ClusterSettingNodes{
-
-	public SortedMapStorage<ClusterSettingKey,ClusterSetting> clusterSetting();
-
-}
