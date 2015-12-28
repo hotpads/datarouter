efa24896d88b1f1097cc97535ebed941b7a40800
cguillaume <cguillaume@hotpads.com>
2015-12-28 14:31:09 -0800

    fix tests

--- datarouter-storage/src/main/java/com/hotpads/datarouter/config/DatarouterStorageGuiceModule.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/config/DatarouterStorageGuiceModule.java
@@ -1,16 +1,16 @@
 package com.hotpads.datarouter.config;
 
-import com.google.inject.servlet.ServletModule;
+import com.google.inject.AbstractModule;
 import com.hotpads.datarouter.client.availability.ClientAvailabilityClusterSettings;
 import com.hotpads.datarouter.client.availability.ClientAvailabilitySettings;
 import com.hotpads.datarouter.inject.DatarouterInjector;
 import com.hotpads.datarouter.inject.guice.GuiceInjector;
 import com.hotpads.datarouter.inject.guice.executor.DatarouterExecutorGuiceModule;
 
-public class DatarouterStorageGuiceModule extends ServletModule{
+public class DatarouterStorageGuiceModule extends AbstractModule{
 
 	@Override
-	protected void configureServlets(){
+	protected void configure(){
 		bind(DatarouterInjector.class).to(GuiceInjector.class);
 		bind(ClientAvailabilitySettings.class).to(ClientAvailabilityClusterSettings.class);
 		install(new DatarouterExecutorGuiceModule());
