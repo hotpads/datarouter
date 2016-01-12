67123adb4f432a27c54b844f8cfcf13d7e0aa581
cguillaume <cguillaume@hotpads.com>
2016-01-12 13:11:51 -0800

    remove NullProvider

--- datarouter-storage/src/main/java/com/hotpads/datarouter/test/DatarouterStorageTestGuiceModule.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/test/DatarouterStorageTestGuiceModule.java
@@ -4,13 +4,12 @@ import com.google.inject.AbstractModule;
 import com.hotpads.datarouter.client.availability.ClientAvailabilitySettings;
 import com.hotpads.datarouter.client.availability.TestClientAvailabilitySettings;
 import com.hotpads.datarouter.config.DatarouterSettings;
-import com.hotpads.datarouter.util.NullProvider;
 
 public class DatarouterStorageTestGuiceModule extends AbstractModule{
 
 	@Override
 	protected void configure(){
-		bind(DatarouterSettings.class).toProvider(NullProvider.create(DatarouterSettings.class));
+		bind(DatarouterSettings.class).toProvider(() -> null);
 		bind(ClientAvailabilitySettings.class).to(TestClientAvailabilitySettings.class);
 	}
 
