b2bdac4cd79ffe0b5ffb8d93e1ebe1b5385a8010
mattc <mattc@hotpads.com>
2016-01-13 16:33:59 -0800

    introduce DatarouterSettings interface with NoOp implementation

--- datarouter-storage/src/main/java/com/hotpads/datarouter/config/DatarouterSettings.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/config/DatarouterSettings.java
@@ -1,51 +1,14 @@
 package com.hotpads.datarouter.config;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import com.hotpads.datarouter.batch.config.BatchSizeOptimizerSettings;
-import com.hotpads.datarouter.client.availability.ClientAvailabilityClusterSettings;
-import com.hotpads.datarouter.profile.ProfilingSettings;
-import com.hotpads.datarouter.setting.SettingFinder;
-import com.hotpads.datarouter.setting.SettingNode;
 import com.hotpads.datarouter.setting.cached.impl.BooleanCachedSetting;
 import com.hotpads.datarouter.setting.cached.impl.IntegerCachedSetting;
 
-@Singleton
-public class DatarouterSettings extends SettingNode {
+public interface DatarouterSettings{
 
-	private BooleanCachedSetting loggingConfigUpdaterEnabled;
-	private BooleanCachedSetting recordCallsites;
-	private IntegerCachedSetting numThreadsForMaxThreadsTest;
+	BooleanCachedSetting getLoggingConfigUpdaterEnabled();
 
-	@Inject
-	public DatarouterSettings(SettingFinder finder, DatarouterNotificationSettings notificationSettings,
-			ProfilingSettings profilingSettings, BatchSizeOptimizerSettings batchSizeOptimizerSettings,
-			ClientAvailabilityClusterSettings clientAvailabilitySettings){
-		super(finder, "datarouter.", "");
-		registerChild(notificationSettings);
-		registerChild(profilingSettings);
-		registerChild(batchSizeOptimizerSettings);
-		registerChild(clientAvailabilitySettings);
-		registerSettings();
-	}
+	BooleanCachedSetting getRecordCallsites();
 
-	private void registerSettings(){
-		this.loggingConfigUpdaterEnabled = registerBoolean("loggingConfigUpdaterEnabled", true);
-		this.recordCallsites = registerBoolean("recordCallsites", false);
-		this.numThreadsForMaxThreadsTest = registerInteger("numThreadsForMaxThreadsTest", 1);
-	}
-
-	public BooleanCachedSetting getLoggingConfigUpdaterEnabled(){
-		return loggingConfigUpdaterEnabled;
-	}
-
-	public BooleanCachedSetting getRecordCallsites(){
-		return recordCallsites;
-	}
-
-	public IntegerCachedSetting getNumThreadsForMaxThreadsTest(){
-		return numThreadsForMaxThreadsTest;
-	}
+	IntegerCachedSetting getNumThreadsForMaxThreadsTest();
 
 }
