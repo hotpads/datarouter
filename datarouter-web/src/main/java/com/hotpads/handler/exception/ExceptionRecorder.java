59b9f60c7c4bf70052cb13b0a48b82557465dfb3
mattc <mattc@hotpads.com>
2016-01-19 18:03:40 -0800

    ExceptionRecorder interface

--- datarouter-web/src/main/java/com/hotpads/handler/exception/ExceptionRecorder.java
+++ datarouter-web/src/main/java/com/hotpads/handler/exception/ExceptionRecorder.java
@@ -3,5 +3,6 @@ package com.hotpads.handler.exception;
 public interface ExceptionRecorder{
 
 	ExceptionRecord tryRecordException(Exception exception, String fallbackLocation);
-
+	ExceptionRecord tryRecordException(Exception exception, String fallbackLocation, ExceptionReporter reporter);
+	ExceptionRecord recordException(Exception exception, ExceptionReporter reporter, String location);
 }
