d86821ea5d0e79399d1cd5e3d5d2541ec9fcf8d0
David Peters <dpeters@hotpads.com>
2016-01-26 13:38:33 -0800

    allow fieldKeys in UInt64Field constructor

--- datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/positive/UInt63Field.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/positive/UInt63Field.java
@@ -3,14 +3,14 @@ package com.hotpads.datarouter.storage.field.imp.positive;
 import java.util.Random;
 
 import com.hotpads.datarouter.storage.field.BasePrimitiveField;
+import com.hotpads.datarouter.storage.field.PrimitiveFieldKey;
 import com.hotpads.datarouter.storage.field.encoding.FieldGeneratorType;
 import com.hotpads.datarouter.util.core.DrStringTool;
 import com.hotpads.util.core.bytes.LongByteTool;
 import com.hotpads.util.core.number.RandomTool;
 
-public class UInt63Field 
-extends BasePrimitiveField<Long>{
-	
+public class UInt63Field extends BasePrimitiveField<Long>{
+
 	public UInt63Field(String name, Long value){
 		super(name, value);
 	}
@@ -18,16 +18,24 @@ extends BasePrimitiveField<Long>{
 	public UInt63Field(String name, boolean nullable, Long value){
 		super(null, name, nullable, FieldGeneratorType.NONE, value);
 	}
-	
+
 	public UInt63Field(String prefix, String name, boolean nullable, FieldGeneratorType fieldGeneratorType, Long value){
 		super(prefix, name, nullable, fieldGeneratorType, value);
 	}
-	
-	public UInt63Field(String prefix, String name, String columnName, boolean nullable, 
+
+	public UInt63Field(String prefix, String name, String columnName, boolean nullable,
 			FieldGeneratorType fieldGeneratorType, Long value){
 		super(prefix, name, columnName, nullable, fieldGeneratorType, value);
 	}
-	
+
+	public UInt63Field(PrimitiveFieldKey<Long> key, Long value){
+		super(key, value);
+	}
+
+	public UInt63Field(String prefix, PrimitiveFieldKey<Long> key, Long value){
+		super(prefix, key, value);
+	}
+
 	/************************ static *********************************/
 
 	private static final Random random = new Random();
@@ -35,8 +43,8 @@ extends BasePrimitiveField<Long>{
 	public static long nextPositiveRandom(){
 		return RandomTool.nextPositiveLong(random);
 	}
-	
-	
+
+
 	/*********************** StringEncodedField ***********************/
 
 	@Override
@@ -44,15 +52,15 @@ extends BasePrimitiveField<Long>{
 		if(value==null){ return null; }
 		return value.toString();
 	}
-	
+
 	@Override
 	public Long parseStringEncodedValueButDoNotSet(String s){
 		if(DrStringTool.isEmpty(s) || s.equals("null")){
-			return null; 
+			return null;
 		}
 		return Long.valueOf(s);
 	}
-	
+
 
 	/*********************** ByteEncodedField ***********************/
 
@@ -60,12 +68,12 @@ extends BasePrimitiveField<Long>{
 	public byte[] getBytes(){
 		return value==null?null:LongByteTool.getUInt63Bytes(value);
 	}
-	
+
 	@Override
 	public int numBytesWithSeparator(byte[] bytes, int offset){
 		return 8;
 	}
-	
+
 	@Override
 	public Long fromBytesButDoNotSet(byte[] bytes, int offset){
 		return LongByteTool.fromUInt63Bytes(bytes, offset);
