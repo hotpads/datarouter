c17f0575d7697f13240d543c834b1d5c3b4be3b6
smenon <smenon@hotpads.com>
2016-01-29 10:21:31 -0800

    remove the unwanted quotes

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/generate/SqlAlterTableGenerator.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/generate/SqlAlterTableGenerator.java
@@ -253,7 +253,7 @@ public class SqlAlterTableGenerator implements DdlGenerator{
 			int numColumns = DrCollectionTool.size(index.getColumns());
 			int columnCounter = 0;
 			sb.append("add unique index ");
-			sb.append("index.getName()");
+			sb.append(index.getName());
 			sb.append("(");
 			for(SqlColumn col : index.getColumns()){
 				sb.append( col.getName());
@@ -378,6 +378,7 @@ public class SqlAlterTableGenerator implements DdlGenerator{
 
 			// case2 : Dropping a boolean field from table with a default value specified
 			SqlAlterTableGenerator alterGenerator12 = new SqlAlterTableGenerator(options, table1, table2, "config");
+			
 			logger.warn(alterGenerator12.generateDdl());
 		}
 	}
