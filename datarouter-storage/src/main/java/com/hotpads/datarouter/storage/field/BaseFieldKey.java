8ca9c5dc00addf8cc3fe37bd6008626ef43cfb7d
cguillaume <cguillaume@hotpads.com>
2016-02-09 21:12:58 -0800

    use clone pattern

--- datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/BaseFieldKey.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/BaseFieldKey.java
@@ -41,10 +41,6 @@ implements FieldKey<T>{
 		this.defaultValue = defaultValue;
 	}
 
-	public BaseFieldKey(String name, FieldGeneratorType fieldGeneratorType){
-		this(name, true, fieldGeneratorType);
-	}
-
 	/******************************** methods *******************************/
 
 	@Override
