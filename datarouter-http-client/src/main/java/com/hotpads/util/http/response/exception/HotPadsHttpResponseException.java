740fc68e1079fe299bddbffa879b7577f924ced1
cguillaume <cguillaume@hotpads.com>
2016-02-16 19:33:16 -0800

    add exception id into HotPadsHttpResponseException message

--- datarouter-http-client/src/main/java/com/hotpads/util/http/response/exception/HotPadsHttpResponseException.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/response/exception/HotPadsHttpResponseException.java
@@ -1,5 +1,6 @@
 package com.hotpads.util.http.response.exception;
 
+import org.apache.http.Header;
 import org.apache.http.HttpStatus;
 
 import com.hotpads.util.http.response.HotPadsHttpResponse;
@@ -8,17 +9,27 @@ import com.hotpads.util.http.response.HotPadsHttpResponse;
 public class HotPadsHttpResponseException extends HotPadsHttpException {
 
 	private HotPadsHttpResponse response;
-	
+
 	public HotPadsHttpResponseException(HotPadsHttpResponse response, long requestStartTimeMs) {
-		super("HTTP response returned with status code " + response.getStatusCode() + " after "
-				+ (System.currentTimeMillis() - requestStartTimeMs) + "ms with entity:\n" + response.getEntity(), null);
+		super(buildMessage(response, requestStartTimeMs), null);
 		this.response = response;
 	}
-	
+
+	private static String buildMessage(HotPadsHttpResponse response, long requestStartTimeMs){
+		String message = "HTTP response returned with status code " + response.getStatusCode();
+		Header header = response.getFirstHeader("x-eid");
+		if(header != null){
+			message += " and exception id " + header.getValue();
+		}
+		message += " after " + (System.currentTimeMillis() - requestStartTimeMs) + "ms";
+		message += " with entity:\n" + response.getEntity();
+		return message;
+	}
+
 	public HotPadsHttpResponse getResponse() {
 		return response;
 	}
-	
+
 	/**
 	 * 4XX status code. Issue exists in the client or request.
 	 */
@@ -26,7 +37,7 @@ public class HotPadsHttpResponseException extends HotPadsHttpException {
 		int statusCode = response.getStatusCode();
 		return statusCode >= HttpStatus.SC_BAD_REQUEST && statusCode < HttpStatus.SC_INTERNAL_SERVER_ERROR;
 	}
-	
+
 	/**
 	 * 5XX status code. Issue exists on the server.
 	 */
