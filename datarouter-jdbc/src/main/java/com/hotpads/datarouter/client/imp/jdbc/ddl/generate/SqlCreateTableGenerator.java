c447374bf496f3057ea43ae2bcfd96d7a6aa4c2c
smenon <smenon@hotpads.com>
2016-02-17 16:34:59 -0800

    add unique key in Create table statement

--- datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/generate/SqlCreateTableGenerator.java
+++ datarouter-jdbc/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/generate/SqlCreateTableGenerator.java
@@ -14,13 +14,13 @@ import com.hotpads.datarouter.util.core.DrStringTool;
 public class SqlCreateTableGenerator implements DdlGenerator{
 
 	/************************** fields *************************/
-	
+
 	protected SqlTable table;
 	protected String databaseName="";
-	
-	
+
+
 	/******************* construct ****************************/
-	
+
 	public SqlCreateTableGenerator(SqlTable table){
 		this.table = table;
 	}
@@ -28,8 +28,8 @@ public class SqlCreateTableGenerator implements DdlGenerator{
 		this.table = table;
 		this.databaseName = databaseName;
 	}
-	
-	
+
+
 	/****************** primary method ****************************/
 
 	@Override
@@ -50,17 +50,17 @@ public class SqlCreateTableGenerator implements DdlGenerator{
 			sb.append(" " + col.getName() + " " + typeString);
 			if(type.shouldSpecifyLength(col.getMaxLength())){
 				sb.append("(" + col.getMaxLength() + ")");
-			}		
+			}
 			sb.append(col.getDefaultValueStatement());
 			if (col.getAutoIncrement()) {
 				sb.append(" auto_increment");
 			}
 			if(i < numberOfColumns-1){ sb.append(",\n"); }
 		}
-		
+
 		if(table.hasPrimaryKey()){
 			sb.append(",\n");
-			sb.append(" primary key ("); 
+			sb.append(" primary key (");
 			int numberOfColumnsInPrimaryKey=table.getPrimaryKey().getColumns().size();
 			for(int i=0; i< numberOfColumnsInPrimaryKey; i++){
 				col = table.getPrimaryKey().getColumns().get(i);
@@ -69,34 +69,62 @@ public class SqlCreateTableGenerator implements DdlGenerator{
 			}
 			sb.append(")");
 		}
-		
-		int numIndexes = DrCollectionTool.size(table.getIndexes());
-		if(numIndexes > 0){ sb.append(",\n"); }
-		int indexCounter = -1;
-		for(SqlIndex index : DrIterableTool.nullSafe(table.getIndexes())){
-			++indexCounter;
-			sb.append(" index "+ index.getName() +" (");
-			int numColumns = DrCollectionTool.size(index.getColumns());
-			int columnCounter = -1;
+
+		int numUniqueIndexes = DrCollectionTool.size(table.getUniqueIndexes());
+		if(numUniqueIndexes > 0){
+			sb.append(",\n");
+		}
+		boolean appendedAny = false;
+		for(SqlIndex index : DrIterableTool.nullSafe(table.getUniqueIndexes())){
+			if(appendedAny){
+				sb.append(",\n");
+			}
+			appendedAny = true;
+			sb.append(" unique index "+ index.getName() +" (");
+			boolean appendedAnyCol = false;
 			for(SqlColumn column : DrIterableTool.nullSafe(index.getColumns())){
-				++columnCounter;
+				if(appendedAnyCol){
+					 sb.append(", ");
+				}
+				appendedAnyCol = true;
 				sb.append(column.getName());
-				if(columnCounter != numColumns -1){ sb.append(", "); }
 			}
 			sb.append(")");
-			if(indexCounter != numIndexes - 1){ sb.append(","); }
-			sb.append("\n");
+		}
+
+		int numIndexes = DrCollectionTool.size(table.getIndexes());
+		if(numIndexes > 0){
+			sb.append(",");
+		}
+		sb.append("\n");
+		boolean appendedAnyIndex = false;
+		for(SqlIndex index : DrIterableTool.nullSafe(table.getIndexes())){
+			if(appendedAnyIndex){
+				sb.append(",\n");
+			}
+			appendedAnyIndex = true;
+			sb.append(" index "+ index.getName() +" (");
+
+			boolean appendedAnyIndexCol = false;
+			for(SqlColumn column : DrIterableTool.nullSafe(index.getColumns())){
+				if(appendedAnyIndexCol){
+					sb.append(", ");
+				}
+				appendedAnyIndexCol= true;
+				sb.append(column.getName());
+			}
+			sb.append(")");
 		}
 		sb.append(")");
 		sb.append(" engine=" + table.getEngine() + " character set = " + table.getCharacterSet() + " collate "
 				+ table.getCollation());
 		return sb.toString();
-		
+
 	}
-	
-		
+
+
 	/******************** tests *************************/
-	
+
 	public static class  SqlCreateTableGeneratorTester{
 		@Test
 		public void testAutoIncrement() {
@@ -109,14 +137,14 @@ public class SqlCreateTableGenerator implements DdlGenerator{
 					.addColumn(colString)
 					.setPrimaryKey(primaryKey);
 			SqlCreateTableGenerator generator = new SqlCreateTableGenerator(sqlTable);
-			String expected = "create table AutoIncrement (\n" + 
-					 " id bigint(8) not null auto_increment,\n" + 
+			String expected = "create table AutoIncrement (\n" +
+					 " id bigint(8) not null auto_increment,\n" +
 					 " string varchar(100) default null,\n" +
 					 " primary key (id)) engine=INNODB character set = latin1 collate latin1_swedish_ci";
 			System.out.println(generator.generateDdl());
 			Assert.assertEquals(expected, generator.generateDdl());
 		}
-		
+
 		@Test
 		public void testGenerate(){
 			String nameOfTable="Model";
