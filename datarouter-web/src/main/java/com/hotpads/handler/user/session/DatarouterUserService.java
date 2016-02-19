5d1bfffff3fc45b8126467ca85c272b65b5e3b35
smenon <smenon@hotpads.com>
2016-02-19 15:16:31 -0800

    change email to personalized email

--- datarouter-web/src/main/java/com/hotpads/handler/user/session/DatarouterUserService.java
+++ datarouter-web/src/main/java/com/hotpads/handler/user/session/DatarouterUserService.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hotpads.datarouter.routing.Datarouter;
 import com.hotpads.datarouter.util.core.DrListTool;
 import com.hotpads.handler.user.DatarouterUser;
 import com.hotpads.handler.user.DatarouterUserNodes;
@@ -17,39 +18,42 @@ import com.hotpads.handler.user.role.DatarouterUserRole;
 
 public class DatarouterUserService{
 	private static final Logger logger = LoggerFactory.getLogger(DatarouterUserService.class);
-	
+
 	public static final long ADMIN_ID = 1L;
 	private static final String ADMIN_USERNAME =  "admin@hotpads.com";
-	private static final String DEFAULT_PASSWORD = "tempAdminPassword";	
+	private static final String DEFAULT_PASSWORD = "tempAdminPassword";
 	private static final List<DatarouterUserRole> DEFAULT_ADMIN_ROLES = DrListTool.create(
-			DatarouterUserRole.datarouterAdmin, DatarouterUserRole.admin, DatarouterUserRole.user, 
+			DatarouterUserRole.datarouterAdmin, DatarouterUserRole.admin, DatarouterUserRole.user,
 			DatarouterUserRole.apiUser);
-	
-	//injected fields	
-	private DatarouterUserNodes userNodes;
-	private DatarouterPasswordService passwordService;
-	
+
+	//injected fields
+	private final DatarouterUserNodes userNodes;
+	private final DatarouterPasswordService passwordService;
+	private final Datarouter datarouter;
+
 	@Inject
-	public DatarouterUserService(DatarouterUserNodes userNodes, DatarouterPasswordService passwordService){
+	public DatarouterUserService(DatarouterUserNodes userNodes, DatarouterPasswordService passwordService,
+			Datarouter datarouter){
 		this.userNodes = userNodes;
 		this.passwordService = passwordService;
+		this.datarouter = datarouter;
 	}
-	
-	public void createAdminUser(){		
-		String salt = passwordService.generateSaltForNewUser();		
-		String digest = passwordService.digest(salt, DEFAULT_PASSWORD);		
+
+	public void createAdminUser(){
+		String salt = passwordService.generateSaltForNewUser();
+		String digest = passwordService.digest(salt, DEFAULT_PASSWORD);
 		DatarouterUser user = new DatarouterUser();
 		user.setCreated(new Date());
-		user.setUsername(ADMIN_USERNAME);
+		user.setUsername(datarouter.getAdministratorEmail());
 		user.setEnabled(true);
 		user.setId(ADMIN_ID);
 		user.setLastLoggedIn(new Date());
 		user.setPasswordDigest(digest);
 		user.setPasswordSalt(salt);
 		user.setRoles(DEFAULT_ADMIN_ROLES);
-		user.setUserToken(DatarouterTokenGenerator.generateRandomToken());		
+		user.setUserToken(DatarouterTokenGenerator.generateRandomToken());
 		userNodes.getUserNode().put(user, null);
-		logger.warn("Created default admin user account");		
+		logger.warn("Created default admin user account");
 	}
 
 }
