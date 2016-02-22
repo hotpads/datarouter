4b7ca20c782a78958a09aa94f66855875da63976
cbonsart <cbonsart@hotpads.com>
2016-02-22 10:57:18 -0800

    Add withSize() for StringFieldKey

--- datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/StringFieldKey.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/StringFieldKey.java
@@ -5,17 +5,20 @@ import com.hotpads.datarouter.storage.field.BaseFieldKey;
 import com.hotpads.datarouter.storage.field.encoding.FieldGeneratorType;
 
 public class StringFieldKey extends BaseFieldKey<String>{
-	
+
 	private static final int DEFAULT_MAX_SIZE = MySqlColumnType.MAX_LENGTH_VARCHAR;
-	
+
 	private final int size;
 
-	
 	public StringFieldKey(String name){
 		super(name);
 		this.size = DEFAULT_MAX_SIZE;
 	}
-	
+
+	/**
+	 * @deprecated use {@link #StringFieldKey(String)} and {@link #withSize(int)}
+	 */
+	@Deprecated
 	public StringFieldKey(String name, int size){
 		super(name);
 		this.size = size;
@@ -30,23 +33,30 @@ public class StringFieldKey extends BaseFieldKey<String>{
 		super(name, columnName, nullable, FieldGeneratorType.NONE);
 		this.size = size;
 	}
-	
-	
+
+	private StringFieldKey(String name, String columnName, boolean nullable, FieldGeneratorType fieldGeneratorType,
+			String defaultValue, int size){
+		super(name, columnName, nullable, fieldGeneratorType, defaultValue);
+		this.size = size;
+	}
+
+	public StringFieldKey withSize(int size){
+		return new StringFieldKey(name, columnName, nullable, fieldGeneratorType, defaultValue, size);
+	}
+
 	/*********************** ByteEncodedField ***********************/
-	
+
 	public static final byte SEPARATOR = 0;
-	
+
 	@Override
 	public boolean isFixedLength(){
 		return false;
 	}
-	
-	
+
 	/**************************** get/set ****************************/
-	
+
 	public int getSize(){
 		return size;
 	}
-	
-	
+
 }
