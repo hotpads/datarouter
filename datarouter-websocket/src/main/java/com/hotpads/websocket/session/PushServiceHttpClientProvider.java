fd5c1da54f0b636272caee5c3215a81883b511e9
cbonsart <cbonsart@hotpads.com>
2016-02-26 11:20:36 -0800

    Revert some server changes regarding csrf token

--- datarouter-websocket/src/main/java/com/hotpads/websocket/session/PushServiceHttpClientProvider.java
+++ datarouter-websocket/src/main/java/com/hotpads/websocket/session/PushServiceHttpClientProvider.java
@@ -11,6 +11,7 @@ import com.hotpads.util.http.security.SignatureValidator;
 public class PushServiceHttpClientProvider implements Provider<HotPadsHttpClient>{
 	public static final String
 			CIPHER_KEY = "JWCnjKR-ASvk+d=PcwX*Kf#M9Xx?kxZs$j5S-4ny",
+			CIPHER_IV = "vPG$?#Q%h7uAj?$wke^$+&f8u_CvJ%+3*HAfnnms",
 			SALT = "MuG=d*SLJtjR=FADCURRRL4vg6uJKC8L4XrnfzM=",
 			API_KEY = "2E^qQf@$Cg-h_MF@e!u9RyqET^MFqNCBd&w*mFx%";
 
@@ -18,7 +19,7 @@ public class PushServiceHttpClientProvider implements Provider<HotPadsHttpClient
 	public HotPadsHttpClient get(){
 		return new HotPadsHttpClientBuilder()
 				.setSignatureValidator(new SignatureValidator(SALT))
-				.setCsrfValidator(new CsrfValidator(CIPHER_KEY))
+				.setCsrfValidator(new CsrfValidator(CIPHER_KEY, CIPHER_IV))
 				.setApiKeyPredicate(new DefaultApiKeyPredicate(API_KEY))
 				.build();
 	}
