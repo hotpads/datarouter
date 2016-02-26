f44477ceb9d549cac7935ce4c24af24d5b668ad1
cguillaume <cguillaume@hotpads.com>
2016-02-26 13:47:36 -0800

    keep sending the old token

--- datarouter-http-client/src/main/java/com/hotpads/util/http/client/HotPadsHttpClient.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/client/HotPadsHttpClient.java
@@ -137,9 +137,10 @@ public class HotPadsHttpClient {
 	private void setSecurityProperties(HotPadsHttpRequest request){
 		Map<String, String> params = new HashMap<>();
 		if (csrfValidator != null) {
-			String csrfIv = CsrfValidator.generateCsrfIv();
-			params.put(SecurityParameters.CSRF_IV, csrfIv);
-			params.put(SecurityParameters.CSRF_TOKEN, csrfValidator.generateCsrfToken(csrfIv));
+			// TODO enable this when all server support the new inputs
+//			String csrfIv = CsrfValidator.generateCsrfIv();
+//			params.put(SecurityParameters.CSRF_IV, csrfIv);
+			params.put(SecurityParameters.CSRF_TOKEN, csrfValidator.generateCsrfToken());
 		}
 		if (apiKeyPredicate != null) {
 			params.put(SecurityParameters.API_KEY, apiKeyPredicate.getApiKey());
