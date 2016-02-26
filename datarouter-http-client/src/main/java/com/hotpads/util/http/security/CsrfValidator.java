193eda20bceb9dd57a127d4f8e6fd743d61c20b2
mdumitriu <mdumitriu@hotpads.com>
2016-02-26 03:29:23 -0800

    fix csrfValidator. AES/CBC requires the IV to be randomly generated and transmitted with the encrypted message.

--- datarouter-http-client/src/main/java/com/hotpads/util/http/security/CsrfValidator.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/security/CsrfValidator.java
@@ -6,6 +6,8 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -20,30 +22,45 @@ public class CsrfValidator{
 
 	private static Logger logger = LoggerFactory.getLogger(CsrfValidator.class.getCanonicalName());
 	private static final String HASHING_ALGORITHM = "SHA-256";
+	// AES/CBC requires IV to be generated for every encrypted message!!
+	// More details here:  https://tools.ietf.org/html/rfc3602
+	// The Encapsulating Security Payload (ESP) payload is made up of the IV and the raw cipher-text.
+	// The IV field MUST be the same size as the block size of the cipher algorithm being used.
+	// The IV MUST be chosen at random, and MUST be unpredictable.
 	private static final String MAIN_CIPHER_ALGORITHM = "AES";
 	private static final String SUB_CIPHER_ALGORITHM = "CBC/PKCS5Padding";
 	private static final String CIPHER_ALGORITHM = MAIN_CIPHER_ALGORITHM + "/" + SUB_CIPHER_ALGORITHM;
 	private static final Long REQUEST_TIMEOUT_IN_MS = 10000L;
 
-	private String cipherKey;
-	private String cipherIv;
+	private final String cipherKey;
 
-	public CsrfValidator(String cipherKey, String cipherIv){
-		this.cipherIv = cipherIv;
+	public CsrfValidator(String cipherKey){
 		this.cipherKey = cipherKey;
 	}
 
-	public boolean check(String token){
-		Long requestTime = getRequestTimeMs(token);
+	public static String generateCsrfIv(){
+		SecureRandom sr;
+		try{
+			sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+		}catch(NoSuchAlgorithmException | NoSuchProviderException e){
+			throw new RuntimeException("error in SecureRandom.getInstance()");
+		}
+		byte[] salt = new byte[16];
+		sr.nextBytes(salt);
+		return Base64.getEncoder().encodeToString(salt);
+	}
+
+	public boolean check(String token, String cipherIv){
+		Long requestTime = getRequestTimeMs(token, cipherIv);
 		if(requestTime == null){
 			return false;
 		}
 		return System.currentTimeMillis() < requestTime + REQUEST_TIMEOUT_IN_MS;
 	}
 
-	public String generateCsrfToken(){
+	public String generateCsrfToken(String cipherIv){
 		try{
-			Cipher aes = getCipher(Cipher.ENCRYPT_MODE);
+			Cipher aes = getCipher(Cipher.ENCRYPT_MODE, cipherIv);
 			return Base64.getEncoder().encodeToString(aes.doFinal(String.valueOf(System.currentTimeMillis())
 					.getBytes()));
 		}catch (Exception e){
@@ -52,9 +69,9 @@ public class CsrfValidator{
 		}
 	}
 
-	public Long getRequestTimeMs(String token){
+	public Long getRequestTimeMs(String token, String cipherIv){
 		try{
-			Cipher aes = getCipher(Cipher.DECRYPT_MODE);
+			Cipher aes = getCipher(Cipher.DECRYPT_MODE, cipherIv);
 			return Long.parseLong(new String(aes.doFinal(Base64.getDecoder().decode(token))));
 		}catch (Exception e){
 			log(e);
@@ -74,7 +91,7 @@ public class CsrfValidator{
 		logger.warn(sw.toString());
 	}
 
-	private Cipher getCipher(int mode)
+	private Cipher getCipher(int mode, String cipherIv)
 	throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException{
 		Cipher aes = Cipher.getInstance(CIPHER_ALGORITHM);
 		aes.init(mode, computeKey(cipherKey), new IvParameterSpec(cipherIv.getBytes(), 0, 16));
