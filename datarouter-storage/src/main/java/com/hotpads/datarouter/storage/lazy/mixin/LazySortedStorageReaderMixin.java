7296a24a50bed015c86c3144e53df74f6bb8ada5
mcorgan <mcorgan@hotpads.com>
2016-03-02 18:19:43 -0800

    use Lazy.of

--- datarouter-storage/src/main/java/com/hotpads/datarouter/storage/lazy/mixin/LazySortedStorageReaderMixin.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/lazy/mixin/LazySortedStorageReaderMixin.java
@@ -18,23 +18,13 @@ extends LazyReader<PK,D,S>{
 
 	@Deprecated
 	public default Lazy<List<D>> getWithPrefix(final PK prefix, final boolean wildcardLastField, final Config config){
-		return new Lazy<List<D>>(){
-			@Override
-			protected List<D> load(){
-				return getBackingStorage().getWithPrefix(prefix, wildcardLastField, config);
-			}
-		};
+		return Lazy.of(() -> { return getBackingStorage().getWithPrefix(prefix, wildcardLastField, config); });
 	}
 
 	@Deprecated
 	public default Lazy<List<D>> getWithPrefixes(final Collection<PK> prefixes, final boolean wildcardLastField,
 			final Config config){
-		return new Lazy<List<D>>(){
-			@Override
-			protected List<D> load(){
-				return getBackingStorage().getWithPrefixes(prefixes, wildcardLastField, config);
-			}
-		};
+		return Lazy.of(() -> { return getBackingStorage().getWithPrefixes(prefixes, wildcardLastField, config); }):
 	}
 
 }
