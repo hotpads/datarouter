65ffd1ca54e0e814e51b91a181573313e2a35f59
mattc <mattc@hotpads.com>
2016-03-21 17:15:29 -0700

    manual merge of moved file

--- datarouter-job/src/main/java/com/hotpads/job/joblet/JobletProcess.java
+++ datarouter-job/src/main/java/com/hotpads/job/joblet/JobletProcess.java
@@ -1,7 +1,11 @@
 package com.hotpads.job.joblet;
 
+import java.util.Collection;
+import java.util.List;
+
 import com.hotpads.config.job.databean.Joblet;
 import com.hotpads.config.job.databean.JobletData;
+import com.hotpads.util.core.stream.StreamTool;
 
 public interface JobletProcess{
 
@@ -11,6 +15,13 @@ public interface JobletProcess{
 	void setJobletData(JobletData jobletData);
 	JobletData getJobletData();
 
+	void setData(String data);
+	String getData();
+
+	default void updateJobletDataIdReference(){
+		getJoblet().setJobletDataId(getJobletData().getId());
+	}
+
     void marshallData();
     void unmarshallData();
     void unmarshallDataIfNotAlready();
@@ -19,5 +30,16 @@ public interface JobletProcess{
     int calculateNumTasks();
 
 	Long process();
+
+
+	/*-------------- static -----------------*/
+
+	static List<Joblet> getJoblets(Collection<? extends JobletProcess> jobletProcesses){
+		return StreamTool.map(jobletProcesses, JobletProcess::getJoblet);
+	}
+
+	static List<JobletData> getJobletDatas(Collection<? extends JobletProcess> jobletProcesses){
+		return StreamTool.map(jobletProcesses, JobletProcess::getJobletData);
+	}
 
 }
