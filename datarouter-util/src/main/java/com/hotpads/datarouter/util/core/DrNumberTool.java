b033d717d0273363ca2b2c2b038c684973c64833
mattc <mattc@hotpads.com>
2016-03-24 13:07:42 -0700

    more cleanup

--- datarouter-util/src/main/java/com/hotpads/datarouter/util/core/DrNumberTool.java
+++ datarouter-util/src/main/java/com/hotpads/datarouter/util/core/DrNumberTool.java
@@ -3,9 +3,7 @@ package com.hotpads.datarouter.util.core;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
-public class DrNumberTool {
+public class DrNumberTool{
 
 	/************************* is this or that methods ************************/
 
@@ -17,36 +15,46 @@ public class DrNumberTool {
 		return !isEmpty(number);
 	}
 
+	// careful, this method is fragile and not even sure if works with BigInteger stuff now
 	public static boolean isNullOrZero(Number number){
-		//careful, this method is fragile and not even sure if works with BigInteger stuff now
-		return number==null || number.equals(0L) || number.equals(0F) || number.equals(0D) || number.intValue()==0;
+		return number == null || number.equals(0L) || number.equals(0F) || number.equals(0D) || number.intValue() == 0;
 	}
 
 	public static boolean isMax(Long number){
-		if(number==null){ return false; }
-		return number==Long.MAX_VALUE;
+		if(number == null) {
+			return false;
+		}
+		return number == Long.MAX_VALUE;
 	}
 
 	public static Long max(Long n1, Long n2){
-		if(n1 == null){ return n2; }
-		if(n2 == null){ return n1; }
+		if(n1 == null) {
+			return n2;
+		}
+		if(n2 == null) {
+			return n1;
+		}
 		return Math.max(n1, n2);
 	}
 
 	/************************ numeric null safe *******************************/
 
 	public static Integer nullSafe(Integer in){
-		if(in==null){ return 0; }
+		if(in == null) {
+			return 0;
+		}
 		return in;
 	}
 
 	public static Long nullSafeLong(Long in, Long defaultValue){
-		if(in==null){ return defaultValue; }
+		if(in == null) {
+			return defaultValue;
+		}
 		return in;
 	}
 
 	public static Long longValue(Number number){
-		if(number == null){
+		if(number == null) {
 			return null;
 		}
 		return number.longValue();
@@ -58,14 +66,13 @@ public class DrNumberTool {
 		return getDoubleNullSafe(toDouble, alternate, false);
 	}
 
-	public static Double getDoubleNullSafe(
-			String toDouble, Double alternate, boolean filterInput){
-		if(toDouble==null) {
+	public static Double getDoubleNullSafe(String toDouble, Double alternate, boolean filterInput){
+		if(toDouble == null) {
 			return alternate;
 		}
-		if(filterInput){
+		if(filterInput) {
 			toDouble = DrStringTool.enforceNumeric(toDouble);
-			if(toDouble==null) {
+			if(toDouble == null) {
 				return alternate;
 			}
 		}
@@ -76,23 +83,22 @@ public class DrNumberTool {
 		}
 	}
 
-	//e.g. For "5.3", it will return 5
+	// e.g. For "5.3", it will return 5
 	public static Integer parseIntegerFromNumberString(String toInteger, Integer alternate){
 		return parseIntegerFromNumberString(toInteger, alternate, false);
 	}
 
-	//e.g. For "5.3", it will return 5
-	public static Integer parseIntegerFromNumberString(
-			String toInteger, Integer alternate, boolean filterInput){
-		Double d = getDoubleNullSafe(toInteger,null,filterInput);
-		if(d==null) {
+	// e.g. For "5.3", it will return 5
+	public static Integer parseIntegerFromNumberString(String toInteger, Integer alternate, boolean filterInput){
+		Double d = getDoubleNullSafe(toInteger, null, filterInput);
+		if(d == null) {
 			return alternate;
 		}
 		return d.intValue();
 	}
 
 	public static Long getLongNullSafe(String toLong, Long alternate){
-		if(toLong==null) {
+		if(toLong == null) {
 			return alternate;
 		}
 		try{
@@ -102,10 +108,9 @@ public class DrNumberTool {
 		}
 	}
 
-
 	/****************************** tests *************************************/
 
-	public static class Tests {
+	public static class Tests{
 		@Test
 		public void testIsNullOrZero(){
 			Byte b = 0;
@@ -121,6 +126,7 @@ public class DrNumberTool {
 			Assert.assertTrue(isNullOrZero(f));
 			Assert.assertTrue(isNullOrZero(d));
 		}
+
 		@Test
 		public void testParseIntegerFromNumberString(){
 			Assert.assertEquals(new Integer(5), parseIntegerFromNumberString("5.5", null));
@@ -134,15 +140,15 @@ public class DrNumberTool {
 			Assert.assertEquals(new Integer(2), parseIntegerFromNumberString("banana", 2));
 			Assert.assertEquals(new Integer(2), parseIntegerFromNumberString("banana", 2));
 			Assert.assertEquals(new Integer(2), parseIntegerFromNumberString("2", 3));
-			Assert.assertEquals(new Integer(Integer.MAX_VALUE),
-					parseIntegerFromNumberString(Integer.MAX_VALUE+"", null));
-			Assert.assertEquals(new Integer(Integer.MIN_VALUE),
-					parseIntegerFromNumberString(Integer.MIN_VALUE+"", null));
+			Assert.assertEquals(new Integer(Integer.MAX_VALUE), parseIntegerFromNumberString(Integer.MAX_VALUE + "",
+					null));
+			Assert.assertEquals(new Integer(Integer.MIN_VALUE), parseIntegerFromNumberString(Integer.MIN_VALUE + "",
+					null));
 
-			Assert.assertEquals(new Integer(400000),
-					parseIntegerFromNumberString("$400,000", null, true));
+			Assert.assertEquals(new Integer(400000), parseIntegerFromNumberString("$400,000", null, true));
 			Assert.assertNull(parseIntegerFromNumberString("$400,000", null, false));
 		}
+
 		@Test
 		public void testCachedIntegers(){
 			Integer foo0 = 1000;
