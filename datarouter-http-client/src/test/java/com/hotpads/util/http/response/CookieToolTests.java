2324ba2211c3b4c6ee8b7c3ab3fb4cdc9055a4df
jdea <jdea@hotpads.com>
2016-04-01 16:53:08 -0700

    CookieTool to find a cookie and value by name

---
+++ datarouter-http-client/src/test/java/com/hotpads/util/http/response/CookieToolTests.java
@@ -0,0 +1,35 @@
+package com.hotpads.util.http.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CookieToolTests {
+
+	private final Cookie shortBreadCookie = new BasicClientCookie("shortBreadCookie", "ThumbPrint");
+	private final Cookie sandwichCookie = new BasicClientCookie("sandwichCookie", "Oreo");
+
+	@Test
+	public void testGetCookie() {
+		List<Cookie> cookies = new ArrayList<>();
+		cookies.add(shortBreadCookie);
+		cookies.add(sandwichCookie);
+
+		Cookie cookie = CookieTool.getCookie(cookies, shortBreadCookie.getName());
+		Assert.assertTrue(shortBreadCookie.getName().equals(cookie.getName()));
+	}
+
+	@Test
+	public void testGetCookieValue() {
+		List<Cookie> cookies = new ArrayList<>();
+		cookies.add(shortBreadCookie);
+
+		String cookieValue = CookieTool.getCookieValue(cookies, shortBreadCookie.getName());
+		Assert.assertTrue(shortBreadCookie.getValue().equals(cookieValue));
+	}
+
+}
