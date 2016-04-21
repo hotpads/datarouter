8aaa364978dcb2fb86debcaf20729f14e0b82ac9
smenon <smenon@hotpads.com>
2016-04-20 17:45:33 -0700

    Create a new enum MysqlRowFormat

---
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/client/imp/jdbc/ddl/domain/MySqlRowFormat.java
@@ -0,0 +1,20 @@
+package com.hotpads.datarouter.client.imp.jdbc.ddl.domain;
+
+import com.hotpads.datarouter.util.core.DrStringTool;
+
+public enum MySqlRowFormat{
+	Compact,
+	Dynamic,
+	Fixed;
+
+ public static MySqlRowFormat parse(String stringValue){
+	 String lowerCase = DrStringTool.toLowerCase(stringValue);
+	 for(MySqlRowFormat rowFormat : values()){
+		 if(rowFormat.toString().equals(lowerCase)){
+			 return rowFormat;
+		 }
+	 }
+	 return null;
+ }
+
+}
