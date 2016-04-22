fd965aeccf0b07eb2276081afa83573cfea604f7
cbonsart <cbonsart@hotpads.com>
2016-04-21 17:00:20 -0700

    Write metrics by batch and reject lost metrics

--- datarouter-storage/src/main/java/com/hotpads/datarouter/profile/metrics/Metrics.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/profile/metrics/Metrics.java
@@ -2,10 +2,10 @@ package com.hotpads.datarouter.profile.metrics;
 
 public interface Metrics{
 
-	void measure(String key, long value);
+	void save(String key, long value);
 
 	default void measure(String key, int value){
-		measure(key, Long.valueOf(value));
+		save(key, Long.valueOf(value));
 	}
 
 }
