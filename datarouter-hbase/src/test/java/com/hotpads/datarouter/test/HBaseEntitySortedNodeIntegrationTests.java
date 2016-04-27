8714bd8cb38077148d82597ef59d019759721e05
manivannana <marivazhagan@hotpads.com>
2016-04-26 17:35:46 -0700

    DATAROUTER-331 HBase Entity Scanner stops before completing (added test)

--- datarouter-hbase/src/test/java/com/hotpads/datarouter/test/HBaseEntitySortedNodeIntegrationTests.java
+++ datarouter-hbase/src/test/java/com/hotpads/datarouter/test/HBaseEntitySortedNodeIntegrationTests.java
@@ -1,13 +1,17 @@
 package com.hotpads.datarouter.test;
 
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import com.hotpads.datarouter.config.Config;
 import com.hotpads.datarouter.node.entity.EntityNode;
-import com.hotpads.datarouter.storage.databean.DatabeanTool;
 import com.hotpads.datarouter.test.node.basic.sorted.BaseSortedNodeIntegrationTests;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBean;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanEntity;
@@ -16,14 +20,6 @@ import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanEntityNode;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeanKey;
 import com.hotpads.datarouter.test.node.basic.sorted.SortedBeans;
 import com.hotpads.datarouter.util.core.DrCollectionTool;
-import com.hotpads.datarouter.util.core.DrIterableTool;
-import com.hotpads.util.core.collections.Range;
-
-import org.testng.Assert;
-import org.testng.AssertJUnit;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class HBaseEntitySortedNodeIntegrationTests extends BaseSortedNodeIntegrationTests{
 
@@ -74,32 +70,6 @@ public class HBaseEntitySortedNodeIntegrationTests extends BaseSortedNodeIntegra
 		Assert.assertEquals(limitedOffset.size(), 1);
 		Assert.assertEquals(limitedOffset.get(0), sortedNode.streamKeysWithPrefix(twoFieldsPk, new Config()
 				.setOffset(offset)).findFirst().get());
-	}
-
-	@Test
-	//this tests scan when there are hbase rows which have 1-1 mapping with datarouter databeans
-	public void testScanForRowDatabean1to1(){
-		Config config = new Config().setIterateBatchSize(2);
-		Iterable<SortedBean> iterable = sortedBeanEntityNode.sortedBean().scan(null, config);
-		long count = DrIterableTool.count(iterable);
-		Assert.assertEquals(count, SortedBeans.TOTAL_RECORDS);
-
-		List<SortedBean> beans = new LinkedList<>();
-		String prefix = "testScanForRowDatabean1to1";
-		for(int i = 1; i< 6; i++){
-			beans.add(new SortedBean(prefix + "-" + i, prefix + "-2-" + i, i, prefix + "-4-" + i,
-					"string so hbase has at least one field", null, null, null));
-		}
-
-		//inserted 5 new rows with 1-1 mapping with datarouter databeans
-		sortedBeanEntityNode.sortedBean().putMulti(beans, null);
-
-		//using the same config with the same batch size
-		iterable = sortedBeanEntityNode.sortedBean().scan(Range.create(null, true, null, false), config);
-		count  = DrIterableTool.count(iterable);
-		Assert.assertEquals(count, SortedBeans.TOTAL_RECORDS + 5);
-
-		sortedNode.deleteMulti(DatabeanTool.getKeys(beans), null);
 	}
 
 }
