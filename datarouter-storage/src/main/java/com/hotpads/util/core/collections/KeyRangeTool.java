5205077399a2babfff97a6ae9e6757b06311a405
cbonsart <cbonsart@hotpads.com>
2016-05-03 17:13:29 -0700

    Extract incrementLastChar method in KeyRangeTool and fix a bug preventing exact match of counter name

--- datarouter-storage/src/main/java/com/hotpads/util/core/collections/KeyRangeTool.java
+++ datarouter-storage/src/main/java/com/hotpads/util/core/collections/KeyRangeTool.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.hotpads.datarouter.client.imp.jdbc.ddl.domain.MySqlColumnType;
 import com.hotpads.datarouter.storage.field.Field;
 import com.hotpads.datarouter.storage.field.imp.StringField;
+import com.hotpads.datarouter.storage.field.imp.StringFieldKey;
 import com.hotpads.datarouter.storage.field.imp.comparable.IntegerField;
+import com.hotpads.datarouter.storage.field.imp.comparable.IntegerFieldKey;
 import com.hotpads.datarouter.storage.field.imp.comparable.LongField;
+import com.hotpads.datarouter.storage.field.imp.comparable.LongFieldKey;
 import com.hotpads.datarouter.storage.key.primary.BasePrimaryKey;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 import com.hotpads.datarouter.util.core.DrStringTool;
@@ -23,19 +25,19 @@ public class KeyRangeTool{
 
 	public static <PK extends PrimaryKey<PK>> Range<PK> forPrefixWithWildcard(String prefixString,
 			KeyWithStringFieldSuffixProvider<PK> keyWithStringFieldSuffixProvider){
-		String startString;
-		String endString;
-		if(DrStringTool.isEmpty(prefixString)){
-			endString = null;
-			startString = null;
-		}else{
-			int lastCharPos = prefixString.length() - 1;
-			startString = prefixString;
-			endString = prefixString.substring(0, lastCharPos) + (char) (prefixString.charAt(lastCharPos) + 1);
-		}
+		String startString = prefixString == null ? null : prefixString;
+		String endString = incrementLastChar(prefixString);
 		PK startKey = keyWithStringFieldSuffixProvider.createWithSuffixStringField(startString);
 		PK endKey = keyWithStringFieldSuffixProvider.createWithSuffixStringField(endString);
 		return new Range<>(startKey,endKey);
+	}
+
+	public static String incrementLastChar(String string){
+		if(DrStringTool.isEmpty(string)){
+			return null;
+		}
+		int lastCharPos = string.length() - 1;
+		return string.substring(0, lastCharPos) + (char) (string.charAt(lastCharPos) + 1);
 	}
 
 	public interface KeyWithStringFieldSuffixProvider<PK extends PrimaryKey<PK>>{
@@ -78,9 +80,9 @@ public class KeyRangeTool{
 			@Override
 			public List<Field<?>> getFields(){
 				return Arrays.asList(
-						new IntegerField("foo", foo),
-						new StringField("bar", bar, MySqlColumnType.MAX_LENGTH_VARCHAR),
-						new LongField("baz", baz));
+						new IntegerField(new IntegerFieldKey("foo"), foo),
+						new StringField(new StringFieldKey("bar"), bar),
+						new LongField(new LongFieldKey("baz"), baz));
 			}
 
 		}
