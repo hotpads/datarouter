7bbfe626ac4b67d3238300f79ac513d25101e12a
cbonsart <cbonsart@hotpads.com>
2016-05-17 15:10:22 -0700

    Create missing fieldkey methods

--- datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/enums/VarIntEnumFieldKey.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/field/imp/enums/VarIntEnumFieldKey.java
@@ -4,9 +4,16 @@ import com.hotpads.datarouter.storage.field.BaseFieldKey;
 import com.hotpads.datarouter.storage.field.enums.IntegerEnum;
 
 public class VarIntEnumFieldKey<E extends IntegerEnum<E>> extends BaseFieldKey<E>{
-	
-	public VarIntEnumFieldKey(String name){
+
+	private final Class<E> enumClass;
+
+	public VarIntEnumFieldKey(String name, Class<E> enumClass){
 		super(name);
+		this.enumClass = enumClass;
 	}
-	
+
+	public Class<E> getEnumClass(){
+		return enumClass;
+	}
+
 }
