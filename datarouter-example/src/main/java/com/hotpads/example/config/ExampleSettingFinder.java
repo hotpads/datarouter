da8a8269a9b39d9cd1406211fb3907aa52b07407
mattc <mcorgan@hotpads.com>
2016-05-25 13:01:15 -0700

    example for adding settings to ExampleSettingsFinder

--- datarouter-example/src/main/java/com/hotpads/example/config/ExampleSettingFinder.java
+++ datarouter-example/src/main/java/com/hotpads/example/config/ExampleSettingFinder.java
@@ -4,10 +4,10 @@ import com.hotpads.setting.cluster.MemorySettingFinder;
 
 public class ExampleSettingFinder extends MemorySettingFinder{
 
-//	@Override
-//	protected void configureSettings(){
-		// Put your config here like this:
+	public ExampleSettingFinder(){
+		super();
+		// add settings here, such as:
 		// settings.put("datarouter.numThreadsForMaxThreadsTest", 500);
-//	}
+	}
 
 }
