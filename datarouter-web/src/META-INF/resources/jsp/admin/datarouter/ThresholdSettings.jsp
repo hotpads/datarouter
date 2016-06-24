554670b75e2a7f599a65c309b795d5139e519cb0
smenon <smenon@hotpads.com>
2016-06-24 10:13:28 -0700

    Threshold page

--- datarouter-web/src/META-INF/resources/jsp/admin/datarouter/ThresholdSettings.jsp
+++ datarouter-web/src/META-INF/resources/jsp/admin/datarouter/ThresholdSettings.jsp
@@ -1,12 +1,31 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ include file="../../generic/prelude-datarouter.jspf"%>
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+	<title>Threshold</title>
+	<%@ include file="/jsp/generic/datarouterHead.jsp" %>
 </head>
 <body>
-
+	<%@ include file="/jsp/menu/common-navbar.jsp" %>
+	<%@ include file="/jsp/menu/dr-navbar.jsp" %>
+	<div class="container">
+		<h2>Set Threshold</h2>		
+		<h3 class="">Routers and Clients</h3>
+		
+		<c:forEach items="${routers}" var="router">			
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			${router['class'].simpleName}
+			<table class="table table-striped table-bordered table-hover table-condensed">					
+				<c:forEach items="${nodeWrappers}" var="nodeWrapper">
+					<tr>
+						<td><a style="color: black;" href="${contextPath}/datarouter/nodes/browseData?submitAction=browseData&routerName=${param.routerName}&nodeName=${nodeWrapper.node.name}">${nodeWrapper.indentHtml}${nodeWrapper.node.name}</a></td>
+						<td><a href="${contextPath}/datarouter/nodes/browseData?submitAction=browseData&routerName=${param.routerName}&nodeName=${nodeWrapper.node.name}">set Threshold</a></td>
+					</tr>
+				</c:forEach>
+			</table>
+			<br/>
+		</c:forEach>
+		
+	</div>
 </body>
 </html>
--- datarouter-web/src/main/java/com/hotpads/handler/ThresholdHandler.java
+++ datarouter-web/src/main/java/com/hotpads/handler/ThresholdHandler.java
@@ -1,15 +1,14 @@
 package com.hotpads.handler;
 
-import javax.inject.Inject;
+import java.util.List;
 
-import org.testng.annotations.Guice;
-import org.testng.annotations.Test;
+import javax.inject.Inject;
 
 import com.hotpads.datarouter.client.DatarouterClients;
 import com.hotpads.datarouter.routing.Datarouter;
 import com.hotpads.datarouter.routing.Router;
-import com.hotpads.datarouter.test.DatarouterStorageTestModuleFactory;
 import com.hotpads.handler.mav.Mav;
+import com.hotpads.handler.util.node.NodeWrapper;
 
 public class ThresholdHandler extends BaseHandler {
 
@@ -42,34 +41,21 @@ public class ThresholdHandler extends BaseHandler {
 	Mav setThreshold(){
 		System.out.println("*************************lala****************");
 		Mav mav = new Mav(JSP_ThresholdSettings);
-		mav.put("serverName", datarouter.getServerName());
-		mav.put("administratorEmail", datarouter.getAdministratorEmail());
+
+
+		for(Router router : datarouter.getRouters()){
+
+			List<NodeWrapper> nodeWrappers = NodeWrapper.getNodeWrappers(router);
+			System.out.println(router+"-"+nodeWrappers.size());
+			for(NodeWrapper node : nodeWrappers){
+				System.out.println("node"+node.getNode().getName());
+			}
+			mav.put("nodeWrappers", nodeWrappers);
+		}
 		mav.put("routers", datarouter.getRouters());
-		mav.put("lazyClientProviderByName", datarouterClients.getLazyClientProviderByName());
-		mav.put("uninitializedClientNames", datarouterClients.getClientNamesByInitialized().get(false));
-		System.out.println("routers "+datarouter.getRouters());
+
+	//	System.out.println("routers "+datarouter.getRouters());
 		return mav;
 	}
-
-
-
-	@Guice(moduleFactory = DatarouterStorageTestModuleFactory.class)
-	public static class ThresholdTests{
-
-		@Inject
-		private Datarouter datarouter;
-
-		@Test
-		public void testthreshold(){
-			int cnt =0;
-			System.out.println(datarouter.getRouters());
-			for(Router router:datarouter.getRouters()){
-				cnt++;
-				System.out.println(cnt+" router "+router);
-			}
-
-		}
-	}
-
 
 }
