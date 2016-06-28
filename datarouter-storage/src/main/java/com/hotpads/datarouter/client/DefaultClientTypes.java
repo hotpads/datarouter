03eeb313f43c83d024d8fe51ee6101e9714e4f38
mattc <mcorgan@hotpads.com>
2016-06-28 11:40:13 -0700

    revert some changes

--- datarouter-storage/src/main/java/com/hotpads/datarouter/client/DefaultClientTypes.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/client/DefaultClientTypes.java
@@ -22,7 +22,6 @@ public class DefaultClientTypes{
 	private DatarouterInjector injector;
 
 	public static final String
-			CLIENT_TYPE_bigtable = "bigtable",
 			CLIENT_TYPE_hbase = "hbase",
 			CLIENT_TYPE_hibernate = "hibernate",
 			CLIENT_TYPE_http = "http",
@@ -35,7 +34,6 @@ public class DefaultClientTypes{
 	//TODO these should eventually be mapped in an external config file, like datarouter-xyz.properties
 	public static final Map<String,String> CLASS_BY_NAME = new TreeMap<>();
 	static{
-		CLASS_BY_NAME.put(CLIENT_TYPE_bigtable, "com.hotpads.datarouter.client.bigtable.BigTableClientType");
 		CLASS_BY_NAME.put(CLIENT_TYPE_hbase, "com.hotpads.datarouter.client.imp.hbase.HBaseClientType");
 		CLASS_BY_NAME.put(CLIENT_TYPE_hibernate, "com.hotpads.datarouter.client.imp.hibernate.HibernateClientType");
 		CLASS_BY_NAME.put(CLIENT_TYPE_http, "com.hotpads.datarouter.client.imp.http.HttpClientType");
@@ -46,13 +44,7 @@ public class DefaultClientTypes{
 	}
 
 	public static final List<String> CLIENTS_IN_CORE_MODULE = DrListTool.createArrayList(
-//			CLIENT_TYPE_hbase,
-//			CLIENT_TYPE_hibernate,
-//			CLIENT_TYPE_http,
-//			CLIENT_TYPE_jdbc,
-//			CLIENT_TYPE_memcached,
-			CLIENT_TYPE_memory
-			);
+			CLIENT_TYPE_memory);
 
 	public ClientType create(String name){
 		return (ClientType)injector.getInstance(ClassTool.forName(CLASS_BY_NAME.get(name)));
