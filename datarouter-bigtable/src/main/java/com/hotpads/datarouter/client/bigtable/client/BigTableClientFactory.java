fe2ed145e8a3cf373e1e7814b5c7e5fbf786b7ab
mattc <mcorgan@hotpads.com>
2016-06-28 16:04:56 -0700

    datarouter-bigtable initial commit

---
+++ datarouter-bigtable/src/main/java/com/hotpads/datarouter/client/bigtable/client/BigTableClientFactory.java
@@ -0,0 +1,31 @@
+package com.hotpads.datarouter.client.bigtable.client;
+
+import java.util.concurrent.ExecutorService;
+
+import org.apache.hadoop.hbase.client.Connection;
+
+import com.google.cloud.bigtable.hbase.BigtableConfiguration;
+import com.hotpads.datarouter.client.availability.ClientAvailabilitySettings;
+import com.hotpads.datarouter.client.imp.hbase.client.BaseHBaseClientFactory;
+import com.hotpads.datarouter.routing.Datarouter;
+
+public class BigTableClientFactory extends BaseHBaseClientFactory{
+
+	private final BigTableOptions bigTableOptions;
+
+	public BigTableClientFactory(Datarouter datarouter, String clientName,
+			ClientAvailabilitySettings clientAvailabilitySettings, ExecutorService executor){
+		super(datarouter, clientName, clientAvailabilitySettings, executor);
+		this.bigTableOptions = new BigTableOptions(multiProperties, clientName);
+	}
+
+	@Override
+	protected Connection makeConnection(){
+		String projectId = bigTableOptions.projectId();
+		String zone = bigTableOptions.zone();
+		String clusterId = bigTableOptions.clusterId();
+		return BigtableConfiguration.connect(projectId, zone, clusterId);
+	}
+
+
+}
---
+++ datarouter-bigtable/src/main/java/com/hotpads/datarouter/client/bigtable/test/BigTableManyFieldIntegrationTester.java
@@ -0,0 +1,21 @@
+package com.hotpads.datarouter.client.bigtable.test;
+
+import org.testng.annotations.BeforeClass;
+
+import com.hotpads.datarouter.test.DrTestConstants;
+import com.hotpads.datarouter.test.node.basic.manyfield.BaseManyFieldIntegrationTests;
+
+//TODO rename to IntegrationTests after it works
+public class BigTableManyFieldIntegrationTester extends BaseManyFieldIntegrationTests{
+
+	@BeforeClass
+	public void beforeClass(){
+		setup(DrTestConstants.CLIENT_drTestBigTable, true);
+	}
+
+	@Override
+	public boolean isHBase(){
+		return true;
+	}
+
+}
