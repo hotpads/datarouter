f7ec7dd91998d1c0ac47ee418ec4fbeea83359d0
mattc <mcorgan@hotpads.com>
2016-07-03 12:13:49 -0700

    add group-node to sqs perf test

--- datarouter-sqs/src/test/java/com/hotpads/datarouter/test/sqs/SqsPerformanceTester.java
+++ datarouter-sqs/src/test/java/com/hotpads/datarouter/test/sqs/SqsPerformanceTester.java
@@ -12,20 +12,25 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
 import com.hotpads.datarouter.client.imp.sqs.config.DatarouterSqsTestModuleFactory;
 import com.hotpads.datarouter.config.Config;
+import com.hotpads.datarouter.node.op.raw.write.QueueStorageWriter;
 import com.hotpads.datarouter.routing.Datarouter;
+import com.hotpads.datarouter.storage.queue.GroupQueueMessage;
+import com.hotpads.datarouter.storage.queue.QueueMessage;
 import com.hotpads.datarouter.test.TestDatabean;
+import com.hotpads.datarouter.test.TestDatabeanKey;
 import com.hotpads.datarouter.util.core.DrCollectionTool;
 import com.hotpads.util.core.concurrent.ExecutorServiceTool;
 import com.hotpads.util.core.iterable.BatchingIterable;
 import com.hotpads.util.core.profile.PhaseTimer;
 
 @Guice(moduleFactory = DatarouterSqsTestModuleFactory.class)
-@Test(singleThreaded=true)
+@Test(singleThreaded = true)
 public class SqsPerformanceTester{
 	private static final Logger logger = LoggerFactory.getLogger(SqsPerformanceTester.class);
 
@@ -34,37 +39,61 @@ public class SqsPerformanceTester{
 	@Inject
 	private SqsTestRouter router;
 
+	@BeforeClass
+	public void beforeClass(){
+		logger.warn("######### drain existing non-group messages #############");
+		drainQueueViaPeek(false, 40);
+		logger.warn("######### drain existing group messages #############");
+		drainQueueViaPeek(true, 40);
+	}
+
 	@AfterClass
-	public void shutdown(){
+	public void afterClas(){
 		datarouter.shutdown();
 	}
 
 	@Test
 	public void testLoadAndDrain(){
-		loadQueue();
-		drainQueue();
+		logger.warn("########### non-group poll ###########");
+		loadQueue(router.testDatabean, 1000);
+		drainQueueViaPoll(40);
+
+		logger.warn("########### non-group via peek/ack ############");
+		loadQueue(router.testDatabean, 1000);
+		drainQueueViaPeek(false, 40);
+
+		logger.warn("########### group via peek/ack ############");
+		loadQueue(router.groupTestDatabean, 1000);
+		drainQueueViaPeek(true, 40);
 	}
 
-	private void loadQueue(){
-		final int numDatabeans = 1000;
+	/*----------------- load ------------------*/
+
+	private void loadQueue(QueueStorageWriter<TestDatabeanKey,TestDatabean> node, int numDatabeans){
+		PhaseTimer timer = new PhaseTimer("putMulti " + node.toString() + " " + numDatabeans);
+		for(List<TestDatabean> batch : new BatchingIterable<>(makeDatabeans(numDatabeans), 100)){
+			node.putMulti(batch, null);
+			logger.warn("put through {}", DrCollectionTool.getLast(batch));
+		}
+		timer.add("loaded");
+		logger.warn(timer.toString() + "@" + timer.getItemsPerSecond(numDatabeans));
+	}
+
+	private List<TestDatabean> makeDatabeans(int numDatabeans){
 		List<TestDatabean> databeans = new ArrayList<>();
 		for(int i = 1; i <= numDatabeans; ++i){
 			databeans.add(new TestDatabean(System.currentTimeMillis() + "_" + i, "asdf", "qwerty"));
 		}
-		PhaseTimer timer = new PhaseTimer("putMulti " + numDatabeans);
-		for(List<TestDatabean> batch : new BatchingIterable<>(databeans, 100)){
-			router.testDatabean.putMulti(batch, null);
-			logger.warn("put through {}", DrCollectionTool.getLast(batch));
-		}
-		logger.warn(timer.toString() + "@" + timer.getItemsPerSecond(numDatabeans));
+		return databeans;
 	}
 
-	private void drainQueue(){
+	/*----------------- drain ------------------*/
+
+	private void drainQueueViaPoll(int numThreads){
 		PhaseTimer timer = new PhaseTimer("drain queue");
-		Config config = new Config().setTimeout(1, TimeUnit.SECONDS);//timeout, otherwise it will poll forever
+		Config config = new Config().setTimeout(3, TimeUnit.SECONDS);// timeout, otherwise it will poll forever
 		AtomicLong numDrained = new AtomicLong(0L);
 		ExecutorService exec = Executors.newCachedThreadPool();
-		final int numThreads = 40;
 		for(int i = 0; i < numThreads; ++i){
 			exec.submit(() -> {
 				for(TestDatabean databean : router.testDatabean.pollUntilEmpty(config)){
@@ -80,6 +109,51 @@ public class SqsPerformanceTester{
 		ExecutorServiceTool.awaitTerminationForever(exec);
 		timer.add("drained " + numDrained.intValue());
 		logger.warn(timer.toString() + "@" + timer.getItemsPerSecond(numDrained.intValue()));
+	}
+
+	private void drainQueueViaPeek(boolean groupNode, int numThreads){
+		PhaseTimer timer = new PhaseTimer("drain queue");
+		Config config = new Config().setTimeout(3, TimeUnit.SECONDS);// timeout, otherwise it will poll forever
+		AtomicLong numDatabeansDrained = new AtomicLong(0L);
+		AtomicLong numMessagesDrained = new AtomicLong(0L);
+		ExecutorService exec = Executors.newCachedThreadPool();
+		for(int i = 0; i < numThreads; ++i){
+			exec.submit(() -> {
+				if(groupNode){
+					for(GroupQueueMessage<TestDatabeanKey,TestDatabean> message : router.groupTestDatabean
+							.peekUntilEmpty(config)){
+						for(TestDatabean databean : message.getDatabeans()){
+							numDatabeansDrained.incrementAndGet();
+							if(numDatabeansDrained.get() % 100 == 0){
+								logger.warn("groupNode={}, drained {}, latest={}", groupNode, numDatabeansDrained.get(),
+										databean.getKey());
+
+							}
+						}
+						router.groupTestDatabean.ack(message.getKey(), null);
+						numMessagesDrained.incrementAndGet();
+					}
+				}else{
+					for(QueueMessage<TestDatabeanKey,TestDatabean> message : router.testDatabean.peekUntilEmpty(
+							config)){
+						TestDatabean databean = message.getDatabean();
+						numDatabeansDrained.incrementAndGet();
+						if(numDatabeansDrained.get() % 100 == 0){
+							logger.warn("groupNode={}, drained {}, latest={}", groupNode, numDatabeansDrained.get(),
+									databean.getKey());
+						}
+						// ack after counting for consistency with group version
+						router.testDatabean.ack(message.getKey(), null);
+						numMessagesDrained.incrementAndGet();
+					}
+				}
+			});
+		}
+		exec.shutdown();
+		ExecutorServiceTool.awaitTerminationForever(exec);
+		timer.add("drained " + numMessagesDrained.intValue() + " messages with " + numDatabeansDrained.intValue()
+				+ " databeans");
+		logger.warn(timer.toString() + "@" + timer.getItemsPerSecond(numDatabeansDrained.intValue()));
 	}
 
 }
