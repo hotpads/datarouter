b72bfb382dd262aa6688938466b480cd96e6e9a7
mattc <mcorgan@hotpads.com>
2016-07-27 17:32:25 -0700

    try readUncommitted isolation

--- datarouter-job/src/main/java/com/hotpads/joblet/dao/JobletRequestDao.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/dao/JobletRequestDao.java
@@ -5,6 +5,7 @@ import javax.inject.Singleton;
 
 import com.google.common.base.Objects;
 import com.hotpads.datarouter.config.Config;
+import com.hotpads.datarouter.config.Isolation;
 import com.hotpads.joblet.JobletNodes;
 import com.hotpads.joblet.databean.JobletRequest;
 import com.hotpads.joblet.databean.JobletRequestKey;
@@ -18,8 +19,9 @@ public class JobletRequestDao{
 
 	public JobletRequest getReservedRequest(JobletType<?> jobletType, String reservedBy){
 		JobletRequestKey prefix = JobletRequestKey.create(jobletType, null, null, null);
-		//keep it small since there should not be thousands of reserved joblets
-		Config config = new Config().setIterateBatchSize(20);
+		Config config = new Config()
+				.setIterateBatchSize(20)//keep it small since there should not be thousands of reserved joblets
+				.setIsolation(Isolation.readUncommitted);
 		return jobletNodes.jobletRequest().streamWithPrefix(prefix, config)
 				.filter(request -> Objects.equal(request.getReservedBy(), reservedBy))
 				.findAny()
