af05eb249a3a01b73466d93a7af86ef27b398008
mattc <mcorgan@hotpads.com>
2016-07-28 15:35:38 -0700

    auto-clean datarouter-http-client

--- datarouter-http-client/src/main/java/com/hotpads/util/http/json/JsonSerializer.java
+++ datarouter-http-client/src/main/java/com/hotpads/util/http/json/JsonSerializer.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Type;
 public interface JsonSerializer{
 
 	public <T> String serialize(T toSerialize);
-	
+
 	public <T> T deserialize(String toDeserialize, Type classOfT);
-	
+
 }
