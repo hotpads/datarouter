087ad3bc3a51136f2934497a414e6ec5bec05523
cbonsart <cbonsart@hotpads.com>
2016-08-01 15:02:59 -0700

    Remove method that creates nodes without fielders

--- datarouter-storage/src/main/java/com/hotpads/datarouter/node/factory/NodeFactory.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/node/factory/NodeFactory.java
@@ -56,16 +56,6 @@ public class NodeFactory{
 
 	/*************** simple helpers *********************/
 
-	//minimum required fields
-	public <PK extends PrimaryKey<PK>,D extends Databean<PK,D>,N extends Node<PK,D>>
-	N create(
-			ClientId clientId,
-			Class<D> databeanClass,
-			Router router,
-			boolean addAdapter){
-		return create(clientId, databeanClass, null, null, router, addAdapter);
-	}
-
 	// +fielderClass
 	public <PK extends PrimaryKey<PK>,D extends Databean<PK,D>,F extends DatabeanFielder<PK,D>,N extends Node<PK,D>>
 	N create(ClientId clientId, Supplier<D> databeanSupplier, Supplier<F> fielderSupplier, Router router,
