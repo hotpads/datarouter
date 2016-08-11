00604cd0dee5d1032d05fbec135b70bde2554a2e
mattc <mcorgan@hotpads.com>
2016-08-11 15:26:32 -0700

    LongRunningTasksHandler cleaning

--- datarouter-job/src/main/java/com/hotpads/job/record/LongRunningTasksHandler.java
+++ datarouter-job/src/main/java/com/hotpads/job/record/LongRunningTasksHandler.java
@@ -12,10 +12,10 @@ import com.hotpads.datarouter.config.Configs;
 import com.hotpads.handler.BaseHandler;
 import com.hotpads.handler.mav.Mav;
 
-//TODO move to datarouter module
 public class LongRunningTasksHandler extends BaseHandler {
 
 	public static final int LRT_DURATION_THRESHOLD_SECONDS = 20;
+
 	public static final String JSP_tasks = "/jsp/admin/datarouter/job/longRunningTasks.jsp";
 
 	@Inject
@@ -26,7 +26,7 @@ public class LongRunningTasksHandler extends BaseHandler {
 	@Handler
 	protected Mav handleDefault(){
 		Mav mav = new Mav(JSP_tasks);
-		Map<String, LongRunningTask> lastCompletions = new HashMap<>();
+		Map<String,LongRunningTask> lastCompletions = new HashMap<>();
 		Iterable<LongRunningTask> tasks = longRunningTaskNodeProvider.get().scan(null, Configs.slaveOk());
 		List<LongRunningTask> currentlyRunningTasks = new ArrayList<>();
 		for(LongRunningTask task : tasks){
@@ -34,9 +34,8 @@ public class LongRunningTasksHandler extends BaseHandler {
 				currentlyRunningTasks.add(task);
 			}
 			if(task.getJobExecutionStatus() == JobExecutionStatus.SUCCESS){
-				if(lastCompletions.get(task.getKey().getJobClass()) == null
-						|| task.getFinishTime().after(lastCompletions.get(task.getKey().getJobClass())
-								.getFinishTime())){
+				if(lastCompletions.get(task.getKey().getJobClass()) == null || task.getFinishTime().after(
+						lastCompletions.get(task.getKey().getJobClass()).getFinishTime())){
 					lastCompletions.put(task.getKey().getJobClass(), task);
 				}
 			}
