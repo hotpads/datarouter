f8e70b73099edbb43f1acfab2b1ecb17e81159e1
mattc <mcorgan@hotpads.com>
2016-08-15 17:22:52 -0700

    createNoOpTracker

--- datarouter-job/src/main/java/com/hotpads/job/record/LongRunningTaskTrackerFactory.java
+++ datarouter-job/src/main/java/com/hotpads/job/record/LongRunningTaskTrackerFactory.java
@@ -5,6 +5,7 @@ import javax.inject.Singleton;
 
 import com.hotpads.datarouter.config.DatarouterProperties;
 import com.hotpads.datarouter.setting.Setting;
+import com.hotpads.datarouter.setting.constant.ConstantBooleanSetting;
 
 @Singleton
 public class LongRunningTaskTrackerFactory {
@@ -24,6 +25,13 @@ public class LongRunningTaskTrackerFactory {
 			LongRunningTaskType type, String triggeredBy){
 		LongRunningTask task = new LongRunningTask(jobClass, datarouterProperties.getServerName(), type, triggeredBy);
 		return new LongRunningTaskTracker(longRunningTaskNodeProvider.get(), task, shouldSaveLongRunningTasks);
+	}
+
+	//TODO do we need the factory for this, or could we have a NoOpLongRunningTaskTracker implementation?
+	public LongRunningTaskTracker createNoOpTracker(String jobClass){
+		LongRunningTask task = new LongRunningTask(jobClass, datarouterProperties.getServerName(),
+				LongRunningTaskType.NOOP, null);
+		return new LongRunningTaskTracker(longRunningTaskNodeProvider.get(), task, new ConstantBooleanSetting(false));
 	}
 
 }
