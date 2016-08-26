9a50354d306611ea168d9e7fd4f7a986e9efd273
cbonsart <cbonsart@hotpads.com>
2016-08-26 13:30:59 -0700

    Use boxed Long in BaseVersioendDatabean

--- datarouter-storage/src/main/java/com/hotpads/datarouter/storage/databean/BaseVersionedDatabean.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/storage/databean/BaseVersionedDatabean.java
@@ -5,15 +5,18 @@ import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 public abstract class BaseVersionedDatabean<PK extends PrimaryKey<PK>,D extends VersionedDatabean<PK,D>>
 extends BaseDatabean<PK,D> implements VersionedDatabean<PK,D>{
 
-	private long version;
+	private Long version;
 
 	@Override
 	public long getVersion(){
-		return version;
+		return version == null ? 0 : version;
 	}
 
 	@Override
 	public void incrementVersion(){
+		if(version == null){
+			version = 0L;
+		}
 		version++;
 	}
 
