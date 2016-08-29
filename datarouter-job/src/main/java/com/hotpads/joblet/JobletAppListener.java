355452c9c712aa1fabc057e3f5257721274d1b13
mattc <mcorgan@hotpads.com>
2016-08-28 19:47:44 -0700

    move JobletAppListener to top-level package; add instructional comment

--- datarouter-job/src/main/java/com/hotpads/joblet/JobletAppListener.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/JobletAppListener.java
@@ -1,9 +1,13 @@
-package com.hotpads.joblet.execute;
+package com.hotpads.joblet;
 
 import javax.inject.Inject;
 
+import com.hotpads.joblet.execute.ParallelJobletProcessors;
 import com.hotpads.listener.DatarouterAppListener;
 
+/*
+ * To run joblets, add this AppListener to your WebApp
+ */
 public class JobletAppListener extends DatarouterAppListener{
 
 	@Inject
