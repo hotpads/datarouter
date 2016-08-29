2f249f1f740f6d6a9da0966fab512bd0a95d5780
mattc <mcorgan@hotpads.com>
2016-08-28 21:45:30 -0700

    delete startup logging

--- datarouter-job/src/main/java/com/hotpads/joblet/JobletAppListener.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/JobletAppListener.java
@@ -2,9 +2,6 @@ package com.hotpads.joblet;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.hotpads.joblet.execute.ParallelJobletProcessors;
 import com.hotpads.listener.DatarouterAppListener;
 
@@ -12,14 +9,12 @@ import com.hotpads.listener.DatarouterAppListener;
  * To run joblets, add this AppListener to your WebApp
  */
 public class JobletAppListener extends DatarouterAppListener{
-	private static final Logger logger = LoggerFactory.getLogger(JobletAppListener.class);
 
 	@Inject
 	private ParallelJobletProcessors parallelJobletProcessors;//don't delete me!  creating me starts the joblets
 
 	@Override
 	protected void onStartUp(){
-		logger.warn("starting {}", parallelJobletProcessors.toString());
 	}
 
 	@Override
