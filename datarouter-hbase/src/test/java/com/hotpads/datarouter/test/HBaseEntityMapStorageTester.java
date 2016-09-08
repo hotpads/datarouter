b3b80096b3349bd33b1f73c28e1cdf3ae0e42b7b
pranavb <pranavb@hotpads.com>
2016-09-08 10:29:21 -0700

    add comment

--- datarouter-hbase/src/test/java/com/hotpads/datarouter/test/HBaseEntityMapStorageTester.java
+++ datarouter-hbase/src/test/java/com/hotpads/datarouter/test/HBaseEntityMapStorageTester.java
@@ -8,6 +8,9 @@ import com.hotpads.datarouter.test.node.basic.map.BaseMapStorageIntegrationTests
 @Guice(moduleFactory=DatarouterStorageTestModuleFactory.class)
 public class HBaseEntityMapStorageTester extends BaseMapStorageIntegrationTests{
 
+	// TODO Debug this. The router/node is not storing any beans in hbase (entity).
+	// Could be issue with MapStorageBeanEntity, MapStorageBeanEntityKey or MapStorageEntityNode
+
 	@BeforeClass
 	public void beforeClass(){
 		setup(DrTestConstants.CLIENT_drTestHBase, true);
