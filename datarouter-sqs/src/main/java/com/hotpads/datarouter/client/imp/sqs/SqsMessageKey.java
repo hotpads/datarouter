ca662e0d0404aed807a005958a5155c50e982541
nathanc <nathanc@hotpads.com>
2016-09-13 16:00:39 -0700

    Add new SqsMessage Databean and key

---
+++ datarouter-sqs/src/main/java/com/hotpads/datarouter/client/imp/sqs/SqsMessageKey.java
@@ -0,0 +1,25 @@
+package com.hotpads.datarouter.client.imp.sqs;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.hotpads.datarouter.storage.field.Field;
+import com.hotpads.datarouter.storage.field.imp.StringField;
+import com.hotpads.datarouter.storage.field.imp.StringFieldKey;
+import com.hotpads.datarouter.storage.key.primary.BasePrimaryKey;
+
+@SuppressWarnings("serial")
+public class SqsMessageKey extends BasePrimaryKey<SqsMessageKey>{
+
+	private String messageId;
+
+	private static class FieldKeys{
+		private static final StringFieldKey messageId = new StringFieldKey("messageId").withColumnName("MessageId");
+	}
+
+	@Override
+	public List<Field<?>> getFields(){
+		return Arrays.asList(new StringField(FieldKeys.messageId, messageId));
+	}
+
+}
