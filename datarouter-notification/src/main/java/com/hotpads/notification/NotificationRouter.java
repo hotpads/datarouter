33a5b795c27222d36d094d98ff4610288fed905b
scottd <scottd@hotpads.com>
2016-09-21 16:51:53 -0700

    Added NIL, destination, and event to NotificationRouter and removed some extra Nodes interfaces

--- datarouter-notification/src/main/java/com/hotpads/notification/NotificationRouter.java
+++ datarouter-notification/src/main/java/com/hotpads/notification/NotificationRouter.java
@@ -12,25 +12,31 @@ import com.hotpads.datarouter.config.DatarouterSettings;
 import com.hotpads.datarouter.node.entity.EntityNodeParams;
 import com.hotpads.datarouter.node.factory.IndexingNodeFactory;
 import com.hotpads.datarouter.node.factory.NodeFactory;
-import com.hotpads.datarouter.node.factory.QueueNodeFactory;
 import com.hotpads.datarouter.node.op.combo.IndexedSortedMapStorage.IndexedSortedMapStorageNode;
-import com.hotpads.datarouter.node.op.combo.SortedMapStorage;
 import com.hotpads.datarouter.node.op.combo.SortedMapStorage.SortedMapStorageNode;
 import com.hotpads.datarouter.node.type.index.UniqueIndexNode;
 import com.hotpads.datarouter.node.type.indexing.IndexingSortedMapStorageNode;
 import com.hotpads.datarouter.node.type.redundant.RedundantSortedMapStorageNode;
 import com.hotpads.datarouter.routing.BaseRouter;
 import com.hotpads.datarouter.routing.Datarouter;
-import com.hotpads.export.DataExportItem;
-import com.hotpads.export.DataExportItem.DataExportItemFielder;
-import com.hotpads.export.DataExportItemKey;
-import com.hotpads.export.DataExportNodes;
+import com.hotpads.notification.databean.NotificationItemLog;
+import com.hotpads.notification.databean.NotificationItemLog.NotificationItemLogFielder;
+import com.hotpads.notification.databean.NotificationItemLogKey;
 import com.hotpads.notification.databean.NotificationLog;
 import com.hotpads.notification.databean.NotificationLog.NotificationLogFielder;
 import com.hotpads.notification.databean.NotificationLogKey;
 import com.hotpads.notification.databean.NotificationRequest;
 import com.hotpads.notification.databean.NotificationRequest.NotificationRequestFielder;
 import com.hotpads.notification.databean.NotificationRequestKey;
+import com.hotpads.notification.destination.NotificationDestination;
+import com.hotpads.notification.destination.NotificationDestination.NotificationDestinationFielder;
+import com.hotpads.notification.destination.NotificationDestinationByAppDeviceId;
+import com.hotpads.notification.destination.NotificationDestinationByAppDeviceId.NotificationDestinationByAppDeviceIdFielder;
+import com.hotpads.notification.destination.NotificationDestinationByAppDeviceIdKey;
+import com.hotpads.notification.destination.NotificationDestinationKey;
+import com.hotpads.notification.itemlog.NotificationItemLogEntity;
+import com.hotpads.notification.itemlog.NotificationItemLogEntityKey;
+import com.hotpads.notification.itemlog.NotificationItemLogPartitioner16;
 import com.hotpads.notification.log.NotificationLogById;
 import com.hotpads.notification.log.NotificationLogById.NotificationLogByIdFielder;
 import com.hotpads.notification.log.NotificationLogByIdEntity;
@@ -43,10 +49,12 @@ import com.hotpads.notification.log.NotificationLogPartitioner16;
 import com.hotpads.notification.request.NotificationRequestEntity;
 import com.hotpads.notification.request.NotificationRequestEntityKey;
 import com.hotpads.notification.request.NotificationRequestPartitioner16;
+import com.hotpads.notification.tracking.NotificationTrackingEvent;
+import com.hotpads.notification.tracking.NotificationTrackingEvent.NotificationTrackingEventFielder;
+import com.hotpads.notification.tracking.NotificationTrackingEventKey;
 
 @Singleton
-public class NotificationRouter extends BaseRouter
-implements DataExportNodes /*, MetricsNodes*/{
+public class NotificationRouter extends BaseRouter{
 
 	private static final String
 		NAME = "notification";
@@ -55,20 +63,23 @@ implements DataExportNodes /*, MetricsNodes*/{
 
 	public static final ClientId
 			CLIENT_hbase1 = new ClientId("hbase1", true),
-			CLIENT_notification = new ClientId(NAME, true);
+			CLIENT_job = new ClientId("job", true),
+			CLIENT_property = new ClientId("property", true),
+			CLIENT_user = new ClientId("user",true),
+			CLIENT_event = new ClientId("event", true);
 
-	private static final List<ClientId> CLIENT_IDS = Arrays.asList(CLIENT_hbase1, CLIENT_notification);
+	private static final List<ClientId> CLIENT_IDS = Arrays.asList(CLIENT_hbase1, CLIENT_job, CLIENT_property,
+			CLIENT_user, CLIENT_event);
 
 	@Override
 	public List<ClientId> getClientIds(){
 		return CLIENT_IDS;
 	}
 
-	public static final String TABLE_JobletRequest = "Joblet";//TODO rename physical table to JobletRequest
-
 	/********************************** nodes **********************************/
 
 	public final SortedMapStorageNode<NotificationRequestKey,NotificationRequest> notificationRequest;
+
 	public final SortedMapStorageNode<NotificationLogKey,NotificationLog> notificationLog;
 	public final IndexedSortedMapStorageNode<NotificationLogKey,NotificationLog> notificationLogMySql;
 	public final SortedMapStorageNode<NotificationLogKey,NotificationLog> notificationLogHbase;
@@ -77,11 +88,24 @@ implements DataExportNodes /*, MetricsNodes*/{
 	public final UniqueIndexNode<NotificationLogKey,NotificationLog,NotificationLogByIdKey,NotificationLogById>
 			notificationLogByIdHbase;
 
-	public final SortedMapStorage<DataExportItemKey,DataExportItem> dataExportItem;
+	public final SortedMapStorageNode<NotificationItemLogKey,NotificationItemLog> notificationItemLog;
+	public final SortedMapStorageNode<NotificationItemLogKey,NotificationItemLog> notificationItemLogMysql;
+	public final SortedMapStorageNode<NotificationItemLogKey,NotificationItemLog> notificationItemLogHbase;
+
+	public final IndexedSortedMapStorageNode<NotificationDestinationKey,NotificationDestination>
+			notificationDestination;
+	public final UniqueIndexNode<NotificationDestinationKey,NotificationDestination,
+			NotificationDestinationByAppDeviceIdKey, NotificationDestinationByAppDeviceId>
+			notificationDestinationByAppDeviceId;
+
+	public final IndexedSortedMapStorageNode<NotificationTrackingEventKey, NotificationTrackingEvent>
+			notificationTrackingEvent;
+
+	//TODO public final SortedMapStorage<NotificationPreferenceKey,NotificationPreference> notificationPreference;
 
 	@Inject
 	public NotificationRouter(Datarouter datarouter, DatarouterProperties datarouterProperties, NodeFactory nodeFactory,
-			QueueNodeFactory queueNodeFactory, DatarouterSettings datarouterSettings){
+			DatarouterSettings datarouterSettings){
 		super(datarouter, datarouterProperties.getConfigPath(), NAME, nodeFactory, datarouterSettings);
 
 		EntityNodeParams<NotificationRequestEntityKey,NotificationRequestEntity> nodeParam = new EntityNodeParams<>(
@@ -99,7 +123,7 @@ implements DataExportNodes /*, MetricsNodes*/{
 				NotificationRequestFielder::new,
 				NotificationRequestEntity.QUALIFIER_PREFIX_NOTIFICATION_REQUEST));
 
-		notificationLogMySql = createAndRegister(CLIENT_notification, NotificationLog::new, NotificationLogFielder::new);
+		notificationLogMySql = createAndRegister(CLIENT_job, NotificationLog::new, NotificationLogFielder::new);
 
 		notificationLogById = notificationLogMySql.registerManaged(IndexingNodeFactory.newManagedUnique(this,
 				notificationLogMySql, NotificationLogByIdFielder::new, NotificationLogById::new, false,
@@ -139,21 +163,43 @@ implements DataExportNodes /*, MetricsNodes*/{
 		IndexingSortedMapStorageNode<NotificationLogKey,NotificationLog,NotificationLogFielder,
 				SortedMapStorageNode<NotificationLogKey,NotificationLog>> indexingNode = IndexingNodeFactory
 				.newSortedMap(rawNotificationLogHbase);
-		notificationLogHbase = indexingNode;
 		indexingNode.registerIndexListener(IndexingNodeFactory.newUniqueListener(NotificationLogById::new, indexNode));
-		notificationLogByIdHbase = IndexingNodeFactory.newManualUnique(notificationLogHbase, indexNode);
 
+		notificationLogHbase = indexingNode;
+		notificationLogByIdHbase = IndexingNodeFactory.newManualUnique(notificationLogHbase, indexNode);
 		notificationLog = register(new RedundantSortedMapStorageNode<>(NotificationLog::new, this,
 				Arrays.asList(notificationLogMySql, notificationLogHbase), notificationLogHbase));
 
-		dataExportItem = register(nodeFactory.create(CLIENT_notification, DataExportItem.class, DataExportItemFielder.class,
-				this, true));
-	}
+		this.notificationItemLogMysql = createAndRegister(CLIENT_property, NotificationItemLog::new,
+				NotificationItemLogFielder::new);
 
-	/*------------ DataExportNodes ---------------*/
+		EntityNodeParams<NotificationItemLogEntityKey,NotificationItemLogEntity> nilNodeParam = new EntityNodeParams<>(
+				"NotificationItemLogEntity16",
+				NotificationItemLogEntityKey.class,
+				NotificationItemLogEntity.class,
+				NotificationItemLogPartitioner16.class,
+				"NotificationItemLogEntity16");
 
-	@Override
-	public SortedMapStorage<DataExportItemKey,DataExportItem> getDataExportItem(){
-		return dataExportItem;
+		this.notificationItemLogHbase = register(nodeFactory.subEntityNode(
+				this,
+				nilNodeParam,
+				CLIENT_hbase1,
+				NotificationItemLog::new,
+				NotificationItemLogFielder::new,
+				NotificationItemLogEntity.QUALIFIER_PREFIX_NOTIFICATION_ITEM_LOG));
+
+		this.notificationItemLog = notificationItemLogHbase;
+
+		notificationDestination = register(nodeFactory.create(CLIENT_property, NotificationDestination.class,
+				NotificationDestinationFielder.class, this, true));
+		notificationDestinationByAppDeviceId = notificationDestination.registerManaged(IndexingNodeFactory
+				.newManagedUnique(this, notificationDestination, NotificationDestinationByAppDeviceIdFielder.class,
+						NotificationDestinationByAppDeviceId.class, false));
+
+		notificationTrackingEvent = register(nodeFactory.create(CLIENT_event, NotificationTrackingEvent.class,
+				NotificationTrackingEventFielder.class, this, true));
+
+//		notificationPreference = register(nodeFactory.create(CLIENT_user, NotificationPreference::new,
+//				NotificationPreferenceFielder::new, this, true));
 	}
 }
