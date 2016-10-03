fd98c684eb1120f2806ab22617987673ed8f2ae2
scottd <scottd@hotpads.com>
2016-10-03 13:59:48 -0700

    Revert "Revert "Merge pull request #1050 in HOT/datarouter from scottd/make-NotificationRouter/IN-1168 to master""
    This reverts commit a99e2b05dd37acc52535017e0061b2744b927ce5.

---
+++ datarouter-notification/src/main/java/com/hotpads/notification/preference/NotificationPreferenceKey.java
@@ -0,0 +1,51 @@
+package com.hotpads.notification.preference;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.hotpads.datarouter.storage.field.Field;
+import com.hotpads.datarouter.storage.field.imp.StringField;
+import com.hotpads.datarouter.storage.field.imp.StringFieldKey;
+import com.hotpads.datarouter.storage.key.primary.BasePrimaryKey;
+
+@SuppressWarnings("serial")
+public class NotificationPreferenceKey extends BasePrimaryKey<NotificationPreferenceKey>{
+
+	public static class FieldKeys{
+		public static final StringFieldKey userToken = new StringFieldKey("userToken");
+		public static final StringFieldKey deviceGroup = new StringFieldKey("deviceGroup");
+		public static final StringFieldKey typeGroup = new StringFieldKey("typeGroup");
+	}
+
+	private String userToken;
+	private NotificationDeviceGroup deviceGroup;
+	private NotificationTypeGroup typeGroup;
+
+	@Override
+	public List<Field<?>> getFields(){
+		return Arrays.asList(
+				new StringField(FieldKeys.userToken, userToken),
+				new StringField(FieldKeys.deviceGroup, deviceGroup.persistentString),
+				new StringField(FieldKeys.typeGroup, typeGroup.persistentString));
+	}
+
+	public NotificationPreferenceKey(String userToken, NotificationDeviceGroup deviceGroup, NotificationTypeGroup
+			typeGroup){
+		this.userToken = userToken;
+		this.deviceGroup = deviceGroup;
+		this.typeGroup = typeGroup;
+	}
+
+	public NotificationPreferenceKey(){
+		this(null, new NotificationDeviceGroup(null), new NotificationTypeGroup(null));
+	}
+
+	public NotificationDeviceGroup getDeviceGroup(){
+		return deviceGroup;
+	}
+
+	public NotificationTypeGroup getTypeGroup(){
+		return typeGroup;
+	}
+
+}
