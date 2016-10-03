b89fc166004a1f2327c1c037c08d8c803ff7a469
mattc <mcorgan@hotpads.com>
2016-10-03 17:09:41 -0400

    vanilla runnable

--- datarouter-storage/src/main/java/com/hotpads/datarouter/conveyor/GroupQueueConveyor.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/conveyor/GroupQueueConveyor.java
@@ -1,9 +1,11 @@
 package com.hotpads.datarouter.conveyor;
 
+import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.hotpads.datarouter.node.op.raw.GroupQueueStorage;
 import com.hotpads.datarouter.node.op.raw.MapStorage;
@@ -16,13 +18,15 @@ public class GroupQueueConveyor<
 		PK extends PrimaryKey<PK>,
 		D extends Databean<PK,D>>
 implements Runnable{
+	private static final Logger logger = LoggerFactory.getLogger(GroupQueueConveyor.class);
+
+	private static final Duration SLEEP_DURATION = Duration.ofSeconds(5);
 
 	private final Setting<Boolean> shouldRunSetting;
 	private final GroupQueueStorage<PK,D> groupQueueStorage;
 	private final MapStorage<PK,D> mapStorage;
 
-	private final ScheduledExecutorService exec;
-
+	private final AtomicBoolean requestShutdown;
 	private long numDatabeansDrained = 0;
 
 
@@ -31,35 +35,41 @@ implements Runnable{
 		this.shouldRunSetting = shouldRunSetting;
 		this.groupQueueStorage = groupQueueStorage;
 		this.mapStorage = mapStorage;
-
-		this.exec = Executors.newSingleThreadScheduledExecutor();
-		exec.scheduleWithFixedDelay(this, 0, 5, TimeUnit.SECONDS);
+		this.requestShutdown = new AtomicBoolean(false);
 	}
 
 
 	@Override
 	public void run(){
-		for(GroupQueueMessage<PK,D> message : groupQueueStorage.peekUntilEmpty(null)){
-			List<D> databeans = message.getDatabeans();
-			mapStorage.putMulti(databeans, null);
-			numDatabeansDrained += databeans.size();
-			groupQueueStorage.ack(message.getKey(), null);
-			if(!shouldRun()){
-				break;
+		while(shouldRun()){
+			try{
+				for(GroupQueueMessage<PK,D> message : groupQueueStorage.peekUntilEmpty(null)){
+					List<D> databeans = message.getDatabeans();
+					mapStorage.putMulti(databeans, null);
+					numDatabeansDrained += databeans.size();
+					groupQueueStorage.ack(message.getKey(), null);
+					if(!shouldRun()){
+						break;
+					}
+				}
+			}catch(Exception e){
+				logger.error("", e);
+			}finally{
+				try{
+					Thread.sleep(SLEEP_DURATION.toMillis());
+				}catch(InterruptedException e){
+					Thread.currentThread().isInterrupted();//clear flag
+				}
 			}
 		}
 	}
 
 	public void shutdown(){
-		exec.shutdown();
-		try{
-			exec.awaitTermination(5, TimeUnit.SECONDS);
-		}catch(InterruptedException e){
-		}
+		requestShutdown.set(true);
 	}
 
 	private boolean shouldRun(){
-		return !exec.isShutdown() && !Thread.currentThread().isInterrupted() && shouldRunSetting.getValue();
+		return shouldRunSetting.getValue() && !requestShutdown.get();
 	}
 
 }
