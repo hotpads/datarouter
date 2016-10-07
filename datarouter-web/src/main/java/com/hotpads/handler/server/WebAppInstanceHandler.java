9dfc2f5da4b43e322e520d45430bccc66dec5a23
scottd <scottd@hotpads.com>
2016-10-07 16:08:52 -0700

    Moved web app dao and instance jobs to dr-core and made a couple interfaces to enable all subprojects to implement/bind the jobs and settings

--- datarouter-web/src/main/java/com/hotpads/handler/server/WebAppInstanceHandler.java
+++ datarouter-web/src/main/java/com/hotpads/handler/server/WebAppInstanceHandler.java
@@ -4,26 +4,21 @@ import java.util.Collection;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.hotpads.datarouter.util.core.DrListTool;
 import com.hotpads.handler.BaseHandler;
 import com.hotpads.handler.mav.Mav;
-import com.hotpads.server.WebAppInstanceRouter;
+import com.hotpads.server.WebAppInstanceNodes;
 import com.hotpads.server.databean.WebAppInstance;
 
 public class WebAppInstanceHandler extends BaseHandler{
-	private static final Logger logger = LoggerFactory.getLogger(WebAppInstanceHandler.class);
-
 	@Inject
-	WebAppInstanceRouter webAppInstanceRouter;
+	private WebAppInstanceNodes webAppNodes;
 
 	@Override
 	protected Mav handleDefault(){
 		Mav mav = new Mav("/jsp/server/webApps.jsp");
 
-		Collection<WebAppInstance> webApps = DrListTool.createArrayList(webAppInstanceRouter.webApp.scan(null, null));
+		Collection<WebAppInstance> webApps = DrListTool.createArrayList(webAppNodes.getWebApps().scan(null, null));
 		mav.put("webAppInstances", webApps);
 
 		return mav;
