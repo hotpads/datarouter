9b974246671a35a55e1c7b92155968cb70033bd0
mattc <mcorgan@hotpads.com>
2016-10-10 17:09:42 -0700

    add Trace conveyors

--- datarouter-storage/src/main/java/com/hotpads/trace/node/TraceNodes.java
+++ datarouter-storage/src/main/java/com/hotpads/trace/node/TraceNodes.java
@@ -1,5 +1,6 @@
 package com.hotpads.trace.node;
 
+import com.hotpads.datarouter.node.op.raw.GroupQueueStorage;
 import com.hotpads.datarouter.node.op.raw.write.StorageWriter;
 import com.hotpads.trace.Trace;
 import com.hotpads.trace.TraceSpan;
@@ -7,11 +8,11 @@ import com.hotpads.trace.TraceThread;
 import com.hotpads.trace.key.TraceKey;
 import com.hotpads.trace.key.TraceSpanKey;
 import com.hotpads.trace.key.TraceThreadKey;
+import com.hotpads.util.core.exception.NotImplementedException;
 
 public interface TraceNodes extends TraceWriteNodes{
 
-	TraceEntityNode traceEntity();
-
+	//write databeans to these; default to the persistent store
 	@Override
 	default StorageWriter<TraceKey,Trace> traceWriteQueue(){
 		return traceEntity().trace();
@@ -26,5 +27,23 @@ public interface TraceNodes extends TraceWriteNodes{
 	default StorageWriter<TraceSpanKey,TraceSpan> traceSpanWriteQueue(){
 		return traceEntity().span();
 	}
+
+
+	//if writing to a queue, use these to read from the queue
+	default GroupQueueStorage<TraceKey,Trace> traceReadQueue(){
+		throw new NotImplementedException("no traceReadQueue configured");
+	}
+
+	default GroupQueueStorage<TraceThreadKey,TraceThread> traceThreadReadQueue(){
+		throw new NotImplementedException("no traceThreadReadQueue configured");
+	}
+
+	default GroupQueueStorage<TraceSpanKey,TraceSpan> traceSpanReadQueue(){
+		throw new NotImplementedException("no traceSpanReadQueue configured");
+	}
+
+
+	//persistent storage
+	TraceEntityNode traceEntity();
 
 }
