ea0a4a248a6cbb720b0109327130e9f7e13fcae0
cbonsart <cbonsart@hotpads.com>
2016-10-17 11:48:02 -0700

    Generate sequential ExceptionRecord ids

--- datarouter-web/src/main/java/com/hotpads/handler/exception/ExceptionRecordKey.java
+++ datarouter-web/src/main/java/com/hotpads/handler/exception/ExceptionRecordKey.java
@@ -7,8 +7,11 @@ import com.hotpads.datarouter.storage.field.Field;
 import com.hotpads.datarouter.storage.field.imp.StringField;
 import com.hotpads.datarouter.storage.field.imp.StringFieldKey;
 import com.hotpads.datarouter.storage.key.primary.BasePrimaryKey;
+import com.hotpads.util.core.number.RandomTool;
 
 public class ExceptionRecordKey extends BasePrimaryKey<ExceptionRecordKey> {
+
+	private static final long ID_TIME_MULTIPLIER = 1000000;
 
 	private String id;
 
@@ -29,6 +32,12 @@ public class ExceptionRecordKey extends BasePrimaryKey<ExceptionRecordKey> {
 
 	public String getId() {
 		return id;
+	}
+
+	public static ExceptionRecordKey generate(){
+		Long id = System.currentTimeMillis() * ID_TIME_MULTIPLIER
+				+ RandomTool.nextPositiveLong() % ID_TIME_MULTIPLIER;
+		return new ExceptionRecordKey(id.toString());
 	}
 
 }
