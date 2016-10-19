7e8b3c407e773edc5aebc7e8bef054afd9da2f9e
cbonsart <cbonsart@hotpads.com>
2016-10-18 18:41:28 -0700

    consolidate test setting finders

--- datarouter-storage/src/main/java/com/hotpads/datarouter/test/DatarouterStorageTestGuiceModule.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/test/DatarouterStorageTestGuiceModule.java
@@ -4,15 +4,15 @@ import com.google.inject.AbstractModule;
 import com.hotpads.datarouter.config.DatarouterProperties;
 import com.hotpads.datarouter.config.DatarouterSettings;
 import com.hotpads.datarouter.config.NoOpDatarouterSettings;
+import com.hotpads.datarouter.setting.MemorySettingFinder;
 import com.hotpads.datarouter.setting.SettingFinder;
-import com.hotpads.datarouter.setting.TestSettingFinder;
 
 public class DatarouterStorageTestGuiceModule extends AbstractModule{
 
 	@Override
 	protected void configure(){
 		bind(DatarouterSettings.class).to(NoOpDatarouterSettings.class);
-		bind(SettingFinder.class).to(TestSettingFinder.class);
+		bind(SettingFinder.class).to(MemorySettingFinder.class);
 		bind(DatarouterProperties.class).to(TestDatarouterProperties.class);
 	}
 
