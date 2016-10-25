b4476e272c83bef1e91aae9d42d1a8b9c1685ade
mattc <mcorgan@hotpads.com>
2016-10-24 18:01:23 -0700

    pass a null startAtPriority for now, optimize later

--- datarouter-job/src/main/java/com/hotpads/joblet/execute/ParallelJobletProcessor.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/execute/ParallelJobletProcessor.java
@@ -118,7 +118,8 @@ public class ParallelJobletProcessor{
 
 	private final JobletPackage getJobletPackage(long counter){
 		String reservedBy = getReservedByString(counter);
-		JobletRequest jobletRequest = jobletService.getJobletRequestForProcessing(jobletType, reservedBy);
+		//TODO pass a later startAtPriority if the first queue(s) are repeatedly empty
+		JobletRequest jobletRequest = jobletService.getJobletRequestForProcessing(jobletType, null, reservedBy);
 		if(jobletRequest == null){
 			return null;
 		}
