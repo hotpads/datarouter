1cd3086a1b4c1dae8e1424f4335bffab83c34f7e
cguillaume <cguillaume@hotpads.com>
2016-10-31 19:53:11 -0700

    Add basic ExceptionHandling

--- datarouter-notification/src/main/java/com/hotpads/handler/exception/NoOpExceptionHandlingConfig.java
+++ datarouter-notification/src/main/java/com/hotpads/handler/exception/NoOpExceptionHandlingConfig.java
@@ -1,15 +1,25 @@
 package com.hotpads.handler.exception;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
 
 import com.hotpads.exception.analysis.ExceptionDto;
+import com.hotpads.handler.user.role.DatarouterUserRole;
+import com.hotpads.handler.user.session.DatarouterSession;
+import com.hotpads.handler.user.session.DatarouterSessionManager;
 import com.hotpads.notification.type.NotificationType;
 
+@Singleton
 public class NoOpExceptionHandlingConfig implements ExceptionHandlingConfig{
+
+	@Inject
+	private DatarouterSessionManager sessionManager;
 
 	@Override
 	public boolean shouldDisplayStackTrace(HttpServletRequest request, Exception exception){
-		return false;
+		DatarouterSession session = sessionManager.getFromRequest(request);
+		return DatarouterUserRole.isSessionAdmin(session);
 	}
 
 	@Override
@@ -24,7 +34,7 @@ public class NoOpExceptionHandlingConfig implements ExceptionHandlingConfig{
 
 	@Override
 	public String getHtmlErrorMessage(Exception exception){
-		return null;
+		return "Error";
 	}
 
 	@Override
--- datarouter-web/src/main/java/com/hotpads/handler/user/role/DatarouterUserRole.java
+++ datarouter-web/src/main/java/com/hotpads/handler/user/role/DatarouterUserRole.java
@@ -1,12 +1,13 @@
 package com.hotpads.handler.user.role;
 
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.hotpads.datarouter.storage.field.enums.DatarouterEnumTool;
 import com.hotpads.datarouter.storage.field.enums.StringEnum;
 import com.hotpads.handler.user.DatarouterUser;
+import com.hotpads.handler.user.session.DatarouterSession;
 
 public enum DatarouterUserRole
 implements StringEnum<DatarouterUserRole>{
@@ -16,39 +17,49 @@ implements StringEnum<DatarouterUserRole>{
 	user("user"),
 	apiUser("apiUser");
 
-	private String persistentString;
+	private final String persistentString;
 
 	private DatarouterUserRole(String persistentString){
 		this.persistentString = persistentString;
 	}
 
+	public static boolean isUserAdmin(DatarouterUser user){
+		return isAdmin(user.getRoles());
+	}
 
-	/**************** StringEnum *******************/
+	public static boolean isSessionAdmin(DatarouterSession session){
+		return isAdmin(session.getRoles());
+	}
 
-	public static boolean isUserAdmin(DatarouterUser user) {
-		List<DatarouterUserRole> roles = user.getRoles();
+	private static boolean isAdmin(Collection<DatarouterUserRole> roles){
 		return roles.contains(datarouterAdmin) || roles.contains(admin);
 	}
 
-	public static Set<DatarouterUserRole> fromStringArray(String[] userRoles) {
+	public static Set<DatarouterUserRole> fromStringArray(String[] userRoles){
 		Set<DatarouterUserRole> userRolesSet = new HashSet<>();
-		if (userRoles == null) { return userRolesSet; }
+		if(userRoles == null){
+			return userRolesSet;
+		}
 
-		for(String role : userRoles) {
-			DatarouterUserRole r = DatarouterEnumTool.getEnumFromString(values(), role, null);
-			if(r != null) {
-				userRolesSet.add(r);
+		for(String roleString : userRoles){
+			DatarouterUserRole role = DatarouterEnumTool.getEnumFromString(values(), roleString, null);
+			if(role != null){
+				userRolesSet.add(role);
 			}
 		}
 		return userRolesSet;
 	}
 
-	public static Set<DatarouterUserRole> getPermissibleRolesForUser(DatarouterUser datarouterUser, boolean isSelf) {
+	public static Set<DatarouterUserRole> getPermissibleRolesForUser(DatarouterUser datarouterUser, boolean isSelf){
 		Set<DatarouterUserRole> userRoles = new HashSet<>(datarouterUser.getRoles());
-		if(isSelf && userRoles.contains(datarouterAdmin)) { return userRoles; }
+		if(isSelf && userRoles.contains(datarouterAdmin)){
+			return userRoles;
+		}
 
 		userRoles.remove(datarouterAdmin);
-		if(userRoles.contains(admin)) { return userRoles; }
+		if(userRoles.contains(admin)){
+			return userRoles;
+		}
 
 		userRoles.remove(admin);
 		return userRoles;
@@ -60,7 +71,8 @@ implements StringEnum<DatarouterUserRole>{
 	}
 
 	@Override
-	public DatarouterUserRole fromPersistentString(String s){
-		return DatarouterEnumTool.getEnumFromString(values(), s, null);
+	public DatarouterUserRole fromPersistentString(String str){
+		return DatarouterEnumTool.getEnumFromString(values(), str, null);
 	}
+
 }
