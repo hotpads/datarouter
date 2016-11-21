c844dc931fde177713ed4cf66f883fc59d50efbb
mattc <mcorgan@hotpads.com>
2016-11-21 15:41:04 -0800

    move JobletCounterJob cluster setting to datarouter

--- datarouter-job/src/main/java/com/hotpads/joblet/setting/JobletSettings.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/setting/JobletSettings.java
@@ -28,6 +28,7 @@ extends SettingNode{
 	public final Setting<Integer> memoryTickets;
 	private final Setting<String> queueMechanism;
 	public final Setting<Integer> processorVersion;
+	public final Setting<Boolean> runJobletCounterJob;
 
 
 	@Inject
@@ -53,6 +54,7 @@ extends SettingNode{
 		queueMechanism = registerString("queueMechanism", JobletQueueMechanism.JDBC_LOCK_FOR_UPDATE
 				.getPersistentString());
 		processorVersion = registerInteger("processorVersion", 1);
+		runJobletCounterJob = registerBoolean("runJobletCounterJob", false);
 	}
 
 	/*------------------ methods -----------------------*/
