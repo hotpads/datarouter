00fcf4a130e8ceea86ceeb84e558cecd18249195
smenon <smenon@hotpads.com>
2016-12-05 14:15:40 -0800

    - rename userInfo

--- datarouter-web/src/main/java/com/hotpads/handler/user/session/DatarouterCurrentUserInfo.java
+++ datarouter-web/src/main/java/com/hotpads/handler/user/session/DatarouterCurrentUserInfo.java
@@ -4,24 +4,16 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
 
-import com.hotpads.handler.user.DatarouterUser;
-import com.hotpads.handler.user.DatarouterUser.DatarouterUserByUserTokenLookup;
-import com.hotpads.handler.user.DatarouterUserNodes;
-
 @Singleton
 public class DatarouterCurrentUserInfo implements CurrentUserInfo{
 
 	@Inject
 	private DatarouterSessionManager sessionManager;
-	@Inject
-	private DatarouterUserNodes userNodes;
 
 	@Override
 	public String getEmail(HttpServletRequest request){
-		String userToken = sessionManager.getUserTokenFromCookie(request);
-		DatarouterUser user = userNodes.getUserNode().lookupUnique(new DatarouterUserByUserTokenLookup(userToken),
-				null);
-		return user.getUsername();
+		DatarouterSession session = sessionManager.getFromRequest(request).get();
+		return session.getUsername();
 	}
 
 }
