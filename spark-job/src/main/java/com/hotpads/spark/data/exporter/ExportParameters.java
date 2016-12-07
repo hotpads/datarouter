e1585349010f0756fd7cac8e24b2145d557bfe41
winstonq <winstonq@hotpads.com>
2016-12-07 10:31:19 -0800

    RDA-124: revert hibernate flag removal, to be addressed in sifferent story

--- spark-job/src/main/java/com/hotpads/spark/data/exporter/ExportParameters.java
+++ spark-job/src/main/java/com/hotpads/spark/data/exporter/ExportParameters.java
@@ -11,15 +11,18 @@ public class ExportParameters<PK extends PrimaryKey<PK>,D extends Databean<PK, D
 	private final String tableName;
 	private final String columnNameCsv;
 	private final int hoursToRedownload;
+	private final boolean isHibernateTable;
 	private final SortedStorageReader<PK, D> sortedStorageReader;
 
 	public ExportParameters(Class<D> databeanClass, Class<DatabeanFielder<PK, D>> fielderClass, String tableName,
-			String columnNameCsv, SortedStorageReader<PK, D> sortedStorageReader, int hoursToRedownload){
+			String columnNameCsv, SortedStorageReader<PK, D> sortedStorageReader, int hoursToRedownload,
+			boolean isHibernateTable){
 		this.databeanClass = databeanClass;
 		this.fielderClass = fielderClass;
 		this.tableName = tableName;
 		this.columnNameCsv = columnNameCsv;
 		this.hoursToRedownload = hoursToRedownload;
+		this.isHibernateTable = isHibernateTable;
 		this.sortedStorageReader = sortedStorageReader;
 	}
 
@@ -43,6 +46,10 @@ public class ExportParameters<PK extends PrimaryKey<PK>,D extends Databean<PK, D
 		return hoursToRedownload;
 	}
 
+	public boolean isHibernateTable(){
+		return isHibernateTable;
+	}
+
 	public SortedStorageReader<PK, D> getSortedStorageReader(){
 		return sortedStorageReader;
 	}
@@ -50,7 +57,7 @@ public class ExportParameters<PK extends PrimaryKey<PK>,D extends Databean<PK, D
 	@Override
 	public String toString(){
 		return "databeanClass=" + getDatabeanClass().getName() + "|fielderClass=" + getFielderClass().getName()
-				+ "|tableName=" + getTableName() + "|columnNameCsv=" + getColumnNameCsv() + "|hoursToRedownload="
-				+ getHoursToRedownload();
+				+ "|tableName=" + getTableName() + "|isHibernateTable=" + isHibernateTable()
+				+ "|columnNameCsv=" + getColumnNameCsv() + "|hoursToRedownload=" + getHoursToRedownload();
 	}
 }
