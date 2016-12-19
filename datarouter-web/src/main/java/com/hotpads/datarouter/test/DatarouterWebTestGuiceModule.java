8ed9edf40dd35e80e7e5bf830b9bcfe69b60deac
scottd <scottd@hotpads.com>
2016-12-19 11:37:12 -0800

    More fine tuning to DefaultDecoder deserialization for empty strings/nulls. Also added a test for it, which required moving around/adding some guice modules

---
+++ datarouter-web/src/main/java/com/hotpads/datarouter/test/DatarouterWebTestGuiceModule.java
@@ -0,0 +1,19 @@
+package com.hotpads.datarouter.test;
+
+import com.google.inject.AbstractModule;
+import com.hotpads.datarouter.config.DatarouterProperties;
+import com.hotpads.datarouter.config.DatarouterSettings;
+import com.hotpads.datarouter.config.NoOpDatarouterSettings;
+import com.hotpads.datarouter.setting.MemorySettingFinder;
+import com.hotpads.datarouter.setting.SettingFinder;
+
+public class DatarouterWebTestGuiceModule extends AbstractModule{
+
+	@Override
+	protected void configure(){
+		bind(DatarouterSettings.class).to(NoOpDatarouterSettings.class);
+		bind(SettingFinder.class).to(MemorySettingFinder.class);
+		bind(DatarouterProperties.class).to(TestDatarouterProperties.class);
+	}
+
+}
