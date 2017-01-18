c99e7cd11c8f3849c0a76e56c3b36006d61fda48
scottd <scottd@hotpads.com>
2017-01-18 15:49:15 -0800

    WIP (need to re-add some tests) for feedback. Changed the way Base64RangeGenerator handles its arguments and remade the orders according to all the charset/collation combinations we currently use

--- datarouter-util/src/main/java/com/hotpads/util/core/collections/Base64RangeGenerator.java
+++ datarouter-util/src/main/java/com/hotpads/util/core/collections/Base64RangeGenerator.java
@@ -8,31 +8,43 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Base64RangeGenerator{
-	private static final String[] CASE_SENSITIVE_ORDER = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-			"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g",
-			"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1",
-			"2", "3", "4", "5", "6", "7", "8", "9", "-", "_"};
+	private static final String[] UTF8_BIN_AND_UTF8MB4_BIN_ORDER = {"-", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+			"9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+			"U", "V", "W", "X", "Y", "Z", "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+			"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-	private static final String[] CASE_INSENSITIVE_ORDER = {"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f",
-			"G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q",
-			"q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "0", "1",
-			"2", "3", "4", "5", "6", "7", "8", "9", "-", "_"};
+	private static final String[] UTF8MB4_UNICODE_CI_ORDER = {"_", "-", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+			"9", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j",
+			"K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U",
+			"u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"};
 
-	/**
-	 * See {@link Base64RangeGenerator#generateRangesOfDepth(int, boolean)}. Assumes case sensitivity.
-	 */
-	public static Iterable<Range<String>> generateRangesOfDepth(int depth){
-		return generateRangesOfDepth(depth, true);
-	}
+	private static final String[] LATIN1_SWEDISH_CI_ORDER = {"-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a",
+			"A", "B", "b", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "H", "h", "i", "I", "J", "j", "k", "K",
+			"L", "l", "m", "M", "n", "N", "o", "O", "P", "p", "Q", "q", "r", "R", "S", "s", "t", "T", "u", "U", "V",
+			"v", "w", "W", "X", "x", "Y", "y", "z", "Z", "_"};
 
 	/**
 	 * Generates Ranges to equally partition all possible Base64 values
 	 * @param depth length of prefixes to use in the generated keys
 	 * @param caseSensitive if true, letter order is A-Za-z; if false, letter order is AaBb, etc.
-	 * @return the Iterable will contain 64^depth ranges, with the first starting at A*depth, and the last going to null
+	 * @return the Iterable will contain 64^depth ranges, with the first starting at null, and the last ending at null
 	 */
-	public static Iterable<Range<String>> generateRangesOfDepth(int depth, boolean caseSensitive){
-		String[] starts = generateRangeStarts(depth, caseSensitive ? CASE_SENSITIVE_ORDER : CASE_INSENSITIVE_ORDER);
+	public static Iterable<Range<String>> generateRangesOfDepth(int depth, String charset, String collation){
+		String[] starts;
+		switch(charset + collation){
+		case "utf8utf8_bin":
+		case "utf8mb4utf8mb4_bin":
+			starts = generateRangeStarts(depth, UTF8_BIN_AND_UTF8MB4_BIN_ORDER);
+			break;
+		case "utf8mb4utf8mb4_unicode_ci":
+			starts = generateRangeStarts(depth, UTF8MB4_UNICODE_CI_ORDER);
+			break;
+		case "latin1latin1_swedish_ci":
+			starts = generateRangeStarts(depth, LATIN1_SWEDISH_CI_ORDER);
+			break;
+		default:
+			throw new RuntimeException("Unknown charset, collation combination: " + charset + ", " + collation);
+		}
 		return rangify(starts);
 	}
 
@@ -55,7 +67,8 @@ public class Base64RangeGenerator{
 
 	private static Iterable<Range<String>> rangify(String[] starts){
 		List<Range<String>> ranges = new ArrayList<>();
-		for(int i = 0; i < starts.length - 1; i++){
+		ranges.add(new Range<String>(null, starts[1]));
+		for(int i = 1; i < starts.length - 1; i++){
 			ranges.add(new Range<String>(starts[i], starts[i + 1]));
 		}
 		ranges.add(new Range<String>(starts[starts.length - 1]));//from last start to null
@@ -65,45 +78,41 @@ public class Base64RangeGenerator{
 	public class Tests{
 		@Test
 		public void testBaseCase(){
-			Assert.assertEquals(generateRangeStarts(0, CASE_SENSITIVE_ORDER), CASE_SENSITIVE_ORDER);
-			Assert.assertEquals(generateRangeStarts(1, CASE_SENSITIVE_ORDER), CASE_SENSITIVE_ORDER);
-			Assert.assertNotEquals(generateRangeStarts(2, CASE_SENSITIVE_ORDER), CASE_SENSITIVE_ORDER);
+			Assert.assertEquals(generateRangeStarts(0, UTF8_BIN_AND_UTF8MB4_BIN_ORDER), UTF8_BIN_AND_UTF8MB4_BIN_ORDER);
+			Assert.assertEquals(generateRangeStarts(1, UTF8_BIN_AND_UTF8MB4_BIN_ORDER), UTF8_BIN_AND_UTF8MB4_BIN_ORDER);
+			Assert.assertNotEquals(generateRangeStarts(2, UTF8_BIN_AND_UTF8MB4_BIN_ORDER),
+					UTF8_BIN_AND_UTF8MB4_BIN_ORDER);
 
-			Assert.assertEquals(generateRangeStarts(0, CASE_INSENSITIVE_ORDER), CASE_INSENSITIVE_ORDER);
-			Assert.assertEquals(generateRangeStarts(1, CASE_INSENSITIVE_ORDER), CASE_INSENSITIVE_ORDER);
-			Assert.assertNotEquals(generateRangeStarts(2, CASE_INSENSITIVE_ORDER), CASE_INSENSITIVE_ORDER);
+			Assert.assertEquals(generateRangeStarts(0, UTF8MB4_UNICODE_CI_ORDER), UTF8MB4_UNICODE_CI_ORDER);
+			Assert.assertEquals(generateRangeStarts(1, UTF8MB4_UNICODE_CI_ORDER), UTF8MB4_UNICODE_CI_ORDER);
+			Assert.assertNotEquals(generateRangeStarts(2, UTF8MB4_UNICODE_CI_ORDER), UTF8MB4_UNICODE_CI_ORDER);
+
+			Assert.assertEquals(generateRangeStarts(0, LATIN1_SWEDISH_CI_ORDER), LATIN1_SWEDISH_CI_ORDER);
+			Assert.assertEquals(generateRangeStarts(1, LATIN1_SWEDISH_CI_ORDER), LATIN1_SWEDISH_CI_ORDER);
+			Assert.assertNotEquals(generateRangeStarts(2, LATIN1_SWEDISH_CI_ORDER), LATIN1_SWEDISH_CI_ORDER);
 		}
 
 		@Test
-		public void testDepth2(){
-			String[] sensitive = generateRangeStarts(2, CASE_SENSITIVE_ORDER);
-			String[] insensitive = generateRangeStarts(2, CASE_INSENSITIVE_ORDER);
-			Assert.assertEquals(sensitive.length, insensitive.length);
-
-			String firstExpected = "AA";
-			String secondSensitiveExpected = "AB";
-			String secondInsensitiveExpected = "Aa";
-			String lastExpected = "__";
-
-			Assert.assertEquals(sensitive[0], firstExpected);
-			Assert.assertEquals(insensitive[0], firstExpected);
-
-			Assert.assertEquals(sensitive[1], secondSensitiveExpected);
-			Assert.assertEquals(insensitive[1], secondInsensitiveExpected);
-
-			Assert.assertEquals(sensitive[sensitive.length - 1], lastExpected);
-			Assert.assertEquals(insensitive[insensitive.length - 1], lastExpected);
-		}
-
-		@Test
-		public void testOverloading(){
-			Assert.assertEquals(generateRangesOfDepth(0), generateRangesOfDepth(0, true));
+		public void testArgParsing(){
+			Assert.assertEquals(generateRangesOfDepth(0,"utf8","utf8_bin"), rangify(UTF8_BIN_AND_UTF8MB4_BIN_ORDER));
+			Assert.assertEquals(generateRangesOfDepth(0,"utf8mb4","utf8mb4_bin"), rangify(
+					UTF8_BIN_AND_UTF8MB4_BIN_ORDER));
+			Assert.assertEquals(generateRangesOfDepth(0,"utf8mb4","utf8mb4_unicode_ci"), rangify(
+					UTF8MB4_UNICODE_CI_ORDER));
+			Assert.assertEquals(generateRangesOfDepth(0,"latin1","latin1_swedish_ci"), rangify(
+					LATIN1_SWEDISH_CI_ORDER));
+			try{
+				Assert.assertEquals(generateRangesOfDepth(0,"char","col"), rangify(UTF8_BIN_AND_UTF8MB4_BIN_ORDER));
+				Assert.fail();
+			}catch(RuntimeException e){
+				Assert.assertEquals(e.getMessage(), "Unknown charset, collation combination: char, col");
+			}
 		}
 
 		@Test
 		public void testRangify(){
 			String[] testIn = {"0", "1", "2", "3"};
-			List<Range<String>> expected = Arrays.asList(new Range<String>("0", "1"), new Range<String>("1","2"),
+			List<Range<String>> expected = Arrays.asList(new Range<String>(null, "1"), new Range<String>("1","2"),
 					new Range<String>("2", "3"), new Range<String>("3", null));
 			Assert.assertEquals(rangify(testIn).iterator(), expected.iterator());
 		}
