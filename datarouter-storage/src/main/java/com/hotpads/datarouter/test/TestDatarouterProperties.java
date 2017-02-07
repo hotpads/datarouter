c0aa9371166c22fe5097bded158f9f0857e6bc0c
cguillaume <cguillaume@hotpads.com>
2017-02-07 15:37:26 -0800

    simplify datarouter setup, cluster setting config and properties.

--- datarouter-storage/src/main/java/com/hotpads/datarouter/test/TestDatarouterProperties.java
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/test/TestDatarouterProperties.java
@@ -1,29 +1,16 @@
 package com.hotpads.datarouter.test;
 
-import java.util.Objects;
-
 import javax.inject.Singleton;
 
-import com.hotpads.datarouter.config.BaseDatarouterProperties;
-import com.hotpads.datarouter.setting.StandardServerType;
+import com.hotpads.datarouter.config.DatarouterProperties;
 
 @Singleton
-public class TestDatarouterProperties extends BaseDatarouterProperties{
+public class TestDatarouterProperties extends DatarouterProperties{
 
 	private static final String CONFIG_PATH = "/hotpads/config/datarouter-test.properties";
 
 	public TestDatarouterProperties(){
 		super(CONFIG_PATH);
-	}
-
-	@Override
-	public String getConfigPath(){
-		return CONFIG_PATH;
-	}
-
-	@Override
-	public StandardServerType getServerType(){
-		return StandardServerType.fromPersistentStringStatic(Objects.requireNonNull(getServerTypeString()));
 	}
 
 }
