5e1728b79f58dd295d6fd63a0e6be19d63aa0459
mattc <mcorgan@hotpads.com>
2017-02-27 19:47:14 -0800

    BaseJobletThreadCountSettings

---
+++ datarouter-job/src/main/java/com/hotpads/joblet/setting/BaseJobletThreadCountSettings.java
@@ -0,0 +1,44 @@
+package com.hotpads.joblet.setting;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import com.hotpads.datarouter.app.WebAppName;
+import com.hotpads.datarouter.setting.Setting;
+import com.hotpads.datarouter.setting.SettingFinder;
+import com.hotpads.datarouter.setting.SettingNode;
+import com.hotpads.joblet.type.JobletType;
+import com.hotpads.joblet.type.JobletTypeFactory;
+
+public class BaseJobletThreadCountSettings extends SettingNode{
+
+	private final Map<JobletType<?>,Setting<Integer>> settingByJobletType = new HashMap<>();
+
+	public BaseJobletThreadCountSettings(SettingFinder finder, WebAppName webAppName,
+			JobletTypeFactory jobletTypeFactory, String nodeName, int defaultNumThreads){
+		super(finder, webAppName + ".joblet." + nodeName + ".", webAppName + ".joblet.");
+
+		for(JobletType<?> jobletType : jobletTypeFactory.getAllTypes()){
+			Setting<Integer> setting = registerSetting(jobletType, jobletType.getPersistentString(), defaultNumThreads);
+			settingByJobletType.put(jobletType, setting);
+		}
+	}
+
+	/*-------------- methods ----------------------*/
+
+	public Setting<Integer> registerSetting(JobletType<?> jobletType, String name, Integer defaultValue){
+		Setting<Integer> setting = registerInteger(name, defaultValue);
+		settingByJobletType.put(jobletType, setting);
+		return setting;
+	}
+
+	public Setting<Integer> getSettingForJobletType(JobletType<?> type){
+		return settingByJobletType.get(type);
+	}
+
+	public int getCountForJobletType(JobletType<?> type){
+		return Optional.ofNullable(settingByJobletType.get(type).getValue()).orElse(0);
+	}
+
+}
--- datarouter-job/src/main/java/com/hotpads/joblet/setting/JobletThreadCountSettings.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/setting/JobletThreadCountSettings.java
@@ -1,54 +1,24 @@
 package com.hotpads.joblet.setting;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import com.hotpads.datarouter.app.WebAppName;
-import com.hotpads.datarouter.setting.Setting;
 import com.hotpads.datarouter.setting.SettingFinder;
-import com.hotpads.datarouter.setting.SettingNode;
 import com.hotpads.joblet.execute.JobletProcessor;
-import com.hotpads.joblet.type.JobletType;
 import com.hotpads.joblet.type.JobletTypeFactory;
 
 @Singleton
-public class JobletThreadCountSettings extends SettingNode{
+public class JobletThreadCountSettings extends BaseJobletThreadCountSettings{
 
 	public static final String NAME = "threadCount";
 
 	private static final int DEFAULT_NUM_THREADS = JobletProcessor.ENABLE_CLUSTER_THREAD_LIMITS ? 1 : 0;
 
-	private final Map<JobletType<?>,Setting<Integer>> settingByJobletType = new HashMap<>();
 
 	@Inject
 	public JobletThreadCountSettings(SettingFinder finder, WebAppName webAppName, JobletTypeFactory jobletTypeFactory){
-		super(finder, webAppName + ".joblet.threadCount.", webAppName + ".joblet.");
-
-		for(JobletType<?> jobletType : jobletTypeFactory.getAllTypes()){
-			Setting<Integer> setting = registerSetting(jobletType, jobletType.getPersistentString(),
-					DEFAULT_NUM_THREADS);
-			settingByJobletType.put(jobletType, setting);
-		}
-	}
-
-	/*-------------- methods ----------------------*/
-
-	public Setting<Integer> registerSetting(JobletType<?> jobletType, String name, Integer defaultValue){
-		Setting<Integer> setting = registerInteger(name, defaultValue);
-		settingByJobletType.put(jobletType, setting);
-		return setting;
-	}
-
-	public Setting<Integer> getSettingForJobletType(JobletType<?> type){
-		return settingByJobletType.get(type);
-	}
-
-	public int getCountForJobletType(JobletType<?> type){
-		return Optional.ofNullable(settingByJobletType.get(type).getValue()).orElse(0);
+		super(finder, webAppName, jobletTypeFactory, NAME, DEFAULT_NUM_THREADS);
 	}
 
 }
