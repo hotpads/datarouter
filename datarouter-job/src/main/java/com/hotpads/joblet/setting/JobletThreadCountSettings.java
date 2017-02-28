114a395a623fe95493d5a272276f8e16a5339f01
mattc <mcorgan@hotpads.com>
2017-02-27 19:52:12 -0800

    finish removing

--- datarouter-job/src/main/java/com/hotpads/joblet/setting/JobletThreadCountSettings.java
+++ datarouter-job/src/main/java/com/hotpads/joblet/setting/JobletThreadCountSettings.java
@@ -5,7 +5,6 @@ import javax.inject.Singleton;
 
 import com.hotpads.datarouter.app.WebAppName;
 import com.hotpads.datarouter.setting.SettingFinder;
-import com.hotpads.joblet.execute.JobletProcessor;
 import com.hotpads.joblet.type.JobletTypeFactory;
 
 @Singleton
@@ -13,12 +12,10 @@ public class JobletThreadCountSettings extends BaseJobletThreadCountSettings{
 
 	public static final String NAME = "threadCount";
 
-	private static final int DEFAULT_NUM_THREADS = JobletProcessor.ENABLE_CLUSTER_THREAD_LIMITS ? 1 : 0;
-
 
 	@Inject
 	public JobletThreadCountSettings(SettingFinder finder, WebAppName webAppName, JobletTypeFactory jobletTypeFactory){
-		super(finder, webAppName, jobletTypeFactory, NAME, DEFAULT_NUM_THREADS);
+		super(finder, webAppName, jobletTypeFactory, NAME, 1);
 	}
 
 }
