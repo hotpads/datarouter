d85d2266cb01834d5bd4cdd675dc50063d03b92a
cbonsart <cbonsart@hotpads.com>
2017-03-06 10:28:04 -0800

    Commit RequestBodyString

---
+++ datarouter-web/src/main/java/com/hotpads/handler/types/RequestBodyString.java
@@ -0,0 +1,11 @@
+package com.hotpads.handler.types;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(PARAMETER)
+public @interface RequestBodyString{}
