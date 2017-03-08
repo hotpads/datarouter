b560b09251d67a9d2a0e139cdfcbc91a090339ba
cguillaume <cguillaume@hotpads.com>
2017-03-07 19:18:23 -0800

    working ssl

--- datarouter-bigtable/src/main/java/com/hotpads/datarouter/client/bigtable/test/BigTableOpenSslTests.java
+++ datarouter-bigtable/src/main/java/com/hotpads/datarouter/client/bigtable/test/BigTableOpenSslTests.java
@@ -1,44 +1,24 @@
 package com.hotpads.datarouter.client.bigtable.test;
 
-import org.apache.tomcat.jni.Library;
-import org.apache.tomcat.jni.SSL;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.netty.handler.ssl.OpenSslEngine;
-import io.netty.util.internal.NativeLibraryLoader;
+import com.google.cloud.bigtable.grpc.BigtableSession;
 
+import io.netty.handler.ssl.OpenSsl;
 
 public class BigTableOpenSslTests{
-	private static final Logger logger = LoggerFactory.getLogger(BigTableOpenSslTests.class);
-
-//	@Test
-	public void testOpenSslAvailable(){
-		io.netty.handler.ssl.OpenSsl.ensureAvailability();
-	}
-
-//	@Test
-	public void testRepackagedOpenSslAvailable(){
-		com.google.bigtable.repackaged.io.netty.handler.ssl.OpenSsl.ensureAvailability();
-	}
-
-//	@Test
-	public void testManualLibraryLoad(){
-        try {
-            NativeLibraryLoader.load("netty-tcnative", SSL.class.getClassLoader());
-            Library.initialize("provided");
-            SSL.initialize(null);
-        } catch (Throwable t) {
-            logger.warn(
-                    "Failed to load netty-tcnative; " +
-                            OpenSslEngine.class.getSimpleName() + " will be unavailable.", t);
-        }
-	}
 
 	@Test
-	public void testNettyTcnative(){
-//        NativeLibraryLoader.load("netty-tcnative", SSL.class.getClassLoader());
-		System.loadLibrary("netty-tcnative");
+	public void testOpenSslAvailable(){
+		if(!BigtableSession.isAlpnProviderEnabled()){
+			if(OpenSsl.isAvailable()){
+				// Should not happen
+				Assert.fail("big table alpn provider is not enable even if open ssl is available");
+			}else{
+				Assert.fail("open ssl is not available because: " + OpenSsl.unavailabilityCause());
+			}
+		}
 	}
+
 }
