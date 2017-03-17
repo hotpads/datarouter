bbd015f5084b75774afee6df0ed0258c2366b826
cguillaume <cguillaume@hotpads.com>
2017-03-16 17:09:17 -0700

    add logger to group conveyor

--- datarouter-job/src/main/java/com/hotpads/conveyor/queue/GroupQueueConveyor.java
+++ datarouter-job/src/main/java/com/hotpads/conveyor/queue/GroupQueueConveyor.java
@@ -3,6 +3,9 @@ package com.hotpads.conveyor.queue;
 import java.time.Duration;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hotpads.conveyor.BaseConveyor;
 import com.hotpads.conveyor.ConveyorCounters;
 import com.hotpads.datarouter.config.Config;
@@ -12,11 +15,13 @@ import com.hotpads.datarouter.setting.Setting;
 import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 import com.hotpads.datarouter.storage.queue.GroupQueueMessage;
+import com.hotpads.datarouter.util.core.DrCollectionTool;
 
 public class GroupQueueConveyor<
 		PK extends PrimaryKey<PK>,
 		D extends Databean<PK,D>>
 extends BaseConveyor<PK,D>{
+	private static final Logger logger = LoggerFactory.getLogger(GroupQueueConveyor.class);
 
 	private static final Duration PEEK_TIMEOUT = Duration.ofSeconds(5);
 	private static final Config PEEK_CONFIG = new Config().setTimeoutMs(PEEK_TIMEOUT.toMillis());
@@ -36,13 +41,20 @@ extends BaseConveyor<PK,D>{
 	@Override
 	public ProcessBatchResult processBatch(){
 		GroupQueueMessage<PK,D> message = groupQueueStorage.peek(PEEK_CONFIG);
-		if(GroupQueueMessage.isEmpty(message)){
+		if(message == null){
+			logger.info("peeked conveyor={} nullMessage", name);
 			return new ProcessBatchResult(false);
 		}
 		List<D> databeans = message.getDatabeans();
+		logger.info("peeked conveyor={} messageCount={}", name, databeans.size());
+		if(DrCollectionTool.isEmpty(databeans)){
+			return new ProcessBatchResult(false);
+		}
 		storageWriter.putMulti(databeans, null);
+		logger.info("wrote conveyor={} messageCount={}", name, databeans.size());
 		ConveyorCounters.incPutMultiOpAndDatabeans(this, databeans.size());
 		groupQueueStorage.ack(message.getKey(), null);
+		logger.info("acked conveyor={} messageCount={}", name, databeans.size());
 		ConveyorCounters.incAck(this);
 		return new ProcessBatchResult(true);
 	}
