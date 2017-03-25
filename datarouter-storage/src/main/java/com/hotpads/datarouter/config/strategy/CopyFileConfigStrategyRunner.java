7e45e9b276d88ea09b1f472ed235e90544957ec0
mattc <mcorgan@hotpads.com>
2017-03-25 00:30:06 -0700

    extract CopyFileConfigStrategyRunner in datarouter-storage

---
+++ datarouter-storage/src/main/java/com/hotpads/datarouter/config/strategy/CopyFileConfigStrategyRunner.java
@@ -0,0 +1,41 @@
+package com.hotpads.datarouter.config.strategy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Files;
+
+public abstract class CopyFileConfigStrategyRunner implements ConfigStrategyRunner{
+	private static final Logger logger = LoggerFactory.getLogger(CopyFileConfigStrategyRunner.class);
+
+	private final String sourceLocation;
+	private final String destinationFileName;
+
+
+	public CopyFileConfigStrategyRunner(String sourceLocation, String destinationFileName){
+		this.sourceLocation = sourceLocation;
+		this.destinationFileName = destinationFileName;
+	}
+
+
+	@Override
+	public void configure(Optional<String> configDirectory){
+		File sourceFile = new File(sourceLocation);
+		File destinationFile = new File(configDirectory + "/" + destinationFileName);
+		if(destinationFile.exists()){
+			logger.warn("replacing {} with {}", destinationFile.getAbsolutePath(), sourceFile.getAbsolutePath());
+		}else{
+			logger.warn("creating {} from {}", destinationFile.getAbsolutePath(), sourceFile.getAbsolutePath());
+		}
+		try{
+			Files.copy(sourceFile, destinationFile);
+		}catch(IOException e){
+			throw new RuntimeException(e);
+		}
+	}
+
+}
--- datarouter-web/src/main/java/com/hotpads/datarouter/config/strategy/CopyWebappFileConfigStrategyRunner.java
+++ datarouter-web/src/main/java/com/hotpads/datarouter/config/strategy/CopyWebappFileConfigStrategyRunner.java
@@ -1,45 +1,12 @@
 package com.hotpads.datarouter.config.strategy;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.io.Files;
 import com.hotpads.datarouter.app.ApplicationPaths;
 
-public class CopyWebappFileConfigStrategyRunner implements ConfigStrategyRunner{
-	private static final Logger logger = LoggerFactory.getLogger(CopyWebappFileConfigStrategyRunner.class);
-
-	private final ApplicationPaths applicationPaths;
-	private final String sourceFileLocation;
-	private final String destinationFileLocation;
-
+public abstract class CopyWebappFileConfigStrategyRunner extends CopyFileConfigStrategyRunner{
 
 	public CopyWebappFileConfigStrategyRunner(ApplicationPaths applicationPaths, String sourceFileLocation,
-			String destinationFileLocation){
-		this.applicationPaths = applicationPaths;
-		this.sourceFileLocation = sourceFileLocation;
-		this.destinationFileLocation = destinationFileLocation;
-	}
-
-
-	@Override
-	public void configure(Optional<String> configDirectory){
-		File sourceFile = new File(applicationPaths.getResourcesPath() + "/" + sourceFileLocation);
-		File destinationFile = new File(configDirectory + "/" + destinationFileLocation);
-		if(destinationFile.exists()){
-			logger.warn("replacing {} with {}", destinationFile.getAbsolutePath(), sourceFile.getAbsolutePath());
-		}else{
-			logger.warn("creating {} from {}", destinationFile.getAbsolutePath(), sourceFile.getAbsolutePath());
-		}
-		try{
-			Files.copy(sourceFile, destinationFile);
-		}catch(IOException e){
-			throw new RuntimeException(e);
-		}
+			String destinationFileName){
+		super(applicationPaths.getResourcesPath() + "/" + sourceFileLocation, destinationFileName);
 	}
 
 }
