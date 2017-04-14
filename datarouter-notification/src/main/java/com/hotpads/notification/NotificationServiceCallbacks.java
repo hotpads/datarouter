5f39d0472fb0d7965d8c7db2d31fec55c6518312
scottd <scottd@hotpads.com>
2017-04-13 17:15:26 -0700

    Removed unnecessary callback and enums (was keeping them in case they were needed in merging)

--- datarouter-notification/src/main/java/com/hotpads/notification/NotificationServiceCallbacks.java
+++ datarouter-notification/src/main/java/com/hotpads/notification/NotificationServiceCallbacks.java
@@ -4,12 +4,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
-import com.hotpads.notification.databean.NotificationLog;
 import com.hotpads.notification.databean.NotificationRequest;
 import com.hotpads.notification.databean.NotificationUserId;
 import com.hotpads.notification.destination.NotificationDestination;
 import com.hotpads.notification.destination.NotificationDestinationAppName;
-import com.hotpads.notification.tracking.NotificationTrackingEventType;
 
 //TODO some batching/asynchronicity should probably be a part of this once it's actually over web
 public interface NotificationServiceCallbacks{
@@ -17,9 +15,6 @@ public interface NotificationServiceCallbacks{
 	List<NotificationRequest> filterOutIrrelevantNotificationRequests(String notificationTypeString,
 			List<NotificationRequest> originalRequests);
 	void onSuccess(String notificationTypeString, List<NotificationRequest> requests);
-	//TODO locate use after merge
-	void onTrackingEvent(String notificationTypeString, NotificationTrackingEventType eventType,
-			NotificationLog notificationLog);
 
 	//other services
 	Set<NotificationDestination> getActiveDestinations(NotificationUserId userId,
