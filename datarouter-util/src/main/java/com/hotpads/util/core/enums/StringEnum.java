f718fab8594ff814d8064c75cc5e7df2ebf23204
davidpe <dpeters@hotpads.com>
2017-04-26 19:15:08 -0700

    log bad enum persistence instead of throwing an exception

--- datarouter-util/src/main/java/com/hotpads/util/core/enums/StringEnum.java
+++ datarouter-util/src/main/java/com/hotpads/util/core/enums/StringEnum.java
@@ -1,7 +1,12 @@
 package com.hotpads.util.core.enums;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface StringEnum<E>
 extends Comparable<E>, PersistentString{
+	static final Logger logger = LoggerFactory.getLogger(StringEnum.class);
+
 	E fromPersistentString(String string);
 
 	static <E extends StringEnum<E>> E fromPersistentStringSafe(E sampleValue, String persistentString){
@@ -10,8 +15,9 @@ extends Comparable<E>, PersistentString{
 		}
 		E enumValue = sampleValue.fromPersistentString(persistentString);
 		if(enumValue == null || !persistentString.equals(enumValue.getPersistentString())){
-			throw new RuntimeException(sampleValue.getClass().getSimpleName() + ".fromPersistentString returned "
-					+ enumValue == null ? "null" : enumValue.getPersistentString() + " instead of " + persistentString);
+			logger.error(sampleValue.getClass().getSimpleName() + ".fromPersistentString returned "
+					+ (enumValue == null ? "null" : enumValue.getPersistentString()) + " instead of "
+					+ persistentString);
 		}
 		return enumValue;
 	}
