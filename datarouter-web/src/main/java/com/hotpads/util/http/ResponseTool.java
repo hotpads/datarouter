ba1358ffcd7894a318f4b5a1431db6fe551c1fc5
cguillaume <cguillaume@hotpads.com>
2017-05-11 17:47:09 -0700

    clean up use ResponseTool to send back json

--- datarouter-web/src/main/java/com/hotpads/util/http/ResponseTool.java
+++ datarouter-web/src/main/java/com/hotpads/util/http/ResponseTool.java
@@ -1,7 +1,9 @@
 package com.hotpads.util.http;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,31 +12,33 @@ import com.google.gson.JsonObject;
 
 public class ResponseTool{
 
-	public static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
+	private static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
 
 	public static void sendError(HttpServletResponse response, int code, String message){
 		try{
-			response.sendError(code, message);  // html
+			response.sendError(code, message); // html
 		}catch(IOException e){
 			throw new RuntimeException(e);
 		}
 	}
 
-	public static void sendErrorInJsonForMessage(HttpServletResponse response, int code, String message){
+	public static void sendJsonForMessage(HttpServletResponse response, int code, String message) throws IOException{
 		JsonObject jsonObject = new JsonObject();
 		jsonObject.addProperty("message", message);
 		jsonObject.addProperty("httpResponseCode", code);
-		sendErrorInJson(response, code, jsonObject.toString());
+		sendJson(response, code, jsonObject.toString());
 	}
 
-	public static void sendErrorInJson(HttpServletResponse response, int code, String body){
-		response.setContentType(CONTENT_TYPE_APPLICATION_JSON);
+	public static void sendJson(HttpServletResponse response, int code, String body) throws IOException{
 		response.setStatus(code);
-		try{
-			response.getWriter().write(body);
-		}catch(IOException e){
-			sendError(response, code, body);
-		}
+		sendJson(response, body);
+	}
+
+	public static void sendJson(HttpServletResponse response, String body) throws IOException{
+		response.setContentType(ResponseTool.CONTENT_TYPE_APPLICATION_JSON);
+		new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8)
+				.append(body)
+				.close();
 	}
 
 	public static void sendRedirect(
@@ -44,14 +48,13 @@ public class ResponseTool{
 			String urlPath){
 		String fullyQualifiedUrl = urlPath;
 		if(!urlPath.contains("://")){
-			//really is just path, create fullUrl
+			// really is just path, create fullUrl
 			fullyQualifiedUrl = RequestTool.getFullyQualifiedUrl(urlPath, request).toString();
 		}
 		sendRedirect(response, code, fullyQualifiedUrl);
 	}
 
-	public static void sendRedirect(
-			HttpServletResponse response, int code, String fullyQualifiedUrl){
+	public static void sendRedirect(HttpServletResponse response, int code, String fullyQualifiedUrl){
 		response.setStatus(code);
 		response.addHeader("Location", fullyQualifiedUrl);
 	}
@@ -63,4 +66,5 @@ public class ResponseTool{
 			throw new RuntimeException(e);
 		}
 	}
+
 }
