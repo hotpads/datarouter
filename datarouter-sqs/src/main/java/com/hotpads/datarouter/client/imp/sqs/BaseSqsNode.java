6efa63c73a8da42c56c14b738a5a4a85fdef9848
mdumitriu <mdumitriu@hotpads.com>
2017-05-15 12:00:21 -0700

    LIS-2546 - empty sqs namespace makes the prefix to be "-". Fix

--- datarouter-sqs/src/main/java/com/hotpads/datarouter/client/imp/sqs/BaseSqsNode.java
+++ datarouter-sqs/src/main/java/com/hotpads/datarouter/client/imp/sqs/BaseSqsNode.java
@@ -18,6 +18,7 @@ import com.hotpads.datarouter.serialize.fielder.DatabeanFielder;
 import com.hotpads.datarouter.storage.databean.Databean;
 import com.hotpads.datarouter.storage.key.primary.PrimaryKey;
 import com.hotpads.datarouter.storage.queue.QueueMessageKey;
+import com.hotpads.datarouter.util.core.DrStringTool;
 import com.hotpads.util.core.concurrent.Lazy;
 
 public abstract class BaseSqsNode<
@@ -60,7 +61,7 @@ implements QueueStorageWriter<PK,D>{
 		}else{
 			String namespace = params.getNamespace()
 					.orElse(datarouterProperties.getEnvironment() + "-" + datarouterProperties.getServiceName());
-			String queueName = namespace == null ? getTableName() : namespace + "-" + getTableName();
+			String queueName = DrStringTool.isEmpty(namespace) ? getTableName() : namespace + "-" + getTableName();
 			queueUrl = tryCreateQueueAndGetUrl(queueName);
 		}
 		logger.warn("nodeName={}, queueName={}", getName(), queueUrl);
