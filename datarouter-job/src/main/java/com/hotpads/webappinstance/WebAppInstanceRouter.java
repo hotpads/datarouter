b519c60cd45a1dcf4d7cdbab1acd2f9d17c3b532
mattc <mcorgan@hotpads.com>
2017-05-28 12:38:23 -0700

    WebAppInstanceRouter

---
+++ datarouter-job/src/main/java/com/hotpads/webappinstance/WebAppInstanceRouter.java
@@ -0,0 +1,51 @@
+package com.hotpads.webappinstance;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import com.hotpads.datarouter.client.ClientId;
+import com.hotpads.datarouter.config.DatarouterSettings;
+import com.hotpads.datarouter.node.factory.NodeFactory;
+import com.hotpads.datarouter.node.op.combo.SortedMapStorage.SortedMapStorageNode;
+import com.hotpads.datarouter.routing.BaseRouter;
+import com.hotpads.datarouter.routing.Datarouter;
+import com.hotpads.webappinstance.databean.WebAppInstance;
+import com.hotpads.webappinstance.databean.WebAppInstance.WebAppInstanceFielder;
+import com.hotpads.webappinstance.databean.WebAppInstanceKey;
+
+@Singleton
+public class WebAppInstanceRouter extends BaseRouter implements WebAppInstanceNodes{
+
+	public static interface WebAppInstanceRouterParamsProvider{
+		WebAppInstanceRouterParams getWebAppInstanceRouterParams();
+	}
+
+	public static class WebAppInstanceRouterParams{
+		private final String configFileLocation;
+		private final ClientId clientId;
+
+		public WebAppInstanceRouterParams(String configFileLocation, ClientId clientId){
+			this.configFileLocation = configFileLocation;
+			this.clientId = clientId;
+		}
+	}
+
+	private static final String NAME = "webAppInstance";
+
+	private SortedMapStorageNode<WebAppInstanceKey,WebAppInstance> webAppInstance;
+
+	@Inject
+	public WebAppInstanceRouter(Datarouter datarouter, NodeFactory nodeFactory, DatarouterSettings datarouterSettings,
+			WebAppInstanceRouterParamsProvider paramsProvider){
+		super(datarouter, paramsProvider.getWebAppInstanceRouterParams().configFileLocation, NAME, nodeFactory,
+				datarouterSettings);
+		WebAppInstanceRouterParams params = paramsProvider.getWebAppInstanceRouterParams();
+		webAppInstance = createAndRegister(params.clientId, WebAppInstance::new, WebAppInstanceFielder::new);
+	}
+
+	@Override
+	public SortedMapStorageNode<WebAppInstanceKey,WebAppInstance> getWebAppInstance(){
+		return webAppInstance;
+	}
+
+}
