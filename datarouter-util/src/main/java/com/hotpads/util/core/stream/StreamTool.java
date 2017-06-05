5413b4a4863088659817becf0216e63dba3d0ee7
winstonq <winstonq@hotpads.com>
2017-06-04 17:01:55 -0700

    remove unused import

--- datarouter-util/src/main/java/com/hotpads/util/core/stream/StreamTool.java
+++ datarouter-util/src/main/java/com/hotpads/util/core/stream/StreamTool.java
@@ -2,7 +2,6 @@ package com.hotpads.util.core.stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
